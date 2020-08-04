cc9d89d288dc1f5ed335619dd4990f5c4dbe4b03
Sashank Thupukari <s@thupukari.com>
2020-08-04 06:38:07 -0400

    (instance-scheduler-4) Show schedule states on scheduler page
    Test Plan: unit
    
    Reviewers: alangenfeld, prha, bengotow, aj.nadel
    
    Differential Revision: https://dagster.phacility.com/D4085

---
+++ js_modules/dagit/src/schedules/ScheduleUtils.tsx
@@ -0,0 +1,106 @@
+import gql from "graphql-tag";
+import { SCHEDULER_FRAGMENT } from "./SchedulerInfo";
+import PythonErrorInfo from "../PythonErrorInfo";
+
+export const SCHEDULE_STATE_FRAGMENT = gql`
+  fragment ScheduleStateFragment on ScheduleState {
+    __typename
+    id
+    scheduleOriginId
+    scheduleName
+    cronSchedule
+    runningScheduleCount
+    ticks(limit: 1) {
+      tickId
+      status
+      timestamp
+      tickSpecificData {
+        __typename
+        ... on ScheduleTickSuccessData {
+          run {
+            pipelineName
+            status
+            runId
+          }
+        }
+        ... on ScheduleTickFailureData {
+          error {
+            ...PythonErrorFragment
+          }
+        }
+      }
+    }
+    runsCount
+    runs(limit: 10) {
+      runId
+      tags {
+        key
+        value
+      }
+      pipelineName
+      status
+    }
+    ticksCount
+    status
+  }
+
+  ${PythonErrorInfo.fragments.PythonErrorFragment}
+`;
+
+export const SCHEDULE_DEFINITION_FRAGMENT = gql`
+  fragment ScheduleDefinitionFragment on ScheduleDefinition {
+    name
+    cronSchedule
+    pipelineName
+    solidSelection
+    mode
+    partitionSet {
+      name
+    }
+    scheduleState {
+      ...ScheduleStateFragment
+    }
+  }
+  ${SCHEDULE_STATE_FRAGMENT}
+`;
+
+export const SCHEDULES_ROOT_QUERY = gql`
+  query SchedulesRootQuery($repositorySelector: RepositorySelector!) {
+    repositoryOrError(repositorySelector: $repositorySelector) {
+      __typename
+      ... on Repository {
+        name
+        id
+        origin {
+          executablePath
+          codePointerDescription
+        }
+      }
+      ...PythonErrorFragment
+    }
+    scheduler {
+      ...SchedulerFragment
+    }
+    scheduleDefinitionsOrError(repositorySelector: $repositorySelector) {
+      ... on ScheduleDefinitions {
+        results {
+          ...ScheduleDefinitionFragment
+        }
+      }
+      ...PythonErrorFragment
+    }
+    scheduleStatesOrError(repositorySelector: $repositorySelector, withNoScheduleDefinition: true) {
+      __typename
+      ... on ScheduleStates {
+        results {
+          ...ScheduleStateFragment
+        }
+      }
+      ...PythonErrorFragment
+    }
+  }
+
+  ${SCHEDULE_DEFINITION_FRAGMENT}
+  ${SCHEDULER_FRAGMENT}
+  ${PythonErrorInfo.fragments.PythonErrorFragment}
+`;
