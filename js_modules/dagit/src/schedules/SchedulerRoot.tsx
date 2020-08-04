2a20b2611ead05f447448cec5f84bb7591e47c5a
Sashank Thupukari <s@thupukari.com>
2020-08-04 08:13:30 -0400

    (instance-scheduler-5) Support changing schedule status available schedules from scheduler page
    Test Plan: manual
    
    Reviewers: alangenfeld, aj.nadel, prha
    
    Differential Revision: https://dagster.phacility.com/D4086

--- js_modules/dagit/src/schedules/SchedulerRoot.tsx
+++ js_modules/dagit/src/schedules/SchedulerRoot.tsx
@@ -11,6 +11,7 @@ import {
 import { ScheduleStateRow } from "./ScheduleRow";
 import PythonErrorInfo from "../PythonErrorInfo";
 import { SCHEDULE_STATE_FRAGMENT } from "./ScheduleUtils";
+import { useRepositoryOptions } from "../DagsterRepositoryContext";
 
 export const SchedulerRoot: React.FunctionComponent<{}> = () => {
   const queryResult = useQuery<SchedulerRootQuery>(SCHEDULER_ROOT_QUERY, {
@@ -39,7 +40,11 @@ export const SchedulerRoot: React.FunctionComponent<{}> = () => {
 const ScheduleStates: React.FunctionComponent<{
   scheduleStatesOrError: SchedulerRootQuery_scheduleStatesOrError;
 }> = ({ scheduleStatesOrError }) => {
-  if (scheduleStatesOrError.__typename === "PythonError") {
+  const { options, error } = useRepositoryOptions();
+
+  if (error) {
+    return <PythonErrorInfo error={error} />;
+  } else if (scheduleStatesOrError.__typename === "PythonError") {
     return <PythonErrorInfo error={scheduleStatesOrError} />;
   } else if (scheduleStatesOrError.__typename === "RepositoryNotFoundError") {
     // Can't reach this case because we didn't use a repository selector
@@ -47,10 +52,39 @@ const ScheduleStates: React.FunctionComponent<{
   }
 
   const { results: scheduleStates } = scheduleStatesOrError;
+
+  // Build map of repositoryOriginId to DagsterRepoOption
+  const repositoryOriginIdMap = {};
+  for (const option of options) {
+    repositoryOriginIdMap[option.repository.id] = option;
+  }
+
+  // Seperate out schedules into in-scope and out-of-scope
+  const inContextSchedules = scheduleStates.filter(({ repositoryOriginId }) =>
+    repositoryOriginIdMap.hasOwnProperty(repositoryOriginId)
+  );
+
+  const outOfContextSchedules = scheduleStates.filter(
+    ({ repositoryOriginId }) => !repositoryOriginIdMap.hasOwnProperty(repositoryOriginId)
+  );
+
   return (
     <div>
       <h2>All Schedules:</h2>
-      {scheduleStates.map(scheduleState => (
+      <Divider />
+
+      <h3>In context schedules:</h3>
+      {inContextSchedules.map(scheduleState => (
+        <ScheduleStateRow
+          scheduleState={scheduleState}
+          key={scheduleState.scheduleOriginId}
+          showStatus={true}
+          dagsterRepoOption={repositoryOriginIdMap[scheduleState.repositoryOriginId]}
+        />
+      ))}
+
+      <h3>Out of context schedules:</h3>
+      {outOfContextSchedules.map(scheduleState => (
         <ScheduleStateRow
           scheduleState={scheduleState}
           key={scheduleState.scheduleOriginId}
