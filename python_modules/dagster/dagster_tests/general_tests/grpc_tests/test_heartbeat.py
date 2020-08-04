5f53a6494cd4f9322f1f2fdfa54f1fc2be111459
Max Gasner <max.gasner@gmail.com>
2020-08-04 17:46:58 -0400

    Heartbeat for GRPC server
    Summary: This lets managed servers shut themselves down when the client processes disappear
    
    Test Plan: Unit
    
    Reviewers: dgibson
    
    Differential Revision: https://dagster.phacility.com/D4099

---
+++ python_modules/dagster/dagster_tests/general_tests/grpc_tests/test_heartbeat.py
@@ -0,0 +1,21 @@
+import time
+
+from dagster.grpc.server import GrpcServerProcess
+from dagster.grpc.types import LoadableTargetOrigin
+from dagster.utils import file_relative_path
+
+
+def test_heartbeat():
+    loadable_target_origin = LoadableTargetOrigin(
+        attribute='bar_repo', python_file=file_relative_path(__file__, 'grpc_repo.py'),
+    )
+    with GrpcServerProcess(
+        loadable_target_origin=loadable_target_origin,
+        max_workers=2,
+        heartbeat=True,
+        heartbeat_timeout=1,
+    ) as server:
+        server.create_client().heartbeat()
+        assert server.server_process.poll() is None
+        time.sleep(2)
+        assert server.server_process.poll() is not None
