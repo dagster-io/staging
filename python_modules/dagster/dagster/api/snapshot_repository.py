e14651e3725ec4f79186c095ed69304c27d6cbbd
gibsondan <daniel@elementl.com>
2020-08-13 13:47:52 -0400

    Make 'reload repository' option reload the code pointer dict
    Summary:
    Right now we calculate the set of repositories and code pointers once, at dagit startup. This means that the reload button doesn't actually discover any new, removed, or renamed repositories. Fix that by moving the calculation of the code pointer dict into the repo location constructor rather than the handle constructor.
    
    Test Plan: BK
    Manually launch dagit, add a repository, press reload button, see new repository in list (not sure how to test that in an automated way)
    
    Reviewers: alangenfeld, prha, catherinewu

--- python_modules/dagster/dagster/api/snapshot_repository.py
+++ python_modules/dagster/dagster/api/snapshot_repository.py
@@ -1,4 +1,5 @@
 from dagster import check
+from dagster.core.code_pointer import CodePointer
 from dagster.core.host_representation import (
     ExternalRepository,
     ExternalRepositoryData,
@@ -7,6 +8,7 @@ from dagster.core.host_representation import (
 )
 from dagster.core.origin import RepositoryGrpcServerOrigin, RepositoryPythonOrigin
 
+from .list_repositories import sync_list_repositories, sync_list_repositories_grpc
 from .utils import execute_unary_api_cli_command
 
 
@@ -15,14 +17,50 @@ def sync_get_external_repositories(repository_location_handle):
         repository_location_handle, 'repository_location_handle', RepositoryLocationHandle,
     )
 
+    loadable_target_origin = repository_location_handle.loadable_target_origin
+
+    response = sync_list_repositories(
+        executable_path=loadable_target_origin.executable_path,
+        python_file=loadable_target_origin.python_file,
+        module_name=loadable_target_origin.module_name,
+        working_directory=loadable_target_origin.working_directory,
+        attribute=loadable_target_origin.attribute,
+    )
+
+    repository_code_pointer_dict = {}
+
+    if loadable_target_origin.python_file:
+        repository_code_pointer_dict = {
+            lrs.repository_name: CodePointer.from_python_file(
+                loadable_target_origin.python_file,
+                lrs.attribute,
+                loadable_target_origin.working_directory,
+            )
+            for lrs in response.repository_symbols
+        }
+    elif repository_location_handle.use_python_package:
+        repository_code_pointer_dict = {
+            lrs.repository_name: CodePointer.from_python_package(
+                loadable_target_origin.module_name, lrs.attribute
+            )
+            for lrs in response.repository_symbols
+        }
+    else:
+        repository_code_pointer_dict = {
+            lrs.repository_name: CodePointer.from_module(
+                loadable_target_origin.module_name, lrs.attribute
+            )
+            for lrs in response.repository_symbols
+        }
+
     repos = []
 
-    for _, pointer in repository_location_handle.repository_code_pointer_dict.items():
+    for _, pointer in repository_code_pointer_dict.items():
         external_repository_data = check.inst(
             execute_unary_api_cli_command(
-                repository_location_handle.executable_path,
+                loadable_target_origin.executable_path,
                 'repository',
-                RepositoryPythonOrigin(repository_location_handle.executable_path, pointer),
+                RepositoryPythonOrigin(loadable_target_origin.executable_path, pointer),
             ),
             ExternalRepositoryData,
         )
@@ -44,8 +82,11 @@ def sync_get_external_repositories_grpc(api_client, repository_location_handle):
         repository_location_handle, 'repository_location_handle', RepositoryLocationHandle
     )
 
+    list_repositories_response = sync_list_repositories_grpc(repository_location_handle.client)
+
     repos = []
-    for repository_name in repository_location_handle.repository_names:
+    for symbol in list_repositories_response.repository_symbols:
+        repository_name = symbol.repository_name
         external_repository_data = check.inst(
             api_client.external_repository(
                 repository_grpc_server_origin=RepositoryGrpcServerOrigin(
