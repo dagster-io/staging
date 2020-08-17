c070f146ddefa2219772134b43a13fd3353a368b
Leor <leor.fishman@gmail.com>
2020-08-17 10:11:13 -0400

    ge tutorial
    Summary: tutorial for GE
    
    Test Plan: unit+
    
    Reviewers: max, sandyryza
    
    Subscribers:

---
+++ examples/ge_example/tests/test_ge_example.py
@@ -0,0 +1,13 @@
+from examples.ge_example.ge_demo import payroll_data_pipeline
+
+from dagster import execute_pipeline
+
+
+def test_pipeline_success():
+    res = execute_pipeline(payroll_data_pipeline, preset='sample_preset_success')
+    assert res.success
+
+
+def test_pipeline_failure():
+    res = execute_pipeline(payroll_data_pipeline, preset='sample_preset_fail')
+    assert not res.success
