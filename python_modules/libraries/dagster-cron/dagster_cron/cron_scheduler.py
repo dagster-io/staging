955e48c6acb15eaaf7e53ce0c11f5be4f40053f3
catherinewu <cat@elementl.com>
2020-08-18 01:15:11 +0000

    patch cronjob instead of deleting + creating cronjob
    Summary: We should patch the k8s cronjob instead of stopping + creating the cronjob to minimize downtime
    
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D4205

--- python_modules/libraries/dagster-cron/dagster_cron/cron_scheduler.py
+++ python_modules/libraries/dagster-cron/dagster_cron/cron_scheduler.py
@@ -99,6 +99,13 @@ class SystemCronScheduler(Scheduler, ConfigurableClass):
                 )
             )
 
+    def refresh_schedule(self, instance, external_schedule):
+        check.inst_param(instance, 'instance', DagsterInstance)
+        check.inst_param(external_schedule, 'external_schedule', ExternalSchedule)
+
+        self.stop_schedule(instance, external_schedule.get_origin_id())
+        self.start_schedule(instance, external_schedule)
+
     def wipe(self, instance):
         # Note: This method deletes schedules from ALL repositories
         check.inst_param(instance, 'instance', DagsterInstance)
