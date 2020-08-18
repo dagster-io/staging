e04e43003cc87db63161fc36c209d19e3177a0b3
Sandy Ryza <sandy@elementl.com>
2020-08-18 16:29:24 -0700

    subset selection syntax over lakehouse assets
    Summary:
    
    Test Plan: added tests
    
    Reviewers: yuhan, cdecarolis, alangenfeld
    
    Subscribers:

---
+++ python_modules/libraries/lakehouse/lakehouse/queryable_asset_set.py
@@ -0,0 +1,28 @@
+from typing import Dict, List, Set
+
+from lakehouse.asset import Asset
+
+from dagster.core.selector.subset_selector import Traverser, clause_to_subset
+
+
+class QueryableAssetSet:
+    def __init__(self, assets: List[Asset]):
+        self._assets_by_path = {asset.path: asset for asset in assets}
+        self._dep_graph = generate_dep_graph(assets)
+
+    def query_assets(self, query: str) -> List[Asset]:
+        traverser = Traverser(graph=self._dep_graph)
+        return clause_to_subset(traverser, self._dep_graph, query)
+
+
+def generate_dep_graph(assets) -> Dict[str, Dict[str, Set[str]]]:
+    # defaultdict isn't appropriate because we also want to include items without dependencies
+    graph = {'upstream': {}, 'downstream': {}}
+    for asset in assets:
+        graph['upstream'][asset.path] = set()
+        if asset.computation:
+            for dep in asset.computation.deps.values():
+                graph['upstream'][asset.path].add(dep.asset.path)
+                graph['downstream'][dep.asset.path].setdefault(set()).add(asset.path)
+
+    return graph
