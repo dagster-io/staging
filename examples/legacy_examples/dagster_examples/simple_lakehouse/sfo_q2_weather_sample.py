e3ef1a50e5f90317acba464b1e47f40a02973844
Sandy Ryza <sandy@elementl.com>
2020-08-18 10:58:16 -0700

    format examples imports

--- examples/legacy_examples/dagster_examples/simple_lakehouse/sfo_q2_weather_sample.py
+++ examples/legacy_examples/dagster_examples/simple_lakehouse/sfo_q2_weather_sample.py
@@ -1,6 +1,5 @@
-from lakehouse import source_table, Column
+from lakehouse import Column, source_table
 from pyarrow import float64, string
-
 
 sfo_q2_weather_sample_table = source_table(
     storage_key='filesystem',
