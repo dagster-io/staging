73cf6644115259677e45be519ef80160483c98d8
gibsondan <daniel@elementl.com>
2020-08-22 12:06:32 -0400

    Revert "temporarily disable more flaky tests"
    Summary: This reverts commit 7a57a813df2b6064d848e0297e93375c0f2ea244.
    
    Test Plan: BK
    
    Differential Revision: https://dagster.phacility.com/D4263

--- python_modules/dagster/dagster_tests/core_tests/launcher_tests/test_default_run_launcher.py
+++ python_modules/dagster/dagster_tests/core_tests/launcher_tests/test_default_run_launcher.py
@@ -184,18 +184,8 @@ def get_external_pipeline_from_in_process_location(pipeline_name):
     "get_external_pipeline",
     [
         get_external_pipeline_from_grpc_server_repository,
-        pytest.param(
-            get_external_pipeline_from_managed_grpc_python_env_repository,
-            marks=pytest.mark.skip(
-                reason="Flaky in Buildkite, see https://github.com/dagster-io/dagster/issues/2846"
-            ),
-        ),
-        pytest.param(
-            get_external_pipeline_from_in_process_location,
-            marks=pytest.mark.skip(
-                reason="Flaky in Buildkite, see https://github.com/dagster-io/dagster/issues/2846"
-            ),
-        ),
+        get_external_pipeline_from_managed_grpc_python_env_repository,
+        get_external_pipeline_from_in_process_location,
     ],
 )
 def test_successful_run(get_external_pipeline):  # pylint: disable=redefined-outer-name
