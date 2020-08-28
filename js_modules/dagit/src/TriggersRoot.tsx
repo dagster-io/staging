da9cbd1f85e52d4511bdf9ee1914e086a4f7a7a2
prha <prha@elementl.com>
2020-08-27 23:26:11 -0700

    [trigger-6] add execution-time based graphs on the trigger dashboard
    Test Plan: bk
    
    Reviewers: catherinewu, sashank
    
    Differential Revision: https://dagster.phacility.com/D4307

--- js_modules/dagit/src/TriggersRoot.tsx
+++ js_modules/dagit/src/TriggersRoot.tsx
@@ -22,6 +22,7 @@ import { showCustomAlert } from "./CustomAlertProvider";
 import { RunStatus } from "./runs/RunStatusDots";
 import { HighlightedCodeBlock } from "./HighlightedCodeBlock";
 import { RunTable } from "./runs/RunTable";
+import { TriggerGraphs, TRIGGER_GRAPHS_RUN_FRAGMENT } from "./TriggerGraphs";
 
 type Trigger = TriggersRootQuery_triggerDefinitionsOrError_TriggerDefinitions_results;
 
@@ -79,6 +80,15 @@ export const TriggersRoot: React.FunctionComponent<RouteComponentProps> = ({ mat
           );
         }
 
+        const allStepKeys = {};
+        if (triggerName) {
+          matching[0].runs.forEach(run => {
+            run.stepStats.forEach(stat => {
+              allStepKeys[stat.stepKey] = true;
+            });
+          });
+        }
+
         return (
           <>
             <ScrollContainer>
@@ -86,11 +96,7 @@ export const TriggersRoot: React.FunctionComponent<RouteComponentProps> = ({ mat
               <TriggersTable triggers={matching} />
               <div style={{ marginTop: 30 }}>
                 {triggerName && matching[0].runs ? (
-                  <RunTable
-                    runs={matching[0].runs || []}
-                    onSetFilter={() => {}}
-                    nonIdealState={<div />}
-                  />
+                  <TriggerGraphs runs={matching[0].runs} stepKeys={Object.keys(allStepKeys)} />
                 ) : null}
               </div>
             </ScrollContainer>
@@ -264,7 +270,7 @@ const TRIGGERS_ROOT_QUERY = gql`
           pipelineName
           solidSelection
           mode
-          runs(limit: 10) {
+          runs {
             runId
             tags {
               key
@@ -272,7 +278,11 @@ const TRIGGERS_ROOT_QUERY = gql`
             }
             pipelineName
             status
+            stepStats {
+              stepKey
+            }
             ...RunTableRunFragment
+            ...TriggerGraphsRunFragment
           }
         }
       }
@@ -283,6 +293,7 @@ const TRIGGERS_ROOT_QUERY = gql`
   }
   ${PythonErrorInfo.fragments.PythonErrorFragment}
   ${RunTable.fragments.RunTableRunFragment}
+  ${TRIGGER_GRAPHS_RUN_FRAGMENT}
 `;
 
 const FETCH_TRIGGER_CONFIG = gql`
