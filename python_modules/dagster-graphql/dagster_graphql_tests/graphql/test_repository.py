5c532492b11f3958a011389231641ff2d2e96e70
prha <prha@elementl.com>
2020-09-01 17:44:41 -0700

    add test coverage for graphql query gaps in assets/repository/schedules
    Summary: looked at coveralls percentages
    
    Test Plan: bk
    
    Reviewers: max, sashank
    
    Differential Revision: https://dagster.phacility.com/D4329

---
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/test_repository.py
@@ -0,0 +1,39 @@
+from dagster_graphql.test.utils import execute_dagster_graphql, infer_repository_selector
+
+
+def repository_query():
+    return """
+    query RepositoryQuery($repositorySelector: RepositorySelector!) {
+        repositoryOrError(repositorySelector: $repositorySelector) {
+           ... on Repository {
+                id
+                origin {
+                    __typename
+                    ... on PythonRepositoryOrigin {
+                        codePointer {
+                            description
+                            metadata {
+                                __typename
+                            }
+                        }
+                    }
+                    ... on GrpcRepositoryOrigin {
+                        grpcUrl
+                    }
+                }
+                location {
+                    name
+                    environmentPath
+                }
+            }
+        }
+    }
+    """
+
+
+def test_query_all_solids(graphql_context, snapshot):
+    selector = infer_repository_selector(graphql_context)
+    result = execute_dagster_graphql(
+        graphql_context, repository_query(), variables={"repositorySelector": selector}
+    )
+    snapshot.assert_match(result.data)
