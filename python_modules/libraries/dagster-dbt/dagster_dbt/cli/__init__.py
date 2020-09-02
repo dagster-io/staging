056641c344d9c5330ae4085af2db1ab6738e6ac1
AJ Nadel <aj@ajnadel.com>
2020-09-02 14:53:23 -0700

    [WIP] add dbt cli module
    Test Plan: add tests soon
    
    Reviewers: sashank
    
    Differential Revision: https://dagster.phacility.com/D4336

---
+++ python_modules/libraries/dagster-dbt/dagster_dbt/cli/__init__.py
@@ -0,0 +1,8 @@
+from .solids import (
+    dbt_cli_run,
+    dbt_cli_run_operation,
+    dbt_cli_snapshot,
+    dbt_cli_snapshot_freshness,
+    dbt_cli_test,
+)
+from .types import DbtCliResult, DbtCliStatsResult
