278efa2b6d08da4f8db893ee590e02fdd3432b91
prha <prha@elementl.com>
2020-09-01 17:44:41 -0700

    add test coverage for graphql query gaps in assets/repository/schedules
    Summary: looked at coveralls percentages
    
    Test Plan: bk
    
    Reviewers: max, sashank
    
    Differential Revision: https://dagster.phacility.com/D4329

---
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_repository.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['test_query_all_solids 1'] = {
+    'repositoryOrError': {
+        'id': '4497bd467cf4af860afa9c962396f415ca1c1b42',
+        'location': {
+            'environmentPath': None,
+            'name': '<<in_process>>'
+        },
+        'origin': {
+            '__typename': 'PythonRepositoryOrigin',
+            'codePointer': {
+                'description': '/Users/prha/code/dagster/python_modules/dagster-graphql/dagster_graphql_tests/graphql/setup.py::test_repo',
+                'metadata': [
+                    {
+                        'key': 'python_file',
+                        'value': '/Users/prha/code/dagster/python_modules/dagster-graphql/dagster_graphql_tests/graphql/setup.py'
+                    },
+                    {
+                        'key': 'attribute',
+                        'value': 'test_repo'
+                    }
+                ]
+            }
+        }
+    }
+}
---
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_scheduler.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['test_get_schedule_states_for_repository_after_reconcile_using_mutation 1'] = {
+    'scheduleStatesOrError': {
+        '__typename': 'ScheduleStates',
+        'results': [
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            },
+            {
+                'runs': [
+                ],
+                'runsCount': 0,
+                'stats': {
+                    'ticksFailed': 0,
+                    'ticksSkipped': 0,
+                    'ticksStarted': 0,
+                    'ticksSucceeded': 0
+                },
+                'status': 'STOPPED',
+                'ticks': [
+                ],
+                'ticksCount': 0
+            }
+        ]
+    }
+}
