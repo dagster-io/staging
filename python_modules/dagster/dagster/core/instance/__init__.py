feaae0fa69cd2e3d29671c8a1e7317cd1caedbb7
catherinewu <cat@elementl.com>
2020-09-03 05:34:02 +0000

    generate synthetic step fail event when k8s step job raises a DagsterK8sError
    Test Plan: todo
    
    Differential Revision: https://dagster.phacility.com/D4337

--- python_modules/dagster/dagster/core/instance/__init__.py
+++ python_modules/dagster/dagster/core/instance/__init__.py
@@ -1011,6 +1011,7 @@ class DagsterInstance:
             timestamp=time.time(),
             dagster_event=dagster_event,
         )
+
         self.handle_new_event(event_record)
         return dagster_event
 
