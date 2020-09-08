7eac201f0da1d0dc7f28b3c48ccaf66c049d50b3
Johann Miller <johann@elementl.com>
2020-09-08 16:13:44 -0400

    Celery run queue
    Test Plan: manual
    
    Differential Revision: https://dagster.phacility.com/D4367

--- integration_tests/test_suites/celery-k8s-integration-test-suite/test_integration.py
+++ integration_tests/test_suites/celery-k8s-integration-test-suite/test_integration.py
@@ -42,6 +42,7 @@ def get_celery_engine_config(dagster_docker_image, job_namespace):
 def test_execute_on_celery_k8s(  # pylint: disable=redefined-outer-name
     dagster_docker_image, dagster_instance, helm_namespace
 ):
+
     run_config = merge_dicts(
         merge_yamls(
             [
@@ -318,3 +319,39 @@ def test_execute_on_celery_k8s_with_env_var_and_termination(  # pylint: disable=
     )
 
     _test_termination(dagster_instance, run_config)
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(sys.version_info < (3, 5), reason="Very slow on Python 2")
+def test_execute_on_celery_k8s_run_queued(  # pylint: disable=redefined-outer-name
+    dagster_docker_image, dagster_instance, helm_namespace_run_launcher_queued
+):
+
+    run_config = merge_dicts(
+        merge_yamls(
+            [
+                os.path.join(test_project_environments_path(), 'env.yaml'),
+                os.path.join(test_project_environments_path(), 'env_s3.yaml'),
+            ]
+        ),
+        get_celery_engine_config(
+            dagster_docker_image=dagster_docker_image,
+            job_namespace=helm_namespace_run_launcher_queued,
+        ),
+    )
+
+    pipeline_name = 'demo_pipeline_celery'
+    run = create_run_for_test(
+        dagster_instance, pipeline_name=pipeline_name, run_config=run_config, mode='default',
+    )
+
+    dagster_instance.launch_run(
+        run.run_id,
+        ReOriginatedExternalPipelineForTest(get_test_project_external_pipeline(pipeline_name)),
+    )
+
+    result = wait_for_job_and_get_raw_logs(
+        job_name='dagster-run-%s' % run.run_id, namespace=helm_namespace_run_launcher_queued
+    )
+
+    assert 'PIPELINE_SUCCESS' in result, 'no match, result: {}'.format(result)
--- python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/app.py
+++ python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/app.py
@@ -1,15 +1,10 @@
 from dagster_celery.make_app import make_app_with_task_routes
 
 from .executor import create_k8s_job_task
+from .launcher import create_k8s_run_coordinator_job_task
 
-app = make_app_with_task_routes(
-    task_routes={
-        "execute_step_k8s_job": {
-            "queue": "dagster",
-            "routing_key": "dagster.execute_step_k8s_job",
-        },
-    }
-)
+app = make_app_with_task_routes(task_routes={})
 
-
+# All tasks must be called here to be registered
 execute_step_k8s_job = create_k8s_job_task(app)
+execute_run_coordinator_k8s_job = create_k8s_run_coordinator_job_task(app)
