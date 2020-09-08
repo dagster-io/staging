ec8b4c4b2fa74ee4cc15e305ec7277cd11b7fdde
Johann Miller <johann@elementl.com>
2020-09-08 16:13:44 -0400

    Celery run queue
    Summary:
    
    Test Plan:
    manual
    Reviewers:
    
    Subscribers:

--- python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/app.py
+++ python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/app.py
@@ -1,6 +1,7 @@
 from dagster_celery.make_app import make_app_with_task_routes
 
 from .executor import create_k8s_job_task
+from .queued_launcher import create_k8s_run_coordinator_job_task
 
 app = make_app_with_task_routes(
     task_routes={
@@ -8,8 +9,13 @@ app = make_app_with_task_routes(
             "queue": "dagster",
             "routing_key": "dagster.execute_step_k8s_job",
         },
+        "execute_run_coordinator_k8s_job": {
+            "queue": "dagster",
+            "routing_key": "dagster.execute_run_coordinator_k8s_job",
+        },
     }
 )
 
-
+# All tasks must be called here to be registered
 execute_step_k8s_job = create_k8s_job_task(app)
+execute_run_coordinator_k8s_job = create_k8s_run_coordinator_job_task(app)
--- python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/executor.py
+++ python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/executor.py
@@ -89,12 +89,12 @@ def celery_k8s_job_executor(init_context):
     run_launcher = init_context.instance.run_launcher
     exc_cfg = init_context.executor_config
 
-    check.inst(
-        run_launcher,
-        CeleryK8sRunLauncher,
-        "This engine is only compatible with a CeleryK8sRunLauncher; configure the "
-        "CeleryK8sRunLauncher on your instance to use it.",
-    )
+    # check.inst(
+    #     run_launcher,
+    #     CeleryK8sRunLauncher,
+    #     "This engine is only compatible with a CeleryK8sRunLauncher; configure the "
+    #     "CeleryK8sRunLauncher on your instance to use it.",
+    # )
 
     job_config = DagsterK8sJobConfig(
         dagster_home=run_launcher.dagster_home,
