2fd4ca6dd0f7322cfe0d47c5cb084f39bead08e0
Johann Miller <johann@elementl.com>
2020-09-08 16:13:44 -0400

    Celery run queue
    Test Plan: manual
    
    Differential Revision: https://dagster.phacility.com/D4367

--- python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/executor.py
+++ python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s/executor.py
@@ -215,8 +215,8 @@ def _submit_task_k8s_job(app, pipeline_context, step, queue, priority):
     )
 
     return task_signature.apply_async(
-        priority=priority,
         queue=queue,
+        priority=priority,
         routing_key="{queue}.execute_step_k8s_job".format(queue=queue),
     )
 
