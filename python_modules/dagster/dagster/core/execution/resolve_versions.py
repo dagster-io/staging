a81f0ccf10c3ed238b89869ce8729fd21e16045f
Sandy Ryza <sandy@elementl.com>
2020-09-17 11:30:11 -0700

    Use version resolution logic for result versions
    Test Plan: added some tests
    
    Reviewers: cdecarolis, yuhan
    
    Differential Revision: https://dagster.phacility.com/D4465

--- python_modules/dagster/dagster/core/execution/resolve_versions.py
+++ python_modules/dagster/dagster/core/execution/resolve_versions.py
@@ -27,6 +27,9 @@ def _resolve_step_input_versions(step, step_versions):
             version = step_input.dagster_type.loader.compute_loaded_input_version(
                 step_input.config_data
             )
+        else:
+            version = None
+
         input_versions[input_name] = version
 
     return input_versions
@@ -61,6 +64,19 @@ def resolve_step_versions(speculative_execution_plan):
         step_versions[step.key] = step_version
 
     return step_versions
+
+
+def resolve_step_output_versions(speculative_execution_plan):
+    from dagster.core.execution.plan.objects import StepOutputHandle
+
+    step_versions = resolve_step_versions(speculative_execution_plan)
+    return {
+        StepOutputHandle(step.key, output_name): join_and_hash(
+            [output_name, step_versions[step.key]]
+        )
+        for step in speculative_execution_plan.steps
+        for output_name in step.step_output_dict.keys()
+    }
 
 
 def join_and_hash(lst):
