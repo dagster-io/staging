b6c63756c76b679784766d22f6f6b1917811daf4
Sashank Thupukari <s@thupukari.com>
2020-09-24 13:34:56 -0400

    Clean up scheduler instance page
    Test Plan: todo
    
    Differential Revision: https://dagster.phacility.com/D4536

--- js_modules/dagit/src/schedules/ScheduleRow.tsx
+++ js_modules/dagit/src/schedules/ScheduleRow.tsx
@@ -21,6 +21,7 @@ import * as React from 'react';
 import {Link, useHistory, useRouteMatch} from 'react-router-dom';
 import styled from 'styled-components/macro';
 
+import {ButtonLink} from '../ButtonLink';
 import {showCustomAlert} from '../CustomAlertProvider';
 import {ConfirmationOptions, useConfirmation} from '../CustomConfirmationProvider';
 import {
@@ -459,7 +460,6 @@ export const ScheduleStateRow: React.FunctionComponent<{
   });
 
   const {options} = useRepositoryOptions();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, setRepo] = useCurrentRepositoryState(options);
   const history = useHistory();
   const confirm = useConfirmation();
@@ -474,14 +474,6 @@ export const ScheduleStateRow: React.FunctionComponent<{
     scheduleOriginId,
   } = scheduleState;
   const latestTick = ticks.length > 0 ? ticks[0] : null;
-
-  const goToRepositorySchedules = () => {
-    if (!dagsterRepoOption) {
-      return;
-    }
-    setRepo(dagsterRepoOption);
-    history.push(`/schedules`);
-  };
 
   const goToSchedule = () => {
     if (!dagsterRepoOption) {
@@ -549,16 +541,10 @@ export const ScheduleStateRow: React.FunctionComponent<{
         </RowColumn>
       )}
       <RowColumn style={{flex: 1.4}}>
-        <div>{scheduleName}</div>
-        {dagsterRepoOption && (
-          <div style={{marginTop: 10}}>
-            <Button onClick={goToRepositorySchedules} small={true}>
-              Go to repository schedules
-            </Button>{' '}
-            <Button onClick={goToSchedule} small={true}>
-              Go to schedule page
-            </Button>
-          </div>
+        {dagsterRepoOption ? (
+          <ButtonLink onClick={goToSchedule}>{scheduleName}</ButtonLink>
+        ) : (
+          <div>{scheduleName}</div>
         )}
       </RowColumn>
       <RowColumn
