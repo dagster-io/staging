8efb1c56c943423f6d1350e46efc11955057be71
Sashank Thupukari <s@thupukari.com>
2020-09-25 02:29:48 -0400

    Add full test suite
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D4545

---
+++ integration_tests/test_suites/full-k8s-integration-test-suite/test_integration_base.py
@@ -0,0 +1,111 @@
+import pytest
+from dagster_k8s.test import wait_for_job_and_get_raw_logs
+from dagster_k8s_test_infra.integration_utils import ReOriginatedExternalPipelineForTest
+from dagster_test.test_project import get_test_project_external_pipeline
+
+from dagster.core.test_utils import create_run_for_test
+
+
+class TestK8sIntegration:
+    """
+    Base Class meant to be subclassed
+    """
+
+    @pytest.fixture(name="pipeline_name", params=[])
+    def pipeline_name(self, request):
+        with request.param() as s:
+            yield s
+
+    @pytest.fixture(name="run_config", params=[])
+    def run_config(self, request):
+        with request.param() as s:
+            yield s
+
+    @pytest.fixture(name="subset_run_config", params=[])
+    def subset_run_config(self, request):
+        with request.param() as s:
+            yield s
+
+    def test_launch_run(
+        self,
+        # From conftest
+        dagster_instance,
+        helm_namespace,
+        # From class fixture
+        pipeline_name,
+        run_config,
+    ):
+        # Create pipeline run
+        external_pipeline = get_test_project_external_pipeline(pipeline_name)
+        pipeline_run = create_run_for_test(
+            dagster_instance, pipeline_name=pipeline_name, run_config=run_config, mode="default"
+        )
+
+        # Launch pipeline
+        reoriginated_external_pipeline = ReOriginatedExternalPipelineForTest(external_pipeline)
+        dagster_instance.launch_run(pipeline_run.run_id, reoriginated_external_pipeline)
+
+        # Verify run was successful
+        result = wait_for_job_and_get_raw_logs(
+            job_name="dagster-run-%s" % pipeline_run.run_id, namespace=helm_namespace
+        )
+        assert "PIPELINE_SUCCESS" in result, "no match, result: {}".format(result)
+
+    def test_launch_run_with_subset(
+        self,
+        # From conftest
+        dagster_instance,
+        helm_namespace,
+        # From class fixture
+        pipeline_name,
+        subset_run_config,
+    ):
+        # Create pipeline run
+        external_pipeline = get_test_project_external_pipeline(pipeline_name)
+        pipeline_run = create_run_for_test(
+            dagster_instance,
+            pipeline_name=pipeline_name,
+            run_config=subset_run_config,
+            mode="default",
+            solids_to_execute={"count_letters"},
+        )
+
+        # Launch pipeline
+        reoriginated_external_pipeline = ReOriginatedExternalPipelineForTest(external_pipeline)
+        dagster_instance.launch_run(pipeline_run.run_id, reoriginated_external_pipeline)
+
+        # Verify run was successful
+        result = wait_for_job_and_get_raw_logs(
+            job_name="dagster-run-%s" % pipeline_run.run_id, namespace=helm_namespace
+        )
+        assert "PIPELINE_SUCCESS" in result, "no match, result: {}".format(result)
+
+    def test_launch_failing_run(
+        self,
+        # From conftest
+        dagster_instance,
+        helm_namespace,
+        # From class fixture
+        pipeline_name,
+    ):
+        invalid_run_config = {"blah blah this is wrong": {}}
+
+        # Create pipeline run
+        external_pipeline = get_test_project_external_pipeline(pipeline_name)
+        pipeline_run = create_run_for_test(
+            dagster_instance,
+            pipeline_name=pipeline_name,
+            run_config=invalid_run_config,
+            mode="default",
+        )
+
+        # Launch pipeline
+        reoriginated_external_pipeline = ReOriginatedExternalPipelineForTest(external_pipeline)
+        dagster_instance.launch_run(pipeline_run.run_id, reoriginated_external_pipeline)
+
+        # Verify run was successful
+        result = wait_for_job_and_get_raw_logs(
+            job_name="dagster-run-%s" % pipeline_run.run_id, namespace=helm_namespace
+        )
+
+        assert "PIPELINE_SUCCESS" not in result, "no match, result: {}".format(result)
