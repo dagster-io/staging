dc5aa38a36a8f8fa1b9f7a562ffb8d56680fdd1a
catherinewu <cat@elementl.com>
2020-09-27 16:49:05 +0000

    pipeline termination event
    Test Plan: todo
    
    Differential Revision: https://dagster.phacility.com/D4567

--- python_modules/dagster/dagster/core/launcher/grpc_run_launcher.py
+++ python_modules/dagster/dagster/core/launcher/grpc_run_launcher.py
@@ -148,14 +148,26 @@ class GrpcRunLauncher(RunLauncher, ConfigurableClass):
 
     def terminate(self, run_id):
         check.str_param(run_id, "run_id")
+        if not self._instance:
+            return False
+
+        run = self._instance.get_run_by_id(run_id)
+        if not run:
+            return False
+
+        self._instance.report_engine_event(
+            message="Received pipeline termination request.", pipeline_run=run,
+        )
 
         client = self._get_grpc_client_for_termination(run_id)
 
         if not client:
+            self._instance.report_engine_event(
+                message="Unable to send termination request to grpc server.", pipeline_run=run,
+            )
             return False
 
         res = client.cancel_execution(CancelExecutionRequest(run_id=run_id))
-
         return res.success
 
     def join(self, timeout=30):
