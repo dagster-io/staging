d9efa12fb79533389aafffd2606872d5e1fa6717
dpeng817 <cdguitar817@gmail.com>
2020-10-01 14:57:22 -0400

    Enabled hydration of step inputs from address when address exists. Added test boilerplate for entire memoized dev loop.
    Summary:
    changed address to addresses (dict mapping step output handles to address)
    
    added functionality to hydrate input from cached output
    
    functionality for hydrating input
    
    address -> addresses
    
    added build_for_memoized_plan and test scaffolding
    
    added step input creation logic to build_memoized_plan
    
    revised implementation of build_memoized_plan
    
    changed resolved_unmemoized_steps to resolve_memoized_execution_plan
    
    implemented input hydration.
    
    test for memoized dev loop
    
    moved tests to own file. Using persistent storage for test.
    
    basic memoized development testing pipeline.
    
    test for full dev loop working.
    
    linted
    
    Test Plan: Added a new CLI test.
    
    Differential Revision: https://dagster.phacility.com/D4659
    
    forgot to get rid of old test
    
    fixed path

---
+++ python_modules/dagster/dagster_tests/cli_tests/command_tests/test_memoized_development_cli.py
@@ -0,0 +1,40 @@
+from dagster import seven
+from dagster.cli.pipeline import execute_execute_command
+from dagster.core.instance import DagsterInstance, InstanceType
+from dagster.core.launcher import CliApiRunLauncher
+from dagster.core.storage.event_log import ConsolidatedSqliteEventLogStorage
+from dagster.core.storage.local_compute_log_manager import LocalComputeLogManager
+from dagster.core.storage.root import LocalArtifactStorage
+from dagster.core.storage.runs import SqliteRunStorage
+
+
+def test_execute_versioned_command():
+    with seven.TemporaryDirectory() as temp_dir:
+        run_store = SqliteRunStorage.from_local(temp_dir)
+        event_store = ConsolidatedSqliteEventLogStorage(temp_dir)
+        compute_log_manager = LocalComputeLogManager(temp_dir)
+        instance = DagsterInstance(
+            instance_type=InstanceType.PERSISTENT,
+            local_artifact_storage=LocalArtifactStorage(temp_dir),
+            run_storage=run_store,
+            event_storage=event_store,
+            compute_log_manager=compute_log_manager,
+            run_launcher=CliApiRunLauncher(),
+        )
+
+        kwargs = {
+            "config": (
+                "python_modules/dagster/dagster_tests/cli_tests/command_tests/basic_pipeline_config_1.yaml",
+            ),
+            "pipeline": "basic_pipeline",
+            "python_file": "dagster_tests/cli_tests/command_tests/memoized_development_basic_pipelines.py",
+            "tags": '{"dagster/is_memoized_run": "true"}',
+        }
+
+        result = execute_execute_command(kwargs=kwargs, instance=instance)
+        assert result.success
+
+        kwargs["config"] = ("dagster_tests/cli_tests/command_tests/basic_pipeline_config_2.yaml",)
+
+        result2 = execute_execute_command(kwargs=kwargs, instance=instance)
+        assert result2.success
