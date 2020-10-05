88dc6156a3fa641b9edcb80b6dbfcacaa1fa5e56
Rex Ledesma <rex@elementl.com>
2020-10-05 16:54:18 -0700

    Add liveness probe to extra celery workers
    Summary: As the title.
    
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D4674

--- integration_tests/python_modules/dagster-k8s-test-infra/dagster_k8s_test_infra/helm.py
+++ integration_tests/python_modules/dagster-k8s-test-infra/dagster_k8s_test_infra/helm.py
@@ -179,6 +179,16 @@ def _helm_chart_helper(namespace, should_cleanup, helm_config):
         print("Waiting for celery workers")
         pods = kubernetes.client.CoreV1Api().list_namespaced_pod(namespace=namespace)
         pod_names = [p.metadata.name for p in pods.items if "celery-workers" in p.metadata.name]
+
+        celery_config = helm_config.get("celery", {})
+        num_celery_workers = celery_config.get("replicaCount", 0) + sum(
+            queue.replicaCount
+            for queue in celery_config.get("extraWorkerQueues", [])
+            if queue.replicaCount
+        )
+
+        assert num_celery_workers == len(pod_names)
+
         for pod_name in pod_names:
             print("Waiting for Celery worker pod %s" % pod_name)
             wait_for_pod(pod_name, namespace=namespace)
@@ -248,8 +258,7 @@ def helm_chart(namespace, docker_image, should_cleanup=True):
         },
         "celery": {
             "image": {"repository": repository, "tag": tag, "pullPolicy": pull_policy},
-            # https://github.com/dagster-io/dagster/issues/2671
-            # 'extraWorkerQueues': [{'name': 'extra-queue-1', 'replicaCount': 1},],
+            "extraWorkerQueues": [{"name": "extra-queue-1", "replicaCount": 1},],
             "livenessProbe": {
                 "initialDelaySeconds": 15,
                 "periodSeconds": 10,
