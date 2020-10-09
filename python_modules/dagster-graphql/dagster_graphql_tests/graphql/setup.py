a77937baca2540dde32d8b3b52b5434b1dfc3e16
gibsondan <daniel@elementl.com>
2020-10-09 10:35:22 -0400

    Remove legacy repo format from examples and loading path
    Summary:
    A whole bunch of our tests are using a format that goes through a deprecated loading process that's different than what we expect our users to use. This caused an issue in prod yesterday where dagster_docker_celery and certain dagster_airflow just stopped working.
    
    This diff removes that loading path (technically a breaking change, but dagster has been prompting you to update since 0.8.0) and replaces all the test yamls that were using it (other than ones specifically designed to test the legacy path). Lots of tests will probably start breaking here.
    
    Test Plan: BK+Azure
    
    Differential Revision: https://dagster.phacility.com/D4742

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/setup.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/setup.py
@@ -6,7 +6,6 @@ import time
 from collections import OrderedDict
 from copy import deepcopy
 
-from dagster_graphql.implementation.context import DagsterGraphQLContext
 from dagster_graphql.test.utils import (
     define_context_for_file,
     define_out_of_process_context,
@@ -52,11 +51,12 @@ from dagster import (
     usable_as_dagster_type,
     weekly_schedule,
 )
-from dagster.cli.workspace import Workspace
+from dagster.cli.workspace.load import location_handle_from_python_file
 from dagster.core.definitions.decorators import executable
 from dagster.core.definitions.partition import last_empty_partition
 from dagster.core.definitions.reconstructable import ReconstructableRepository
-from dagster.core.host_representation import InProcessRepositoryLocation, RepositoryLocationHandle
+from dagster.core.host_representation import RepositoryLocation
+from dagster.core.host_representation.handle import python_user_process_api_from_instance
 from dagster.core.log_manager import coerce_valid_log_level
 from dagster.core.storage.tags import RESUME_RETRY_TAG
 from dagster.utils import file_relative_path, segfault
@@ -100,25 +100,20 @@ def create_main_recon_repo():
     return ReconstructableRepository.for_file(__file__, "test_repo")
 
 
-def get_main_external_repo():
-    return InProcessRepositoryLocation(
-        ReconstructableRepository.from_legacy_repository_yaml(
-            file_relative_path(__file__, "repo.yaml")
-        ),
+def get_main_external_repo(instance):
+    return RepositoryLocation.from_handle(
+        location_handle_from_python_file(
+            python_file=file_relative_path(__file__, "setup.py"),
+            attribute="test_repo",
+            working_directory=None,
+            user_process_api=python_user_process_api_from_instance(instance),
+            location_name=main_repo_location_name(),
+        )
     ).get_repository("test_repo")
 
 
-def define_test_snapshot_context():
-    return DagsterGraphQLContext(
-        instance=DagsterInstance.ephemeral(),
-        workspace=Workspace(
-            [RepositoryLocationHandle.create_in_process_location(create_main_recon_repo().pointer)]
-        ),
-    )
-
-
 def main_repo_location_name():
-    return "<<in_process>>"
+    return "graphql_test_repo_location"
 
 
 def main_repo_name():
