53803ebaf0069fc61c79b96c1593b13dd8cc65ed
Johann Miller <johann@elementl.com>
2020-10-12 10:18:36 -0400

    Add postgres DB container to celery docker test, fix api call escaping
    Summary:
    This adds a postgres DB link to the test so that the user process can load the instance in the test. So far it just passes locally, looking into BK passing now.
    
    Test Plan: BK
    
    Reviewers: johann, alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D4752

--- integration_tests/python_modules/dagster-k8s-test-infra/dagster_k8s_test_infra/integration_utils.py
+++ integration_tests/python_modules/dagster-k8s-test-infra/dagster_k8s_test_infra/integration_utils.py
@@ -4,11 +4,7 @@ import subprocess
 
 import six
 
-from dagster.core.code_pointer import FileCodePointer
-from dagster.core.host_representation import ExternalPipeline
-from dagster.core.origin import PipelinePythonOrigin, RepositoryPythonOrigin
-
-IS_BUILDKITE = os.getenv('BUILDKITE') is not None
+IS_BUILDKITE = os.getenv("BUILDKITE") is not None
 
 
 def image_pull_policy():
@@ -17,9 +13,9 @@ def image_pull_policy():
     # registry / pollute the private registry / set up and network a local registry as a condition
     # of running tests
     if IS_BUILDKITE:
-        return 'Always'
+        return "Always"
     else:
-        return 'IfNotPresent'
+        return "IfNotPresent"
 
 
 def check_output(*args, **kwargs):
@@ -31,7 +27,7 @@ def check_output(*args, **kwargs):
 
 
 def which_(exe):
-    '''Uses distutils to look for an executable, mimicking unix which'''
+    """Uses distutils to look for an executable, mimicking unix which"""
     from distutils import spawn  # pylint: disable=no-name-in-module
 
     # https://github.com/PyCQA/pylint/issues/73
@@ -40,27 +36,27 @@ def which_(exe):
 
 def get_test_namespace():
     namespace_suffix = hex(random.randint(0, 16 ** 6))[2:]
-    return 'dagster-test-%s' % namespace_suffix
+    return "dagster-test-%s" % namespace_suffix
 
 
 def within_docker():
-    '''detect if we're running inside of a docker container
+    """detect if we're running inside of a docker container
 
     from: https://stackoverflow.com/a/48710609/11295366
-    '''
-    cgroup_path = '/proc/self/cgroup'
+    """
+    cgroup_path = "/proc/self/cgroup"
     return (
-        os.path.exists('/.dockerenv')
+        os.path.exists("/.dockerenv")
         or os.path.isfile(cgroup_path)
-        and any('docker' in line for line in open(cgroup_path))
+        and any("docker" in line for line in open(cgroup_path))
     )
 
 
 def remove_none_recursively(obj):
-    '''Remove none values from a dict. This is used here to support comparing provided config vs.
+    """Remove none values from a dict. This is used here to support comparing provided config vs.
     config we retrive from kubernetes, which returns all fields, even those which have no value
     configured.
-    '''
+    """
     if isinstance(obj, (list, tuple, set)):
         return type(obj)(remove_none_recursively(x) for x in obj if x is not None)
     elif isinstance(obj, dict):
@@ -71,31 +67,3 @@ def remove_none_recursively(obj):
         )
     else:
         return obj
-
-
-class ReOriginatedExternalPipelineForTest(ExternalPipeline):
-    def __init__(
-        self, external_pipeline,
-    ):
-        super(ReOriginatedExternalPipelineForTest, self).__init__(
-            external_pipeline.external_pipeline_data, external_pipeline.repository_handle,
-        )
-
-    def get_origin(self):
-        '''
-        Hack! Inject origin that the k8s images will use. The BK image uses a different directory
-        structure (/workdir/python_modules/dagster-test/dagster_test/test_project) than the images
-        inside the kind cluster (/dagster_test/test_project). As a result the normal origin won't
-        work, we need to inject this one.
-        '''
-
-        return PipelinePythonOrigin(
-            self._pipeline_index.name,
-            RepositoryPythonOrigin(
-                executable_path='python',
-                code_pointer=FileCodePointer(
-                    '/dagster_test/test_project/test_pipelines/repo.py',
-                    'define_demo_execution_repo',
-                ),
-            ),
-        )
