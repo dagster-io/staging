bac05d702edc4692c11deef3ab1cb0c96af76f5a
Johann Miller <johann@elementl.com>
2020-10-12 11:13:37 -0400

    modify execute_step args
    Summary: Currently passing args that can be gathered from the pipeline_run
    
    Test Plan: integration
    
    Differential Revision: https://dagster.phacility.com/D4754

--- python_modules/dagster/dagster/grpc/types.py
+++ python_modules/dagster/dagster/grpc/types.py
@@ -53,17 +53,15 @@ class ExecuteRunArgs(namedtuple("_ExecuteRunArgs", "pipeline_origin pipeline_run
 class ExecuteStepArgs(
     namedtuple(
         "_ExecuteStepArgs",
-        "pipeline_origin pipeline_run_id instance_ref mode step_keys_to_execute run_config retries_dict",
+        "pipeline_origin pipeline_run_id step_keys_to_execute instance_ref retries_dict",
     )
 ):
     def __new__(
         cls,
         pipeline_origin,
         pipeline_run_id,
+        step_keys_to_execute,
         instance_ref=None,
-        mode=None,
-        step_keys_to_execute=None,
-        run_config=None,
         retries_dict=None,
     ):
         return super(ExecuteStepArgs, cls).__new__(
@@ -72,12 +70,10 @@ class ExecuteStepArgs(
                 pipeline_origin, "pipeline_origin", PipelinePythonOrigin
             ),
             pipeline_run_id=check.str_param(pipeline_run_id, "pipeline_run_id"),
-            instance_ref=check.opt_inst_param(instance_ref, "instance_ref", InstanceRef),
-            mode=check.opt_str_param(mode, "mode"),
             step_keys_to_execute=check.opt_list_param(
                 step_keys_to_execute, "step_keys_to_execute", of_type=str
             ),
-            run_config=check.opt_dict_param(run_config, "run_config"),
+            instance_ref=check.opt_inst_param(instance_ref, "instance_ref", InstanceRef),
             retries_dict=check.opt_dict_param(retries_dict, "retries_dict"),
         )
 
