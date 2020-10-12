763bddabe0029b104789893cab7497dd8f9a4002
prha <prha@elementl.com>
2020-10-12 18:48:27 -0400

    add summary table for asset keys
    Test Plan: back compat test
    
    Differential Revision: https://dagster.phacility.com/D4764

--- python_modules/dagster/dagster/core/storage/event_log/migration.py
+++ python_modules/dagster/dagster/core/storage/event_log/migration.py
@@ -1,4 +1,12 @@
-from dagster.core.storage.event_log.sql_event_log import SqlEventLogStorage
+import sqlalchemy as db
+
+from dagster.core.storage.event_log.sql_event_log import (
+    SECONDARY_INDEX_ASSET_KEY,
+    AssetAwareSqlEventLogStorage,
+    SqlEventLogStorage,
+)
+
+from .schema import AssetKeyTable, SqlEventLogStorageTable
 
 
 def migrate_event_log_data(instance=None):
@@ -16,3 +24,34 @@ def migrate_event_log_data(instance=None):
         event_records_by_id = event_log_storage.get_logs_for_run_by_log_id(run.run_id)
         for record_id, event in event_records_by_id.items():
             event_log_storage.update_event_log_record(record_id, event)
+
+
+def migrate_asset_key_data(instance=None):
+    """
+    Utility method to migrate the data in the existing event log records.  Reads every event log row
+    reachable from the instance and reserializes it to event log storage.  Deserializing and then
+    reserializing the event from storage allows for things like SQL column extraction, filling
+    explicit default values, etc.
+    """
+    event_log_storage = instance._event_storage  # pylint: disable=protected-access
+    if not isinstance(event_log_storage, AssetAwareSqlEventLogStorage):
+        return
+
+    query = (
+        db.select([SqlEventLogStorageTable.c.asset_key])
+        .where(SqlEventLogStorageTable.c.asset_key != None)
+        .group_by(SqlEventLogStorageTable.c.asset_key)
+    )
+    with event_log_storage.connect() as conn:
+        to_insert = conn.execute(query).fetchall()
+        for (asset_key,) in to_insert:
+            try:
+                conn.execute(
+                    AssetKeyTable.insert().values(  # pylint: disable=no-value-for-parameter
+                        asset_key=asset_key
+                    )
+                )
+            except db.exc.IntegrityError:
+                # asset key already present
+                pass
+    event_log_storage.mark_summary_data_complete(SECONDARY_INDEX_ASSET_KEY)
