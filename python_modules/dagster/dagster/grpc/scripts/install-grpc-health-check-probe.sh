606e38eb8dd7ee733a0d25a51769aa770ea683b2
Johann Miller <johann@elementl.com>
2020-10-14 10:55:46 -0400

    Add grpc-health-probe
    Test Plan: Integrations
    
    Differential Revision: https://dagster.phacility.com/D4776

---
+++ python_modules/dagster/dagster/grpc/scripts/install-grpc-health-check-probe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+GRPC_HEALTH_PROBE_VERSION=v0.2.1 &&
+wget -qO/bin/grpc_health_probe https://github.com/grpc-ecosystem/grpc-health-probe/releases/download/${GRPC_HEALTH_PROBE_VERSION}/grpc_health_probe-linux-amd64 &&
+chmod +x /bin/grpc_health_probe
