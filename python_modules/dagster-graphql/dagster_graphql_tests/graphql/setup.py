3fa3a73038aa5631c9ad9a1445767186792d9788
gibsondan <daniel@elementl.com>
2020-10-15 09:03:40 -0400

    Require that start_date be on a month/day boundary for monthly/daily schedules
    Summary: Right now the start date determines what time gets passed in as a partition value to the run config, which might not be what people expect (like if you start a monthly schedule on the 5th of each month, we pass in a partition with the name as the month and the value as the 5th of each month). It also makes it harder to think about timezones and ensuring that we can map from tick time to partition time, although I don't *think* there's actually any bugs here.
    
    Test Plan: New BK coverage
    
    Reviewers: sashank, alangenfeld, prha, schrockn
    
    Differential Revision: https://dagster.phacility.com/D4787

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/setup.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/setup.py
@@ -59,6 +59,7 @@ from dagster.core.definitions.reconstructable import ReconstructableRepository
 from dagster.core.host_representation import InProcessRepositoryLocation, RepositoryLocationHandle
 from dagster.core.log_manager import coerce_valid_log_level
 from dagster.core.storage.tags import RESUME_RETRY_TAG
+from dagster.core.test_utils import today_at_midnight
 from dagster.utils import file_relative_path, segfault
 
 
@@ -870,7 +871,7 @@ def define_schedules():
 
     @daily_schedule(
         pipeline_name="no_config_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+        start_date=today_at_midnight() - datetime.timedelta(days=1),
         execution_time=(datetime.datetime.now() + datetime.timedelta(hours=2)).time(),
     )
     def partition_based_decorator(_date):
@@ -878,7 +879,7 @@ def define_schedules():
 
     @daily_schedule(
         pipeline_name="multi_mode_with_loggers",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+        start_date=today_at_midnight() - datetime.timedelta(days=1),
         execution_time=(datetime.datetime.now() + datetime.timedelta(hours=2)).time(),
         mode="foo_mode",
     )
@@ -887,7 +888,7 @@ def define_schedules():
 
     @hourly_schedule(
         pipeline_name="no_config_chain_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+        start_date=today_at_midnight() - datetime.timedelta(days=1),
         execution_time=(datetime.datetime.now() + datetime.timedelta(hours=2)).time(),
         solid_selection=["return_foo"],
     )
@@ -896,7 +897,7 @@ def define_schedules():
 
     @daily_schedule(
         pipeline_name="no_config_chain_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=2),
+        start_date=today_at_midnight() - datetime.timedelta(days=2),
         execution_time=(datetime.datetime.now() + datetime.timedelta(hours=3)).time(),
         solid_selection=["return_foo"],
     )
@@ -905,7 +906,7 @@ def define_schedules():
 
     @monthly_schedule(
         pipeline_name="no_config_chain_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=100),
+        start_date=(today_at_midnight() - datetime.timedelta(days=100)).replace(day=1),
         execution_time=(datetime.datetime.now() + datetime.timedelta(hours=4)).time(),
         solid_selection=["return_foo"],
     )
@@ -914,7 +915,7 @@ def define_schedules():
 
     @weekly_schedule(
         pipeline_name="no_config_chain_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=50),
+        start_date=today_at_midnight() - datetime.timedelta(days=50),
         execution_time=(datetime.datetime.now() + datetime.timedelta(hours=5)).time(),
         solid_selection=["return_foo"],
     )
@@ -924,7 +925,7 @@ def define_schedules():
     # Schedules for testing the user error boundary
     @daily_schedule(
         pipeline_name="no_config_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+        start_date=today_at_midnight() - datetime.timedelta(days=1),
         should_execute=lambda _: asdf,  # pylint: disable=undefined-variable
     )
     def should_execute_error_schedule(_date):
@@ -932,7 +933,7 @@ def define_schedules():
 
     @daily_schedule(
         pipeline_name="no_config_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+        start_date=today_at_midnight() - datetime.timedelta(days=1),
         tags_fn_for_date=lambda _: asdf,  # pylint: disable=undefined-variable
     )
     def tags_error_schedule(_date):
@@ -940,7 +941,7 @@ def define_schedules():
 
     @daily_schedule(
         pipeline_name="no_config_pipeline",
-        start_date=datetime.datetime.now() - datetime.timedelta(days=1),
+        start_date=today_at_midnight() - datetime.timedelta(days=1),
     )
     def run_config_error_schedule(_date):
         return asdf  # pylint: disable=undefined-variable
