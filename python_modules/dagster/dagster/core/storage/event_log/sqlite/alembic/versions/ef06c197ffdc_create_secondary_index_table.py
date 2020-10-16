d6b94ff65dd7c612079e295da3192e6e8e717b82
prha <prha@elementl.com>
2020-10-16 09:22:34 -0400

    RFC: Add table to track migration of secondary indexes on the event_log table
    Summary:
    For SQL-event log implementations, adds a check for event_log migration status.
    The instance implementations fronts those migration status queries with an in-memory cache.
    
    This does mean that dagit would need to be restarted to pick up any migration change, but
    seems worth it.
    
    Test Plan: BK
    
    Differential Revision: https://dagster.phacility.com/D4800

---
+++ python_modules/dagster/dagster/core/storage/event_log/sqlite/alembic/versions/ef06c197ffdc_create_secondary_index_table.py
@@ -0,0 +1,41 @@
+"""create_secondary_index_table
+
+Revision ID: ef06c197ffdc
+Revises: c34498c29964
+Create Date: 2020-10-16 09:35:25.369956
+
+"""
+import sqlalchemy as db
+from alembic import op
+from sqlalchemy.engine import reflection
+
+# pylint: disable=no-member
+
+# revision identifiers, used by Alembic.
+revision = "ef06c197ffdc"
+down_revision = "c34498c29964"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+    if "event_logs" in has_tables and "event_log_secondary_indexes" not in has_tables:
+        # create table
+        op.create_table(
+            "event_log_secondary_indexes",
+            db.Column("id", db.Integer, primary_key=True, autoincrement=True),
+            db.Column("name", db.String, unique=True),
+            db.Column("create_timestamp", db.DateTime, server_default=db.text("CURRENT_TIMESTAMP")),
+            db.Column("migration_completed", db.DateTime),
+        )
+
+
+def downgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+    if "event_logs" in has_tables and "event_log_secondary_indexes" in has_tables:
+        op.drop_table("event_log_secondary_indexes")
---
+++ python_modules/libraries/dagster-postgres/dagster_postgres/event_log/alembic/versions/6c0d40c3d559_create_secondary_index_table.py
@@ -0,0 +1,41 @@
+"""create_secondary_index_table
+
+Revision ID: 6c0d40c3d559
+Revises: 07f83cc13695
+Create Date: 2020-10-16 09:41:03.068648
+
+"""
+import sqlalchemy as db
+from alembic import op
+from sqlalchemy.engine import reflection
+
+# pylint: disable=no-member
+
+# revision identifiers, used by Alembic.
+revision = "6c0d40c3d559"
+down_revision = "07f83cc13695"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+    if "event_logs" in has_tables and "event_log_secondary_indexes" not in has_tables:
+        # create table
+        op.create_table(
+            "event_log_secondary_indexes",
+            db.Column("id", db.Integer, primary_key=True, autoincrement=True),
+            db.Column("name", db.String, unique=True),
+            db.Column("create_timestamp", db.DateTime, server_default=db.text("CURRENT_TIMESTAMP")),
+            db.Column("migration_completed", db.DateTime),
+        )
+
+
+def downgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+    if "event_logs" in has_tables and "event_log_secondary_indexes" in has_tables:
+        op.drop_table("event_log_secondary_indexes")
