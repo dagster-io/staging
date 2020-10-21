aa112cfdb95cbd3a622731ed2701b35cfe1ee253
gibsondan <daniel@elementl.com>
2020-10-21 14:29:30 -0400

    Clean up in ComputeLogManager on instance cleanup
    Summary:
    Best I can tell from https://buildkite.com/dagster/dagster/builds/14878#ddfa5e35-46ae-40f9-99d0-222a1dfb203b
    
    is that there's an issue during teardown:
    "Unhandled exception in thread started by <bound method PollingObserver.__bootstrap of <PollingObserver(Thread-82, stopped daemon 139963773277952)>>"
    
    This is an attempt to address that by adding a stop() call to the cleanup path.
    
    Test Plan: BK and pray
    
    Reviewers: alangenfeld

--- python_modules/dagster/dagster/core/instance/__init__.py
+++ python_modules/dagster/dagster/core/instance/__init__.py
@@ -455,6 +455,7 @@ class DagsterInstance:
         self._run_storage.dispose()
         self._run_launcher.dispose()
         self._event_storage.dispose()
+        self._compute_log_manager.dispose()
 
     # run storage
 
