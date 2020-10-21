b3f89f0048b37782aedd24011a78de68d70938f2
Rex Ledesma <rex@elementl.com>
2020-10-21 15:31:03 -0700

    Support slackclient 2.x when the python version is 3.x
    Summary:
    As the title. We do a conditional import on the slackclient module depending on the python version.
    
    Addresses https://github.com/dagster-io/dagster/issues/3058
    
    Test Plan: unit, BK
    
    Reviewers: max, sashank
    
    Subscribers: schrockn
    
    Differential Revision: https://dagster.phacility.com/D4797

--- python_modules/libraries/dagster-slack/setup.py
+++ python_modules/libraries/dagster-slack/setup.py
@@ -19,7 +19,6 @@ if __name__ == "__main__":
         description="A Slack client resource for posting to Slack",
         url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-slack",
         classifiers=[
-            "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "License :: OSI Approved :: Apache Software License",
@@ -28,9 +27,9 @@ if __name__ == "__main__":
         packages=find_packages(exclude=["test"]),
         install_requires=[
             "dagster",
-            # Slack 2.x does not support Python 2
+            # Note: Slack 2.x does not support Python 2
             # https://github.com/slackapi/python-slackclient/wiki/Migrating-to-2.x#minimum-python-versions
-            "slackclient<2.0.0",
+            "slackclient>=2,<3",
         ],
         zip_safe=False,
     )
