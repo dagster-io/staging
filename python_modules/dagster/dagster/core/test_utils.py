9c80a5455bf274c8c8184bb52efd43e41951b31d
Johann Miller <johann@elementl.com>
2020-10-21 20:03:56 -0400

    add pipeline_origin to pipeline run
    Test Plan: existing
    
    Differential Revision: https://dagster.phacility.com/D4861

--- python_modules/dagster/dagster/core/test_utils.py
+++ python_modules/dagster/dagster/core/test_utils.py
@@ -133,6 +133,7 @@ def create_run_for_test(
     pipeline_snapshot=None,
     execution_plan_snapshot=None,
     parent_pipeline_snapshot=None,
+    pipeline_origin=None,
 ):
     return instance.create_run(
         pipeline_name,
@@ -148,6 +149,7 @@ def create_run_for_test(
         pipeline_snapshot,
         execution_plan_snapshot,
         parent_pipeline_snapshot,
+        pipeline_origin=pipeline_origin,
     )
 
 
