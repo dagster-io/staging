da33d15acada41b6104e0f2d352a9f9dbbaae45c
gibsondan <daniel@elementl.com>
2020-10-22 17:16:03 -0400

    Temporarily disable hanging windows tests
    Summary: This test is inexplicably hanging on windows since we switched to gRPC, something about trying to load a non-existant module is causing it to hang. Temporarily disable it while we investigate.
    
    Test Plan: BK+Azure
    
    Reviewers: max, alangenfeld, schrockn, prha
    
    Differential Revision: https://dagster.phacility.com/D4874

--- python_modules/dagster/dagster/api/utils.py
+++ python_modules/dagster/dagster/api/utils.py
@@ -7,12 +7,13 @@ from dagster.utils.temp_file import get_temp_file_name
 
 def execute_command_in_subprocess(parts):
     check.list_param(parts, "parts", of_type=str)
-    try:
-        subprocess.check_output(parts, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
+    process = subprocess.Popen(parts, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    stdout, _stderr = process.communicate()
+
+    if process.returncode != 0:
         raise DagsterIPCProtocolError(
             "Error when executing API command {cmd}: {output}".format(
-                cmd=e.cmd, output=e.output.decode("utf-8")
+                cmd=" ".join(parts), output=stdout.decode("utf-8")
             )
         )
 
