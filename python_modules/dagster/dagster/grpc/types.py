1964f6952c757c31ebc044e3c767b28f87c72f33
prha <prha@elementl.com>
2020-10-22 11:10:18 -0400

    [sensors-2] sensor def
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D5019

--- python_modules/dagster/dagster/grpc/types.py
+++ python_modules/dagster/dagster/grpc/types.py
@@ -265,6 +265,24 @@ class ExternalScheduleExecutionArgs(
 
 
 @whitelist_for_serdes
+class SensorExecutionArgs(
+    namedtuple(
+        "_SensorExecutionArgs", "repository_origin instance_ref sensor_name last_checked_time"
+    )
+):
+    def __new__(cls, repository_origin, instance_ref, sensor_name, last_checked_time):
+        return super(SensorExecutionArgs, cls).__new__(
+            cls,
+            repository_origin=check.inst_param(
+                repository_origin, "repository_origin", ExternalRepositoryOrigin
+            ),
+            instance_ref=check.inst_param(instance_ref, "instance_ref", InstanceRef),
+            sensor_name=check.str_param(sensor_name, "sensor_name"),
+            last_checked_time=check.opt_float_param(last_checked_time, "last_checked_time"),
+        )
+
+
+@whitelist_for_serdes
 class ExternalJobArgs(namedtuple("_ExternalJobArgs", "repository_origin instance_ref name",)):
     def __new__(cls, repository_origin, instance_ref, name):
         return super(ExternalJobArgs, cls).__new__(
