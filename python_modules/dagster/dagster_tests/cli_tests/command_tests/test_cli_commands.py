57d9c086a59423b3b771744acf011c860002a005
Johann Miller <johann@elementl.com>
2020-10-23 19:29:43 -0400

    [Run-queue-3] Default run queuer
    Summary: Add a run queuer that simply wraps the run launcher. This will allow instance methods to go through the queuer, instead of conditionally using the queuer if enabled and the launcher otherwise.
    
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D4891

--- python_modules/dagster/dagster_tests/cli_tests/command_tests/test_cli_commands.py
+++ python_modules/dagster/dagster_tests/cli_tests/command_tests/test_cli_commands.py
@@ -149,47 +149,10 @@ def stderr_pipeline():
     fail()
 
 
-class InMemoryRunLauncher(RunLauncher, ConfigurableClass):
-    def __init__(self, inst_data=None):
-        self._inst_data = inst_data
-        self._queue = []
-
-    def launch_run(self, instance, run, external_pipeline):
-        check.inst_param(instance, "instance", DagsterInstance)
-        check.inst_param(run, "run", PipelineRun)
-        check.inst_param(external_pipeline, "external_pipeline", ExternalPipeline)
-        self._queue.append(run)
-        return run
-
-    def queue(self):
-        return self._queue
-
-    @classmethod
-    def config_type(cls):
-        return Shape({})
-
-    @classmethod
-    def from_config_value(cls, inst_data, config_value):
-        return cls(inst_data=inst_data,)
-
-    @property
-    def inst_data(self):
-        return self._inst_data
-
-    def can_terminate(self, run_id):
-        return False
-
-    def terminate(self, run_id):
-        check.not_implemented("Termintation not supported")
-
-
 @contextmanager
 def _default_cli_test_instance_tempdir(temp_dir, overrides=None):
     default_overrides = {
-        "run_launcher": {
-            "module": "dagster_tests.cli_tests.command_tests.test_cli_commands",
-            "class": "InMemoryRunLauncher",
-        }
+        "run_launcher": {"module": "dagster_tests.utils", "class": "InMemoryRunLauncher",}
     }
     with instance_for_test_tempdir(
         temp_dir, overrides=merge_dicts(default_overrides, (overrides if overrides else {}))
