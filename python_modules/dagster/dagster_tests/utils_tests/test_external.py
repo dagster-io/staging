4fc9bf6f2b786f54829045b3d4e22bdac879b347
Johann Miller <johann@elementl.com>
2020-10-23 19:29:43 -0400

    [Run-queue-3] Default run queuer
    Summary: Add a run queuer that simply wraps the run launcher. This will allow instance methods to go through the queuer, instead of conditionally using the queuer if enabled and the launcher otherwise.
    
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D4891

---
+++ python_modules/dagster/dagster_tests/utils_tests/test_external.py
@@ -0,0 +1,17 @@
+from dagster_tests.api_tests.utils import get_foo_pipeline_handle
+
+from dagster.core.test_utils import create_run_for_test, instance_for_test
+from dagster.utils.external import get_external_pipeline_from_run
+
+
+def test_get_external_pipeline_from_run():
+    with instance_for_test() as instance:
+        pipeline_handle = get_foo_pipeline_handle()
+        run = create_run_for_test(
+            instance,
+            pipeline_name=pipeline_handle.pipeline_name,
+            pipeline_origin=pipeline_handle.get_origin(),
+        )
+
+        external_pipeline = get_external_pipeline_from_run(instance, run)
+        assert external_pipeline.name == pipeline_handle.pipeline_name
