e33ef79216c64e16780737e1c6767ad144165589
Johann Miller <johann@elementl.com>
2020-10-23 19:29:43 -0400

    [Run-queue-3] Default run queuer
    Summary: Add a run queuer that simply wraps the run launcher. This will allow instance methods to go through the queuer, instead of conditionally using the queuer if enabled and the launcher otherwise.
    
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D4891

---
+++ python_modules/dagster/dagster_tests/core_tests/queuer_tests/test_instant_queuer.py
@@ -0,0 +1,40 @@
+import pytest
+from dagster_tests.api_tests.utils import get_foo_pipeline_handle
+
+from dagster.core.queuer.instant_queuer import InstantQueuer
+from dagster.core.test_utils import create_run_for_test, instance_for_test
+
+
+@pytest.fixture()
+def instance():
+    overrides = {"run_launcher": {"module": "dagster_tests.utils", "class": "InMemoryRunLauncher",}}
+    with instance_for_test(overrides=overrides) as inst:
+        yield inst
+
+
+@pytest.fixture()
+def queuer(instance):  # pylint: disable=redefined-outer-name
+
+    q = InstantQueuer()
+    q.initialize(instance)
+    yield q
+
+
+def create_run(instance, **kwargs):  # pylint: disable=redefined-outer-name
+
+    pipeline_handle = get_foo_pipeline_handle()
+    create_run_for_test(
+        instance=instance,
+        pipeline_origin=pipeline_handle.get_origin(),
+        pipeline_name="foo",
+        **kwargs
+    )
+
+
+def test_enqueue_run(instance, queuer):  # pylint: disable=redefined-outer-name
+
+    queuer.enqueue_run(
+        instance, run=create_run(instance, run_id="foo-1",),
+    )
+    assert len(instance.run_launcher.queue()) == 1
+    assert instance.run_launcher.queue()[0].run_id == "foo-1"
