23b030e0231cea65b6385d5df2dd980816d4be2b
Bob Chen <bob@elementl.com>
2020-10-26 16:21:20 -0400

    Adds AWS actions for k8s_worker security group.

--- mapbox-infra/terraform/security-groups.tf
+++ mapbox-infra/terraform/security-groups.tf
@@ -1,27 +1,27 @@
 
-# resource "aws_security_group" "k8s_workers" {
-#   name   = "${local.deployment_name}-k8s_workers"
-#   vpc_id = module.vpc.vpc_id
+resource "aws_security_group" "k8s_workers" {
+  name   = "${local.deployment_name}-k8s_workers"
+  vpc_id = module.vpc.vpc_id
 
-#   ingress {
-#     from_port = 22
-#     to_port   = 22
-#     protocol  = "tcp"
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
 
-#     cidr_blocks = [
-#       "10.0.0.0/8",
-#       "172.16.0.0/12",
-#       "192.168.0.0/16",
-#     ]
-#   }
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 # resource "aws_security_group" "pg_sec_group" {
