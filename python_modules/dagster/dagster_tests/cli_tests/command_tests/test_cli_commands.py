211646f0ae16a60ea0288cde9ea203ddce81a999
prha <prha@elementl.com>
2020-10-27 11:02:12 -0400

    [sensors-3] add sensor cli, stored state
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D5020

--- python_modules/dagster/dagster_tests/cli_tests/command_tests/test_cli_commands.py
+++ python_modules/dagster/dagster_tests/cli_tests/command_tests/test_cli_commands.py
@@ -22,6 +22,7 @@ from dagster import (
 from dagster.cli import ENV_PREFIX, cli
 from dagster.cli.pipeline import pipeline_execute_command
 from dagster.cli.run import run_list_command, run_wipe_command
+from dagster.core.definitions.sensor import SensorDefinition
 from dagster.core.test_utils import instance_for_test, instance_for_test_tempdir
 from dagster.core.types.loadable_target_origin import LoadableTargetOrigin
 from dagster.grpc.server import GrpcServerProcess
@@ -112,6 +113,14 @@ def define_bar_partitions():
     }
 
 
+def define_bar_sensors():
+    return {
+        "foo_sensor": SensorDefinition(
+            name="foo_sensor", pipeline_name="baz", should_execute=lambda _: True
+        )
+    }
+
+
 @repository
 def bar():
     return {
@@ -122,6 +131,7 @@ def bar():
         },
         "schedules": define_bar_schedules(),
         "partition_sets": define_bar_partitions(),
+        "jobs": define_bar_sensors(),
     }
 
 
@@ -287,13 +297,9 @@ def scheduler_instance(overrides=None):
             yield instance
 
 
-def managed_grpc_scheduler_instance():
-    return scheduler_instance(overrides={"opt_in": {"local_servers": True}})
-
-
 @contextmanager
-def grpc_server_scheduler_cli_args():
-    with scheduler_instance() as instance:
+def grpc_server_scheduler_cli_args(overrides=None):
+    with scheduler_instance(overrides) as instance:
         with grpc_server_bar_cli_args() as args:
             yield args, instance
 
@@ -305,14 +311,21 @@ def schedule_command_contexts():
         args_with_instance(
             scheduler_instance(), ["-w", file_relative_path(__file__, "workspace.yaml")]
         ),
-        pytest.param(
-            args_with_instance(
-                managed_grpc_scheduler_instance(),
-                ["-w", file_relative_path(__file__, "workspace.yaml")],
-            ),
-            marks=pytest.mark.managed_grpc,
-        ),
         pytest.param(grpc_server_scheduler_cli_args(), marks=pytest.mark.deployed_grpc),
+    ]
+
+
+def sensor_command_contexts():
+    command_line_scheduler_overrides = {
+        "scheduler": {"module": "dagster.core.scheduler", "class": "DagsterDaemonScheduler",}
+    }
+
+    return [
+        args_with_instance(
+            scheduler_instance(command_line_scheduler_overrides),
+            ["-w", file_relative_path(__file__, "workspace.yaml")],
+        ),
+        grpc_server_scheduler_cli_args(command_line_scheduler_overrides),
     ]
 
 
@@ -325,11 +338,7 @@ def backfill_command_contexts():
     }
     return [
         args_with_instance(default_cli_test_instance(), repo_args, True),
-        pytest.param(
-            args_with_instance(managed_grpc_instance(), repo_args, True),
-            marks=pytest.mark.managed_grpc,
-        ),
-        pytest.param(grpc_server_backfill_args(), marks=pytest.mark.deployed_grpc),
+        grpc_server_backfill_args(),
     ]
 
 
