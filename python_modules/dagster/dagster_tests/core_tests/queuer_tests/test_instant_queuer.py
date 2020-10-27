480399a3612325e8a7adf7aeeaed2853b1a0e599
Johann Miller <johann@elementl.com>
2020-10-27 11:16:34 -0400

    [run-queue-6] Dequer process
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D4917

--- python_modules/dagster/dagster_tests/core_tests/queuer_tests/test_instant_queuer.py
+++ python_modules/dagster/dagster_tests/core_tests/queuer_tests/test_instant_queuer.py
@@ -1,11 +1,11 @@
 import pytest
-from dagster_tests.api_tests.utils import get_foo_pipeline_handle
 
 from dagster.check import CheckError
 from dagster.core.queuer.instant_queuer import InstantRunQueuer
-from dagster.core.storage.pipeline_run import PipelineRun, PipelineRunStatus
+from dagster.core.storage.pipeline_run import PipelineRunStatus
 from dagster.core.test_utils import create_run_for_test, instance_for_test
-from dagster.utils import merge_dicts
+
+from .utils import new_pipeline_run
 
 
 @pytest.fixture()
@@ -25,17 +25,10 @@ def queuer(instance):  # pylint: disable=redefined-outer-name
     yield q
 
 
-def create_run(**kwargs):  # pylint: disable=redefined-outer-name
-
-    pipeline_handle = get_foo_pipeline_handle()
-    pipeline_args = merge_dicts(
-        {"pipeline_name": "foo", "pipeline_origin": pipeline_handle.get_origin(),}, kwargs,
-    )
-    return PipelineRun(**pipeline_args)
-
-
 def test_enqueue_run(instance, queuer):  # pylint: disable=redefined-outer-name
-    run = queuer.enqueue_run(instance, create_run(run_id="foo-1", status=PipelineRunStatus.QUEUED))
+    run = queuer.enqueue_run(
+        instance, new_pipeline_run(run_id="foo-1", status=PipelineRunStatus.QUEUED)
+    )
     assert run.run_id == "foo-1"
 
     assert len(instance.run_launcher.queue()) == 1
@@ -47,7 +40,7 @@ def test_enqueue_run(instance, queuer):  # pylint: disable=redefined-outer-name
 def test_enqueue_run_checks_status(instance, queuer):  # pylint: disable=redefined-outer-name
     with pytest.raises(CheckError):
         queuer.enqueue_run(
-            instance, create_run(run_id="foo-1", status=PipelineRunStatus.NOT_STARTED)
+            instance, new_pipeline_run(run_id="foo-1", status=PipelineRunStatus.NOT_STARTED)
         )
 
 
@@ -55,5 +48,5 @@ def test_enqueue_run_already_in_run_store(instance, queuer):  # pylint: disable=
     create_run_for_test(instance, run_id="foo-1")
     with pytest.raises(CheckError):
         queuer.enqueue_run(
-            instance, create_run(run_id="foo-1", status=PipelineRunStatus.NOT_STARTED)
+            instance, new_pipeline_run(run_id="foo-1", status=PipelineRunStatus.NOT_STARTED)
         )
