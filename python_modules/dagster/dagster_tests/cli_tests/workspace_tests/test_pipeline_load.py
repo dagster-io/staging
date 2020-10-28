a66cd6ac07e5bb5f6e126affbef023a0657b1af6
gibsondan <daniel@elementl.com>
2020-10-28 10:30:20 -0400

    kill the cli api
    Summary: A number of changes related to repo/pipeline/schedule origins become a lot simpler if the cli api goes away. This diff makes that happen - we can decide when we are comfortable doing this. Since 0.9.16 all users are no longer using the cli api by default, so this should be an invisible change to end users.
    
    Test Plan: BK + Azure
    
    Reviewers: alangenfeld, sashank, schrockn
    
    Differential Revision: https://dagster.phacility.com/D4935

--- python_modules/dagster/dagster_tests/cli_tests/workspace_tests/test_pipeline_load.py
+++ python_modules/dagster/dagster_tests/cli_tests/workspace_tests/test_pipeline_load.py
@@ -9,7 +9,6 @@ from dagster.cli.workspace.cli_target import (
     pipeline_target_argument,
 )
 from dagster.core.host_representation import ExternalPipeline
-from dagster.core.instance import DagsterInstance
 from dagster.utils import file_relative_path
 
 WINDOWS_PY36 = seven.IS_WINDOWS and sys.version_info[0] == 3 and sys.version_info[1] == 6
@@ -21,9 +20,7 @@ def load_pipeline_via_cli_runner(cli_args):
     @click.command(name="test_pipeline_command")
     @pipeline_target_argument
     def command(**kwargs):
-        with get_external_pipeline_from_kwargs(
-            kwargs, DagsterInstance.ephemeral()
-        ) as external_pipeline:
+        with get_external_pipeline_from_kwargs(kwargs) as external_pipeline:
             capture_result["external_pipeline"] = external_pipeline
 
     runner = CliRunner()
