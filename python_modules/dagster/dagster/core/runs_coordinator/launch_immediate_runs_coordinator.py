44bafba28ef942498fde79dbaf09cd9bcebbedee
Johann Miller <johann@elementl.com>
2020-10-29 13:14:25 -0400

    [run-queue-3] local run coordinator

---
+++ python_modules/dagster/dagster/core/runs_coordinator/launch_immediate_runs_coordinator.py
@@ -0,0 +1,27 @@
+from dagster import DagsterInstance, check
+from dagster.core.host_representation import ExternalPipeline
+from dagster.core.storage.pipeline_run import PipelineRun, PipelineRunStatus
+
+from .base import RunsCoordinator
+
+
+class LaunchImmediateRunsCoordinator(RunsCoordinator):
+    """Immediately send runs to the run launcher.
+    """
+
+    def initialize(self, instance):
+        check.inst_param(instance, "instance", DagsterInstance)
+
+    def submit_run(self, instance, pipeline_run, external_pipeline):
+        check.inst_param(instance, "instance", DagsterInstance)
+        check.inst_param(pipeline_run, "pipeline_run", PipelineRun)
+        check.inst_param(external_pipeline, "external_pipeline", ExternalPipeline)
+        check.invariant(pipeline_run.status == PipelineRunStatus.NOT_STARTED)
+
+        return instance.launch_run(pipeline_run.run_id, external_pipeline)
+
+    def can_cancel_run(self, run_id):
+        raise NotImplementedError()
+
+    def cancel_run(self, run_id):
+        raise NotImplementedError()
