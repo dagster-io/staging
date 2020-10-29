fc15a9a0de3d99a0ee6955d1bf18fe7fa10ac91d
dpeng817 <cdguitar817@gmail.com>
2020-10-29 11:51:39 -0400

    python typeddict type loader
    Summary: There didn't previously exist a DagsterTypeLoader for TypedDict. This creates one, and adds a test to `test_python_dict.py` to ensure proper functioning.
    
    Test Plan: Added `test_dict_type_loader` to tests.
    
    Differential Revision: https://dagster.phacility.com/D4952
    
    improved testing idiomatic-ness

--- python_modules/dagster/dagster_tests/core_tests/runtime_types_tests/test_python_dict.py
+++ python_modules/dagster/dagster_tests/core_tests/runtime_types_tests/test_python_dict.py
@@ -165,3 +165,18 @@ def test_complicated_dictionary_typing_fail():
 
     with pytest.raises(DagsterTypeCheckDidNotPass):
         execute_solid(emit_dict)
+
+
+def test_dict_type_loader():
+    test_input = {"hello": 5, "goodbye": 42}
+
+    @lambda_solid(input_defs=[InputDefinition("dict_input", dagster_type=typing.Dict[str, int])])
+    def emit_nothing(dict_input):
+        return dict_input
+
+    result = execute_solid(
+        emit_nothing,
+        run_config={"solids": {"emit_nothing": {"inputs": {"dict_input": test_input}}}},
+    )
+    assert result.success
+    assert result.output_value() == test_input
