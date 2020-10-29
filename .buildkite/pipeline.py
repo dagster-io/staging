06476e572d36a636fae4414e8d6379ec5dc90822
Bob Chen <bob@elementl.com>
2020-10-29 17:11:30 -0400

    Fix the pruned buildkite pipeline. 0

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -800,8 +800,7 @@ if __name__ == "__main__":
                     "CI_BRANCH": "$BUILDKITE_BRANCH",
                     "CI_PULL_REQUEST": "$BUILDKITE_PULL_REQUEST",
                 },
-                # "steps": all_steps,
-                "steps": python_steps(),
+                "steps": all_steps,
             },
             default_flow_style=False,
         )
