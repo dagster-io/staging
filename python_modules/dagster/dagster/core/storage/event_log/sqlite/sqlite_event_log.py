e238ce19366aeda134a5807e9cb572d5f3cd3a92
gibsondan <daniel@elementl.com>
2020-10-30 17:13:14 -0400

    Add retries to sqlite event log connection, ensure event log always tries to connect at least once for each run
    Summary: Tests are failing sporadically because there's a race where thread 1 starts creating an event log, creates the file, and then thread 2 comes in and skips creating the DB. This diff ensures that every thread tries to create the connection at least once.
    
    Also fix an issue where you might leave connect without a valid connection if there were concurrency issues.
    
    Test Plan: New version of concurrency tests passes under many trials
    
    Reviewers: max, nate, alangenfeld, prha
    
    Differential Revision: https://dagster.phacility.com/D4984

--- python_modules/dagster/dagster/core/storage/event_log/sqlite/sqlite_event_log.py
+++ python_modules/dagster/dagster/core/storage/event_log/sqlite/sqlite_event_log.py
@@ -2,6 +2,7 @@ import glob
 import logging
 import os
 import sqlite3
+import time
 from collections import defaultdict
 from contextlib import contextmanager
 
@@ -9,6 +10,7 @@ import sqlalchemy as db
 from dagster import StringSource, check
 from dagster.core.storage.pipeline_run import PipelineRunStatus
 from dagster.core.storage.sql import (
+    check_alembic_revision,
     create_engine,
     get_alembic_config,
     handle_schema_errors,
@@ -63,6 +65,9 @@ class SqliteEventLogStorage(SqlEventLogStorage, ConfigurableClass):
         self._obs.start()
         self._inst_data = check.opt_inst_param(inst_data, "inst_data", ConfigurableClassData)
 
+        # Used to ensure that each run ID attempts to initialize its DB the first time it connects
+        self._initialized_dbs = set()
+
     def upgrade(self):
         all_run_ids = self.get_all_run_ids()
         print(  # pylint: disable=print-call
@@ -74,6 +79,8 @@ class SqliteEventLogStorage(SqlEventLogStorage, ConfigurableClass):
         for run_id in tqdm(all_run_ids):
             with self.connect(run_id) as conn:
                 run_alembic_upgrade(alembic_config, conn, run_id)
+
+        self._initialized_dbs = set()
 
     @property
     def inst_data(self):
@@ -99,33 +106,50 @@ class SqliteEventLogStorage(SqlEventLogStorage, ConfigurableClass):
         return create_db_conn_string(self._base_dir, run_id)
 
     def _initdb(self, engine):
-
         alembic_config = get_alembic_config(__file__)
 
-        try:
-            SqlEventLogStorageMetadata.create_all(engine)
-            engine.execute("PRAGMA journal_mode=WAL;")
-            stamp_alembic_rev(alembic_config, engine)
-        except (db.exc.DatabaseError, sqlite3.DatabaseError, sqlite3.OperationalError) as exc:
-            # This is SQLite-specific handling for concurrency issues that can arise when, e.g.,
-            # the root nodes of a pipeline execute simultaneously on Airflow with SQLite storage
-            # configured and contend with each other to init the db. When we hit the following
-            # errors, we know that another process is on the case and it's safe to continue:
-            err_msg = str(exc)
-            if not (
-                "table asset_keys already exists" in err_msg
-                or "table secondary_indexes already exists" in err_msg
-                or "table event_logs already exists" in err_msg
-                or "database is locked" in err_msg
-                or "table alembic_version already exists" in err_msg
-                or "UNIQUE constraint failed: alembic_version.version_num" in err_msg
-            ):
-                raise
-            else:
-                logging.info(
-                    "SqliteEventLogStorage._initdb: Encountered apparent concurrent init, "
-                    "swallowing {str_exc}".format(str_exc=err_msg)
-                )
+        retry_limit = 20
+
+        while True:
+            try:
+                SqlEventLogStorageMetadata.create_all(engine)
+                engine.execute("PRAGMA journal_mode=WAL;")
+
+                connection = engine.connect()
+                db_revision, head_revision = check_alembic_revision(alembic_config, connection)
+
+                if not (db_revision and head_revision):
+                    stamp_alembic_rev(alembic_config, engine)
+
+                break
+            except (db.exc.DatabaseError, sqlite3.DatabaseError, sqlite3.OperationalError) as exc:
+                # This is SQLite-specific handling for concurrency issues that can arise when, e.g.,
+                # the root nodes of a pipeline execute simultaneously on Airflow with SQLite storage
+                # configured and contend with each other to init the db. When we hit the following
+                # errors, we know that another process is on the case and it's safe to continue:
+                err_msg = str(exc)
+
+                if not (
+                    "table asset_keys already exists" in err_msg
+                    or "table secondary_indexes already exists" in err_msg
+                    or "table event_logs already exists" in err_msg
+                    or "database is locked" in err_msg
+                    or "table alembic_version already exists" in err_msg
+                    or "UNIQUE constraint failed: alembic_version.version_num" in err_msg
+                ):
+                    raise
+
+                if retry_limit == 0:
+                    raise
+                else:
+                    logging.info(
+                        "SqliteEventLogStorage._initdb: Encountered apparent concurrent init, "
+                        "retrying ({retry_limit} retries left). Exception: {str_exc}".format(
+                            retry_limit=retry_limit, str_exc=err_msg
+                        )
+                    )
+                    time.sleep(0.2)
+                    retry_limit -= 1
 
     @contextmanager
     def connect(self, run_id=None):
@@ -134,10 +158,12 @@ class SqliteEventLogStorage(SqlEventLogStorage, ConfigurableClass):
         conn_string = self.conn_string_for_run_id(run_id)
         engine = create_engine(conn_string, poolclass=NullPool)
 
-        if not os.path.exists(self.path_for_run_id(run_id)):
+        if not run_id in self._initialized_dbs:
             self._initdb(engine)
+            self._initialized_dbs.add(run_id)
 
         conn = engine.connect()
+
         try:
             with handle_schema_errors(
                 conn,
@@ -162,6 +188,8 @@ class SqliteEventLogStorage(SqlEventLogStorage, ConfigurableClass):
             + glob.glob(os.path.join(self._base_dir, "*.db-shm"))
         ):
             os.unlink(filename)
+
+        self._initialized_dbs = set()
 
     def watch(self, run_id, start_cursor, callback):
         watchdog = SqliteEventLogStorageWatchdog(self, run_id, callback, start_cursor)
