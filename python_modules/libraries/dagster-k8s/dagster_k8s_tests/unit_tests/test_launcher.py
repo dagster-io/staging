4b3690f86a7d1e6dd836b743ec63d287deb1e078
Bob Chen <bob@elementl.com>
2020-10-30 15:04:41 -0400

    Adds test for user defined k8s config in run tags.

---
+++ python_modules/libraries/dagster-k8s/dagster_k8s_tests/unit_tests/test_launcher.py
@@ -0,0 +1,49 @@
+import json
+from dagster_k8s.job import UserDefinedDagsterK8sConfig
+from dagster.core.test_utils import (
+    create_run_for_test,
+    instance_for_test,
+)
+from dagster.seven import mock
+from dagster_test.test_project import (
+    get_test_project_external_pipeline,
+    test_project_docker_image,
+)
+from dagster_k8s import K8sRunLauncher
+
+
+def test_user_defined_k8s_config_in_run_tags():
+    mock_k8s_client_batch_api = mock.MagicMock()
+    expected_resources = {
+        "requests": {"cpu": "250m", "memory": "64Mi"},
+        "limits": {"cpu": "500m", "memory": "2560Mi"},
+    }
+    user_defined_k8s_config = UserDefinedDagsterK8sConfig(
+        container_config={"resources": expected_resources},
+    )
+    user_defined_k8s_config_json = json.dumps(user_defined_k8s_config.to_dict())
+    tags = {"dagster-k8s/config": user_defined_k8s_config_json}
+
+    k8s_run_launcher = K8sRunLauncher(
+        service_account_name="dagit-admin",
+        instance_config_map="dagster-instance",
+        postgres_password_secret="dagster-postgresql-secret",
+        dagster_home="/opt/dagster/dagster_home",
+        job_image=test_project_docker_image(),
+        load_incluster_config=False,
+        k8s_client_batch_api=mock_k8s_client_batch_api,
+    )
+
+    with instance_for_test() as instance:
+        pipeline_name = "demo_pipeline"
+        run = create_run_for_test(instance, pipeline_name=pipeline_name, tags=tags)
+        external_pipeline = get_test_project_external_pipeline(pipeline_name)
+        k8s_run_launcher.initialize(instance)
+        k8s_run_launcher.launch_run(None, run, external_pipeline)
+
+    mock_method_calls = mock_k8s_client_batch_api.method_calls
+    assert len(mock_method_calls) > 0
+    method_name, args, kwargs = mock_method_calls[0]
+    assert method_name == "create_namespaced_job"
+    job_resources = kwargs["body"].spec.template.spec.containers[0].resources
+    assert job_resources == expected_resources
