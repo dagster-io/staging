7debb55dd590a9f63b47887e0f1eb7ae47dc55a7
Bob Chen <bob@elementl.com>
2020-10-30 15:35:41 -0400

    Adds  test for celery-k8s run launcher.

--- python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s_tests/test_launcher.py
+++ python_modules/libraries/dagster-celery-k8s/dagster_celery_k8s_tests/test_launcher.py
@@ -1,9 +1,17 @@
+import json
+
 import pytest
 from dagster import check
-from dagster.core.test_utils import environ
+from dagster.core.test_utils import create_run_for_test, environ, instance_for_test
+from dagster.seven import mock
 from dagster_celery_k8s.config import get_celery_engine_config
 from dagster_celery_k8s.executor import CELERY_K8S_CONFIG_KEY
-from dagster_celery_k8s.launcher import _get_validated_celery_k8s_executor_config
+from dagster_celery_k8s.launcher import (
+    CeleryK8sRunLauncher,
+    _get_validated_celery_k8s_executor_config,
+)
+from dagster_k8s.job import UserDefinedDagsterK8sConfig
+from dagster_test.test_project import get_test_project_external_pipeline, test_project_docker_image
 
 
 def test_get_validated_celery_k8s_executor_config():
@@ -112,3 +120,42 @@ def test_get_validated_celery_k8s_executor_config():
             "env_config_maps": ["config-pipeline-env"],
             "env_secrets": ["config-secret-env"],
         }
+
+
+def test_user_defined_k8s_config_in_run_tags():
+    mock_k8s_client_batch_api = mock.MagicMock()
+    expected_resources = {
+        "requests": {"cpu": "250m", "memory": "64Mi"},
+        "limits": {"cpu": "500m", "memory": "2560Mi"},
+    }
+    user_defined_k8s_config = UserDefinedDagsterK8sConfig(
+        container_config={"resources": expected_resources},
+    )
+    user_defined_k8s_config_json = json.dumps(user_defined_k8s_config.to_dict())
+    tags = {"dagster-k8s/config": user_defined_k8s_config_json}
+
+    celery_k8s_run_launcher = CeleryK8sRunLauncher(
+        instance_config_map="dagster-instance",
+        postgres_password_secret="dagster-postgresql-secret",
+        dagster_home="/opt/dagster/dagster_home",
+        load_incluster_config=False,
+        k8s_client_batch_api=mock_k8s_client_batch_api,
+    )
+
+    with instance_for_test() as instance:
+        celery_k8s_run_launcher.initialize(instance)
+
+        pipeline_name = "demo_pipeline"
+        run_config = {"execution": {"celery-k8s": {"config": {"job_image": "fake-image-name"}}}}
+        run = create_run_for_test(
+            instance, pipeline_name=pipeline_name, run_config=run_config, tags=tags
+        )
+        external_pipeline = get_test_project_external_pipeline(pipeline_name)
+        celery_k8s_run_launcher.launch_run(instance, run, external_pipeline)
+
+    mock_method_calls = mock_k8s_client_batch_api.method_calls
+    assert len(mock_method_calls) > 0
+    method_name, args, kwargs = mock_method_calls[0]
+    assert method_name == "create_namespaced_job"
+    job_resources = kwargs["body"].spec.template.spec.containers[0].resources
+    assert job_resources == expected_resources
