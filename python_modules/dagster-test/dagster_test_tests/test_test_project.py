2540e4072394abbfdf16f756f13c81a055bbd187
Johann Miller <johann@elementl.com>
2020-11-03 17:29:00 -0500

    helm deployment of dagster daemon [Experimental]
    Test Plan: integration
    
    Differential Revision: https://dagster.phacility.com/D5024

---
+++ python_modules/dagster-test/dagster_test_tests/test_test_project.py
@@ -0,0 +1,13 @@
+from dagster.core.host_representation import external
+from dagster_test.test_project import (
+    ReOriginatedExternalPipelineForTest,
+    get_test_project_external_pipeline,
+)
+
+
+def test_reoriginated_external_pipeline():
+    external_pipeline = get_test_project_external_pipeline("demo_pipeline_celery")
+    reoriginated_pipeline = ReOriginatedExternalPipelineForTest(external_pipeline)
+
+    assert reoriginated_pipeline.get_origin()
+    assert reoriginated_pipeline.get_external_origin()
