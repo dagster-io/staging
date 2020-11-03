9d669a06c47b7ee8486a39f9f98bed79132a4b7d
Sandy Ryza <sandy@elementl.com>
2020-11-03 06:57:36 -0800

    active execution

--- python_modules/dagster/dagster_tests/core_tests/execution_plan_tests/test_execution_plan.py
+++ python_modules/dagster/dagster_tests/core_tests/execution_plan_tests/test_execution_plan.py
@@ -6,6 +6,7 @@ from dagster.core.errors import (
     DagsterUnknownStepStateError,
 )
 from dagster.core.execution.api import create_execution_plan, execute_plan
+from dagster.core.execution.plan.objects import StepOutputHandle
 from dagster.core.execution.retries import Retries, RetryMode
 
 from ..engine_tests.test_multiprocessing import define_diamond_pipeline
@@ -44,6 +45,7 @@ def test_active_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_1.key)
+        active_execution.mark_step_produced_output(StepOutputHandle(step_1.key, "result"))
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 2
@@ -56,11 +58,13 @@ def test_active_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_2.key)
+        active_execution.mark_step_produced_output(StepOutputHandle(step_2.key, "result"))
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_3.key)
+        active_execution.mark_step_produced_output(StepOutputHandle(step_3.key, "result"))
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
@@ -93,6 +97,7 @@ def test_failing_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_1.key)
+        active_execution.mark_step_produced_output(StepOutputHandle(step_1.key, "result"))
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 2
@@ -105,6 +110,7 @@ def test_failing_execution_plan():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_2.key)
+        active_execution.mark_step_produced_output(StepOutputHandle(step_2.key, "result"))
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -155,6 +161,7 @@ def test_retries_active_execution():
         assert steps[0].key == "return_two.compute"
 
         active_execution.mark_success(step_1.key)
+        active_execution.mark_step_produced_output(StepOutputHandle(step_1.key, "result"))
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 2
@@ -167,6 +174,7 @@ def test_retries_active_execution():
         assert len(steps) == 0  # cant progress
 
         active_execution.mark_success(step_2.key)
+        active_execution.mark_step_produced_output(StepOutputHandle(step_2.key, "result"))
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
