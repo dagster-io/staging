4f8fdeaa427257e29be85ce7f10a2e17ff8b5937
Johann Miller <johann@elementl.com>
2020-11-03 17:29:00 -0500

    helm deployment of dagster daemon [Experimental]
    Test Plan: integration
    
    Differential Revision: https://dagster.phacility.com/D5024

---
+++ integration_tests/test_suites/celery-k8s-integration-test-suite/test_queued_run_coordinator.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+import pytest
+from dagster.core.test_utils import create_run_for_test
+from dagster.utils import merge_dicts
+from dagster.utils.yaml_utils import merge_yamls
+from dagster_k8s.test import wait_for_job_and_get_raw_logs
+from dagster_test.test_project import (
+    ReOriginatedExternalPipelineForTest,
+    get_test_project_external_pipeline,
+    test_project_environments_path,
+)
+
+
+def get_celery_engine_config(dagster_docker_image, job_namespace):
+    return {
+        "execution": {
+            "celery-k8s": {
+                "config": {
+                    "job_image": dagster_docker_image,
+                    "job_namespace": job_namespace,
+                    "image_pull_policy": "Always",
+                    "env_config_maps": ["dagster-pipeline-env"],
+                }
+            }
+        },
+    }
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(sys.version_info < (3, 5), reason="Very slow on Python 2")
+def test_execute_on_celery_k8s(  # pylint: disable=redefined-outer-name
+    dagster_docker_image, dagster_instance_for_run_coordinator, helm_namespace_for_run_coordinator
+):
+    run_config = merge_dicts(
+        merge_yamls(
+            [
+                os.path.join(test_project_environments_path(), "env.yaml"),
+                os.path.join(test_project_environments_path(), "env_s3.yaml"),
+            ]
+        ),
+        get_celery_engine_config(
+            dagster_docker_image=dagster_docker_image,
+            job_namespace=helm_namespace_for_run_coordinator,
+        ),
+    )
+
+    pipeline_name = "demo_pipeline_celery"
+    run = create_run_for_test(
+        dagster_instance_for_run_coordinator,
+        pipeline_name=pipeline_name,
+        run_config=run_config,
+        mode="default",
+    )
+
+    dagster_instance_for_run_coordinator.launch_run(
+        run.run_id,
+        ReOriginatedExternalPipelineForTest(get_test_project_external_pipeline(pipeline_name)),
+    )
+
+    result = wait_for_job_and_get_raw_logs(
+        job_name="dagster-run-%s" % run.run_id, namespace=helm_namespace_for_run_coordinator
+    )
+
+    assert "PIPELINE_ENQUEUED" in result, "no match, result: {}".format(result)
+    assert "PIPELINE_DEQUEUED" in result, "no match, result: {}".format(result)
+    assert "PIPELINE_SUCCESS" in result, "no match, result: {}".format(result)
