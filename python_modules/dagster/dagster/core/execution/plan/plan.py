8c19e95b5af2676c3d4c3ac51fcdef445b311e0b
dpeng817 <cdguitar817@gmail.com>
2020-11-06 15:29:40 -0500

    [Asset Store Versioning 1 of 3] Place computed step output versions on the execution plan.
    Summary: Adding the step output versions to the execution plan allows them to be accessible at runtime through a `SystemStepExecutionContext`, which enables them to be used in an asset store's implementation.
    
    Test Plan: Unit tests
    
    Reviewers: sandyryza, yuhan
    
    Differential Revision: https://dagster.phacility.com/D5081
    
    migrate resolve_step_version, resolve_resource_version, resolve_step_output_version, to execution plan
    
    transitioned completely away from old step resolution method
    
    completely removed old resolve_version method
    
    use topological steps
    
    removed logic from plan.py. Removed mode definition from execution plan instance.
    
    fixed artifact

--- python_modules/dagster/dagster/core/execution/plan/plan.py
+++ python_modules/dagster/dagster/core/execution/plan/plan.py
@@ -12,6 +12,10 @@ from dagster.core.definitions import (
 )
 from dagster.core.definitions.dependency import DependencyStructure
 from dagster.core.errors import DagsterExecutionStepNotFoundError, DagsterInvariantViolationError
+from dagster.core.execution.resolve_versions import (
+    resolve_step_output_versions_helper,
+    resolve_step_versions_helper,
+)
 from dagster.core.system_config.objects import (
     EmptyIntermediateStoreBackcompatConfig,
     EnvironmentConfig,
@@ -109,7 +113,12 @@ class _PlanBuilder(object):
         check_asset_store_intermediate_storage(self.mode_definition, self.environment_config)
 
         return ExecutionPlan(
-            self.pipeline, step_dict, deps, self.storage_is_persistent(), step_keys_to_execute,
+            self.pipeline,
+            step_dict,
+            deps,
+            self.storage_is_persistent(),
+            step_keys_to_execute,
+            self.environment_config,
         )
 
     def storage_is_persistent(self):
@@ -280,11 +289,18 @@ def get_step_input(
 
 class ExecutionPlan(
     namedtuple(
-        "_ExecutionPlan", "pipeline step_dict deps steps artifacts_persisted step_keys_to_execute",
+        "_ExecutionPlan",
+        "pipeline step_dict deps steps artifacts_persisted step_keys_to_execute environment_config",
     )
 ):
     def __new__(
-        cls, pipeline, step_dict, deps, artifacts_persisted, step_keys_to_execute,
+        cls,
+        pipeline,
+        step_dict,
+        deps,
+        artifacts_persisted,
+        step_keys_to_execute,
+        environment_config,
     ):
         missing_steps = [step_key for step_key in step_keys_to_execute if step_key not in step_dict]
         if missing_steps:
@@ -305,6 +321,9 @@ class ExecutionPlan(
             artifacts_persisted=check.bool_param(artifacts_persisted, "artifacts_persisted"),
             step_keys_to_execute=check.list_param(
                 step_keys_to_execute, "step_keys_to_execute", of_type=str
+            ),
+            environment_config=check.inst_param(
+                environment_config, "environment_config", EnvironmentConfig
             ),
         )
 
@@ -388,6 +407,7 @@ class ExecutionPlan(
             self.deps,
             self.artifacts_persisted,
             step_keys_to_execute,
+            self.environment_config,
         )
 
     def build_memoized_plan(self, step_keys_to_execute, addresses):
@@ -397,8 +417,8 @@ class ExecutionPlan(
         steps do not need to re-execute.
 
         Args:
-            step_keys_to_execute (List[String]): A list of execution step keys to actually run in this
-                execution plan.
+            step_keys_to_execute (List[String]): A list of execution step keys to actually run in
+                this execution plan.
             addresses: (Dict[(str, StepOutputHandle), str]): A dictionary mapping pipeline name and
                 step output handle to an "address", which the intermediate storage can use to
                 retrieve the value for this step output.
@@ -452,7 +472,14 @@ class ExecutionPlan(
             self.deps,
             self.artifacts_persisted,
             step_keys_to_execute,
+            self.environment_config,
         )
+
+    def resolve_step_versions(self):
+        return resolve_step_versions_helper(self)
+
+    def resolve_step_output_versions(self):
+        return resolve_step_output_versions_helper(self)
 
     def start(
         self, retries, sort_key_fn=None,
