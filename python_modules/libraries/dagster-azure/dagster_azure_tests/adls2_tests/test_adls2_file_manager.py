0844214a7dbfb16e9cace4d83345b1a6b636b6bd
yuhan <yuhan@elementl.com>
2020-11-25 11:45:23 -0800

    bye-system-storage-1 cut file manager non-resource paths
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D5305

--- python_modules/libraries/dagster-azure/dagster_azure_tests/adls2_tests/test_adls2_file_manager.py
+++ python_modules/libraries/dagster-azure/dagster_azure_tests/adls2_tests/test_adls2_file_manager.py
@@ -143,13 +143,16 @@ def create_adls2_key(run_id, step_key, output_name):
 def test_depends_on_adls2_resource_file_manager(storage_account, file_system):
     bar_bytes = "bar".encode()
 
-    @solid(output_defs=[OutputDefinition(ADLS2FileHandle)])
+    @solid(output_defs=[OutputDefinition(ADLS2FileHandle)], required_resource_keys={"file_manager"})
     def emit_file(context):
-        return context.file_manager.write_data(bar_bytes)
+        return context.resources.file_manager.write_data(bar_bytes)
 
-    @solid(input_defs=[InputDefinition("file_handle", ADLS2FileHandle)])
+    @solid(
+        input_defs=[InputDefinition("file_handle", ADLS2FileHandle)],
+        required_resource_keys={"file_manager"},
+    )
     def accept_file(context, file_handle):
-        local_path = context.file_manager.copy_handle_to_local_temp(file_handle)
+        local_path = context.resources.file_manager.copy_handle_to_local_temp(file_handle)
         assert isinstance(local_path, str)
         assert open(local_path, "rb").read() == bar_bytes
 
@@ -159,7 +162,10 @@ def test_depends_on_adls2_resource_file_manager(storage_account, file_system):
         mode_defs=[
             ModeDefinition(
                 system_storage_defs=adls2_plus_default_storage_defs,
-                resource_defs={"adls2": ResourceDefinition.hardcoded_resource(adls2_fake_resource)},
+                resource_defs={
+                    "adls2": ResourceDefinition.hardcoded_resource(adls2_fake_resource),
+                    "file_manager": adls2_file_manager,
+                },
             )
         ]
     )
@@ -168,7 +174,14 @@ def test_depends_on_adls2_resource_file_manager(storage_account, file_system):
 
     result = execute_pipeline(
         adls2_file_manager_test,
-        run_config={"storage": {"adls2": {"config": {"adls2_file_system": file_system}}}},
+        run_config={
+            "resources": {
+                "file_manager": {
+                    "config": {"adls2_file_system": file_system, "adls2_prefix": "some-prefix",}
+                }
+            },
+            "storage": {"adls2": {"config": {"adls2_file_system": file_system}}},
+        },
     )
 
     assert result.success
@@ -186,7 +199,7 @@ def test_depends_on_adls2_resource_file_manager(storage_account, file_system):
     file_key = list(keys_in_bucket)[0]
     comps = file_key.split("/")
 
-    assert "/".join(comps[:-1]) == "dagster/storage/{run_id}/files".format(run_id=result.run_id)
+    assert "/".join(comps[:-1]) == "some-prefix"
 
     assert uuid.UUID(comps[-1])
 
