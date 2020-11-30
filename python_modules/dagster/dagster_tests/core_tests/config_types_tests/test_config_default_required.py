d873fa74a3fe47bd94424f3e9fe03d2c5361da67
Nicholas Schrock <schrockn@elementl.com>
2020-11-29 16:56:51 -0800

    Make Noneable types make fields becomes optional
    Summary: TODO
    
    Test Plan: BK
    
    Differential Revision: https://dagster.phacility.com/D5320

--- python_modules/dagster/dagster_tests/core_tests/config_types_tests/test_config_default_required.py
+++ python_modules/dagster/dagster_tests/core_tests/config_types_tests/test_config_default_required.py
@@ -1,4 +1,4 @@
-from dagster import Field, execute_solid, solid
+from dagster import Field, Noneable, execute_solid, solid
 
 
 def test_default_implies_not_required_field_correct():
@@ -18,3 +18,16 @@ def test_default_implies_not_required_execute_solid():
     # This currently throws. It should not require configuration as the
     # the default value should make it *not* required.
     execute_solid(return_default_to_one)
+
+
+def test_scalar_field_defaults():
+    assert Field(int).is_required is True
+    assert Field(Noneable(int)).is_required is False
+    assert Field(Noneable(int)).default_value is None
+
+
+def test_noneable_shaped_field_defaults():
+    schema = {"an_int": int}
+    assert Field(schema).is_required is True
+    assert Field(Noneable(schema)).is_required is False
+    assert Field(Noneable(schema)).default_value is None
