53efb440fb86ae577320a6db4e06b34f9d14141b
gibsondan <daniel@elementl.com>
2020-12-01 08:54:53 -0600

    Add DockerRunLauncher
    Summary:
    This adds a run launcher that we could include as part of a docker-compose based deployment using a container for dagit and a container for dagster-daemon. (Our previous non-k8s docker example relied on everything being in a single container, which doesn't work well with the addition of the daemon). It's pretty similar to the existing celery docker executor, just launching runs instead of executing steps.
    
    That said if we want to add this, I should probably create a separate dagster-docker module instead with no celery dependencies?
    
    Test Plan: BK
    
    Reviewers: alangenfeld, nate, sashank, johann, schrockn
    
    Subscribers: prha, schrockn
    
    Differential Revision: https://dagster.phacility.com/D5348

--- python_modules/dagster-test/dagster_test/test_project/test_pipelines/repo.py
+++ python_modules/dagster-test/dagster_test/test_project/test_pipelines/repo.py
@@ -67,6 +67,23 @@ def error_solid():
     raise Exception("Unusual error")
 
 
+@solid
+def hanging_solid(_):
+    while True:
+        time.sleep(0.1)
+
+
+@pipeline(
+    mode_defs=[
+        ModeDefinition(
+            system_storage_defs=s3_plus_default_storage_defs, resource_defs={"s3": s3_resource}
+        )
+    ]
+)
+def hanging_pipeline():
+    hanging_solid()
+
+
 @pipeline(
     mode_defs=[
         ModeDefinition(
@@ -454,6 +471,7 @@ def define_demo_execution_repo():
                 "resource_pipeline": define_resource_pipeline,
                 "docker_celery_pipeline": define_docker_celery_pipeline,
                 "demo_airflow_execution_date_pipeline": demo_airflow_execution_date_pipeline,
+                "hanging_pipeline": hanging_pipeline,
                 "hard_failer": define_hard_failer,
             },
             "schedules": define_schedules(),
