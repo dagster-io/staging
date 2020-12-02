97950445cebc8b7563b2634b323a107ae41576c4
gibsondan <daniel@elementl.com>
2020-12-02 10:34:04 -0600

    Example docker deployment with daemon and user codeh
    Summary:
    This provides a sample docker-compose setup that we could reference in the docs. There's a container for dagit, the dagster-daemon, and user code, and each run is also launched in its own container using the DockerRunLauncher introduced in D5348.
    
    Once we have 0.10.0 available in pypi, the Dockerfiles (especially dagit) can be made a lot simpler since they no longer need to build from source.
    
    Still need to figure out the best way to test this, would welcome some initial feedback in the meantime though.
    
    Test Plan: TBD
    
    Reviewers: nate, johann, max, alangenfeld, catherinewu
    
    Subscribers: schrockn, prha
    
    Differential Revision: https://dagster.phacility.com/D5416

---
+++ examples/deploy_docker/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+ROOT=$(git rev-parse --show-toplevel)
+BASE_DIR="${ROOT}/examples/deploy_docker"
+
+function cleanup {
+    rm -rf "${BASE_DIR}/modules"
+    set +ux
+}
+
+# ensure cleanup happens on error or normal exit
+trap cleanup INT TERM EXIT ERR
+
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Must specify a Python version.\n" 1>&2
+    echo "Usage: ./build.sh 3.7.8" 1>&2
+    exit 1
+fi
+set -ux
+
+# e.g. 3.7.4
+PYTHON_VERSION=$1
+
+
+pushd $BASE_DIR
+
+mkdir -p modules
+
+cp $GOOGLE_APPLICATION_CREDENTIALS ./modules/gac.json
+
+echo -e "--- \033[32m:truck: Copying files...\033[0m"
+cp -R $ROOT/python_modules/dagster \
+      $ROOT/python_modules/dagit \
+      $ROOT/python_modules/dagster-graphql \
+      $ROOT/python_modules/libraries/dagster-airflow \
+      $ROOT/python_modules/libraries/dagster-aws \
+      $ROOT/python_modules/libraries/dagster-celery \
+      $ROOT/python_modules/libraries/dagster-celery-k8s \
+      $ROOT/python_modules/libraries/dagster-celery-docker \
+      $ROOT/python_modules/libraries/dagster-cron \
+      $ROOT/python_modules/libraries/dagster-docker \
+      $ROOT/python_modules/libraries/dagster-pandas \
+      $ROOT/python_modules/libraries/dagster-postgres \
+      $ROOT/python_modules/libraries/dagster-gcp \
+      $ROOT/python_modules/libraries/dagster-k8s \
+      modules/
+
+find . \( -name '*.egg-info' -o -name '*.tox' -o -name 'dist' \) | xargs rm -rf
+
+echo -e "--- \033[32m:docker: Building Docker image\033[0m"
+docker build . \
+    --build-arg PYTHON_VERSION="${PYTHON_VERSION}" \
+    -t "${IMAGE_TAG}"
