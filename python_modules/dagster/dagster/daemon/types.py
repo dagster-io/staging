58410d1cbb0997130d4fc78b73b93ccf1c6ed238
Johann Miller <johann@elementl.com>
2020-12-02 21:21:01 -0500

    [RFC] GraphQL endpoint for daemon statuses
    Summary: Fetch info about the running daemons
    
    Test Plan:
    In the playground:
    
    ```
    # Write your query or mutation here
    query InstanceDetailSummaryQuery {
      version
      instance {
        daemonHealth {
          sensorDaemon {
            required
            healthy
            lastHeartbeatTime
          }
        }
      }
    }
    
    {
      "data": {
        "version": "0.9.20",
        "instance": {
          "daemonHealth": {
            "sensorDaemon": {
              "required": true,
              "healthy": true,
              "lastHeartbeatTime": 1606962000.61016
            }
          }
        }
      }
    }
    ```
    
    Differential Revision: https://dagster.phacility.com/D5402

--- python_modules/dagster/dagster/daemon/types.py
+++ python_modules/dagster/dagster/daemon/types.py
@@ -1,6 +1,17 @@
 from collections import namedtuple
 
+from dagster import check
+
 
 class DaemonHeartbeat(namedtuple("_DaemonHeartbeat", "timestamp daemon_type daemon_id info")):
     def __new__(cls, timestamp, daemon_type, daemon_id, info):
         return super(DaemonHeartbeat, cls).__new__(cls, timestamp, daemon_type, daemon_id, info)
+
+
+class DaemonStatus(namedtuple("_DaemonStatus", "daemon_type required healthy last_heartbeat")):
+    def __new__(cls, daemon_type, required, healthy, last_heartbeat):
+        check.str_param(daemon_type, "daemon_type")
+        check.bool_param(required, "required")
+        check.opt_bool_param(healthy, "healthy")
+        check.opt_inst_param(last_heartbeat, "last_heartbeat", DaemonHeartbeat)
+        return super(DaemonStatus, cls).__new__(cls, daemon_type, required, healthy, last_heartbeat)
