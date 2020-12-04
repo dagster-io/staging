880b0201ebee64cbd479d183379b267390c62e9f
Rex Ledesma <rex@elementl.com>
2020-12-04 18:20:37 -0500

    Add DaemonHeartbeatsTable to module export
    Summary:
    Running into this error when deploying dagit nightly:
    ```
    Traceback (most recent call last):
      File "/usr/local/bin/dagit", line 33, in <module>
        sys.exit(load_entry_point('dagit', 'console_scripts', 'dagit')())
      File "/dagit/dagit/cli.py", line 215, in main
        cli(auto_envvar_prefix="DAGIT")  # pylint:disable=E1120
      File "/usr/local/lib/python3.7/site-packages/click/core.py", line 829, in __call__
        return self.main(*args, **kwargs)
      File "/usr/local/lib/python3.7/site-packages/click/core.py", line 782, in main
        rv = self.invoke(ctx)
      File "/usr/local/lib/python3.7/site-packages/click/core.py", line 1066, in invoke
        return ctx.invoke(self.callback, **ctx.params)
      File "/usr/local/lib/python3.7/site-packages/click/core.py", line 610, in invoke
        return callback(*args, **kwargs)
      File "/dagit/dagit/cli.py", line 114, in ui
        **kwargs,
      File "/dagit/dagit/cli.py", line 125, in host_dagit_ui
        with DagsterInstance.get(storage_fallback) as instance:
      File "/usr/local/lib/python3.7/site-packages/dagster/core/instance/__init__.py", line 276, in get
        return DagsterInstance.from_config(_dagster_home())
      File "/usr/local/lib/python3.7/site-packages/dagster/core/instance/__init__.py", line 303, in from_config
        return DagsterInstance.from_ref(instance_ref)
      File "/usr/local/lib/python3.7/site-packages/dagster/core/instance/__init__.py", line 311, in from_ref
        run_storage=instance_ref.run_storage,
      File "/usr/local/lib/python3.7/site-packages/dagster/core/instance/ref.py", line 196, in run_storage
        return self.run_storage_data.rehydrate()
      File "/usr/local/lib/python3.7/site-packages/dagster/serdes/__init__.py", line 342, in rehydrate
        module = importlib.import_module(self.module_name)
      File "/usr/local/lib/python3.7/importlib/__init__.py", line 127, in import_module
        return _bootstrap._gcd_import(name[level:], package, level)
      File "<frozen importlib._bootstrap>", line 1006, in _gcd_import
      File "<frozen importlib._bootstrap>", line 983, in _find_and_load
      File "<frozen importlib._bootstrap>", line 953, in _find_and_load_unlocked
      File "<frozen importlib._bootstrap>", line 219, in _call_with_frames_removed
      File "<frozen importlib._bootstrap>", line 1006, in _gcd_import
      File "<frozen importlib._bootstrap>", line 983, in _find_and_load
      File "<frozen importlib._bootstrap>", line 967, in _find_and_load_unlocked
      File "<frozen importlib._bootstrap>", line 677, in _load_unlocked
      File "<frozen importlib._bootstrap_external>", line 728, in exec_module
      File "<frozen importlib._bootstrap>", line 219, in _call_with_frames_removed
      File "/dagster-postgres/dagster_postgres/__init__.py", line 4, in <module>
        from .run_storage import PostgresRunStorage
      File "/dagster-postgres/dagster_postgres/run_storage/__init__.py", line 1, in <module>
        from .run_storage import PostgresRunStorage
      File "/dagster-postgres/dagster_postgres/run_storage/run_storage.py", line 4, in <module>
        from dagster.core.storage.runs.schema import DaemonHeartbeatsTable
    ImportError: cannot import name 'DaemonHeartbeatsTable' from 'dagster.core.storage.runs.schema' (/usr/local/lib/python3.7/site-packages/dagster/core/storage/runs/schema.py)
    ```
    
    We add the missing export.
    
    Test Plan:
    N/A
    
    Reviewers:
    johann, dgibson
    
    Subscribers:

--- python_modules/dagster/dagster/core/storage/runs/__init__.py
+++ python_modules/dagster/dagster/core/storage/runs/__init__.py
@@ -1,5 +1,5 @@
 from .base import RunStorage
 from .in_memory import InMemoryRunStorage
-from .schema import RunStorageSqlMetadata
+from .schema import RunStorageSqlMetadata, DaemonHeartbeatsTable
 from .sql_run_storage import SqlRunStorage
 from .sqlite import SqliteRunStorage
