7253df945845dd8193b1f5761b2f2b25146c2ea7
gibsondan <daniel@elementl.com>
2020-12-08 15:01:01 -0600

    Add PipelineRunStatus.CANCELLING and PipelineRunStatus.CANCELLED
    Summary: This gives the frontend an extra signal abuot whether or not a termination has been attempted previously, and gives more information about what's going on, which is always good.
    
    Test Plan: Integration
    
    Reviewers: prha, johann, alangenfeld
    
    Subscribers: dish
    
    Differential Revision: https://dagster.phacility.com/D5491

--- js_modules/dagit/src/runs/RunStatusTag.tsx
+++ js_modules/dagit/src/runs/RunStatusTag.tsx
@@ -55,6 +55,18 @@ export const RunStatusTag = (props: {status: IRunStatus}) => {
           Managed
         </StatusTag>
       );
+    case 'CANCELING':
+      return (
+        <StatusTag minimal intent="danger">
+          Canceling
+        </StatusTag>
+      );
+    case 'CANCELED':
+      return (
+        <StatusTag minimal intent="danger">
+          Canceled
+        </StatusTag>
+      );
     default:
       return assertUnreachable(status);
   }
