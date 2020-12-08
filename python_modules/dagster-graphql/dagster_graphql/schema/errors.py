1546a23052f54e569cfec87bbc1022f556ee25f8
Johann Miller <johann@elementl.com>
2020-12-08 17:31:10 -0500

    Dauphin pipeline runs count
    Test Plan: Unit
    
    Differential Revision: https://dagster.phacility.com/D5488

--- python_modules/dagster-graphql/dagster_graphql/schema/errors.py
+++ python_modules/dagster-graphql/dagster_graphql/schema/errors.py
@@ -16,6 +16,7 @@ from dagster.core.snap import ConfigSchemaSnapshot
 from dagster.utils.error import SerializableErrorInfo
 from dagster_graphql import dauphin
 
+from ..implementation.fetch_runs import get_runs, get_runs_count
 from ..implementation.utils import PipelineSelector
 from .config_types import DauphinConfigTypeField
 
@@ -559,7 +560,19 @@ class DauphinPipelineRuns(dauphin.ObjectType):
     class Meta:
         name = "PipelineRuns"
 
+    def __init__(self, filters, cursor, limit):
+        self._filters = filters
+        self._cursor = cursor
+        self._limit = limit
+
     results = dauphin.non_null_list("PipelineRun")
+    count = dauphin.Int()
+
+    def resolve_results(self, _info):
+        return get_runs(_info, self._filters, self._cursor, self._limit)
+
+    def resolve_count(self, _info):
+        return get_runs_count(_info, self._filters)
 
 
 class DauphinPipelineRunsOrError(dauphin.Union):
