1a9ea72fd0a535ed81a5d5b6581314aa9ac1aca1
Sandy Ryza <sandy@elementl.com>
2020-12-07 16:51:50 -0800

    object managers docs overview
    Summary:
    This is mostly just renaming from Asset Store -> Object Manager
    
    I also added high-level explanation of the relationship between the different manager types.  Here's an issue to track explaining InputManagers with examples like we have for ObjectManager: https://github.com/dagster-io/dagster/issues/3383.
    
    Test Plan: manual inspection
    
    Reviewers: yuhan, sashank, schrockn
    
    Differential Revision: https://dagster.phacility.com/D5522

--- examples/docs_snippets/docs_snippets_tests/overview_tests/object_manager_tests/test_object_manager_per_output.py
+++ examples/docs_snippets/docs_snippets_tests/overview_tests/object_manager_tests/test_object_manager_per_output.py
@@ -1,9 +1,9 @@
 from dagster import execute_pipeline
 from dagster.seven import TemporaryDirectory
-from docs_snippets.overview.asset_stores.default_asset_store import my_pipeline
+from docs_snippets.overview.object_managers.object_manager_per_output import my_pipeline
 
 
-def test_default_asset_store():
+def test_object_manager_per_output():
     with TemporaryDirectory() as tmpdir:
         execute_pipeline(
             my_pipeline,
