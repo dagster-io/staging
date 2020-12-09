601af00a9cc3511f00331cc32e8415aed2f69b2c
alangenfeld <alex@elementl.com>
2020-12-09 11:04:48 -0600

    [RFC] drop .compute suffix on step keys
    Test Plan: integration
    
    Differential Revision: https://dagster.phacility.com/D5512

--- python_modules/dagster/dagster_tests/core_tests/execution_plan_tests/test_execution_plan.py
+++ python_modules/dagster/dagster_tests/core_tests/execution_plan_tests/test_execution_plan.py
@@ -18,9 +18,9 @@ def test_topological_sort():
 
     assert len(levels) == 3
 
-    assert [step.key for step in levels[0]] == ["return_two.compute"]
-    assert [step.key for step in levels[1]] == ["add_three.compute", "mult_three.compute"]
-    assert [step.key for step in levels[2]] == ["adder.compute"]
+    assert [step.key for step in levels[0]] == ["return_two"]
+    assert [step.key for step in levels[1]] == ["add_three", "mult_three"]
+    assert [step.key for step in levels[2]] == ["adder"]
 
 
 def test_create_execution_plan_with_bad_inputs():
@@ -38,7 +38,7 @@ def test_active_execution_plan():
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
         step_1 = steps[0]
-        assert step_1.key == "return_two.compute"
+        assert step_1.key == "return_two"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -49,8 +49,8 @@ def test_active_execution_plan():
         assert len(steps) == 2
         step_2 = steps[0]
         step_3 = steps[1]
-        assert step_2.key == "add_three.compute"
-        assert step_3.key == "mult_three.compute"
+        assert step_2.key == "add_three"
+        assert step_3.key == "mult_three"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -66,7 +66,7 @@ def test_active_execution_plan():
         assert len(steps) == 1
         step_4 = steps[0]
 
-        assert step_4.key == "adder.compute"
+        assert step_4.key == "adder"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -87,7 +87,7 @@ def test_failing_execution_plan():
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
         step_1 = steps[0]
-        assert step_1.key == "return_two.compute"
+        assert step_1.key == "return_two"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -98,8 +98,8 @@ def test_failing_execution_plan():
         assert len(steps) == 2
         step_2 = steps[0]
         step_3 = steps[1]
-        assert step_2.key == "add_three.compute"
-        assert step_3.key == "mult_three.compute"
+        assert step_2.key == "add_three"
+        assert step_3.key == "mult_three"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -122,7 +122,7 @@ def test_failing_execution_plan():
         assert len(steps) == 1
         step_4 = steps[0]
 
-        assert step_4.key == "adder.compute"
+        assert step_4.key == "adder"
         active_execution.mark_abandoned(step_4.key)
 
         assert active_execution.is_complete
@@ -137,7 +137,7 @@ def test_retries_active_execution():
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
         step_1 = steps[0]
-        assert step_1.key == "return_two.compute"
+        assert step_1.key == "return_two"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -146,13 +146,13 @@ def test_retries_active_execution():
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
-        assert steps[0].key == "return_two.compute"
+        assert steps[0].key == "return_two"
 
         active_execution.mark_up_for_retry(step_1.key)
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
-        assert steps[0].key == "return_two.compute"
+        assert steps[0].key == "return_two"
 
         active_execution.mark_success(step_1.key)
 
@@ -160,8 +160,8 @@ def test_retries_active_execution():
         assert len(steps) == 2
         step_2 = steps[0]
         step_3 = steps[1]
-        assert step_2.key == "add_three.compute"
-        assert step_3.key == "mult_three.compute"
+        assert step_2.key == "add_three"
+        assert step_3.key == "mult_three"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -184,7 +184,7 @@ def test_retries_active_execution():
         assert len(steps) == 1
         step_4 = steps[0]
 
-        assert step_4.key == "adder.compute"
+        assert step_4.key == "adder"
         active_execution.mark_abandoned(step_4.key)
 
         assert active_execution.is_complete
@@ -200,7 +200,7 @@ def test_retries_disabled_active_execution():
             steps = active_execution.get_steps_to_execute()
             assert len(steps) == 1
             step_1 = steps[0]
-            assert step_1.key == "return_two.compute"
+            assert step_1.key == "return_two"
 
             steps = active_execution.get_steps_to_execute()
             assert len(steps) == 0  # cant progress
@@ -218,7 +218,7 @@ def test_retries_deferred_active_execution():
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 1
         step_1 = steps[0]
-        assert step_1.key == "return_two.compute"
+        assert step_1.key == "return_two"
 
         steps = active_execution.get_steps_to_execute()
         assert len(steps) == 0  # cant progress
@@ -284,12 +284,12 @@ def test_priorities():
     plan = create_execution_plan(priorities)
     with plan.start(Retries(RetryMode.DISABLED), sort_key_fn) as active_execution:
         steps = active_execution.get_steps_to_execute()
-        assert steps[0].key == "pri_5.compute"
-        assert steps[1].key == "pri_4.compute"
-        assert steps[2].key == "pri_3.compute"
-        assert steps[3].key == "pri_2.compute"
-        assert steps[4].key == "pri_none.compute"
-        assert steps[5].key == "pri_neg_1.compute"
+        assert steps[0].key == "pri_5"
+        assert steps[1].key == "pri_4"
+        assert steps[2].key == "pri_3"
+        assert steps[3].key == "pri_2"
+        assert steps[4].key == "pri_none"
+        assert steps[5].key == "pri_neg_1"
         _ = [active_execution.mark_skipped(step.key) for step in steps]
 
 
