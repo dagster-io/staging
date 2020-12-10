d1455c9bb60ff8f73e5a4baf095543fee796a317
yuhan <yuhan@elementl.com>
2020-12-10 10:16:00 -0800

    intermediate-deprecation-1 ObjectStoreBackcompatObjectManager & InMemoryBackcompatObjectManager
    Summary:
    
    Adapter for deprecating object store and serialization strategy
    so that we could take serialization strategy through dagster type
    
    serialization strategy is likely to be deprecated later in stack
    
    Test Plan:
    wip
    Reviewers:
    
    Subscribers:

--- python_modules/dagster/dagster/core/storage/mem_object_manager.py
+++ python_modules/dagster/dagster/core/storage/mem_object_manager.py
@@ -1,3 +1,12 @@
-from dagster.core.storage.asset_store import mem_asset_store
+from dagster.core.storage.asset_store import InMemoryAssetStore, mem_asset_store
 
 mem_object_manager = mem_asset_store
+
+
+class InMemoryBackcompatObjectManager(InMemoryAssetStore):
+    # TODO: change it to InMemoryObjectManager
+    """Adapter for deprecating Object Store and Serialization Strategy, in favor of Object Manager"""
+
+    def has_object(self, context):
+        keys = tuple(context.get_run_scoped_output_identifier())
+        return keys in self.values
