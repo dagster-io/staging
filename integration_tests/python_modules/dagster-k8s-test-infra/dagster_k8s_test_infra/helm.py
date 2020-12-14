ae04a2b0ba3ebdeb4bd2ff0221d51502262ec71e
Bob Chen <bob@elementl.com>
2020-12-14 13:11:50 -0500

    Use httpGet probe handle for dagit in helm integration tests.

--- integration_tests/python_modules/dagster-k8s-test-infra/dagster_k8s_test_infra/helm.py
+++ integration_tests/python_modules/dagster-k8s-test-infra/dagster_k8s_test_infra/helm.py
@@ -301,12 +301,12 @@ def helm_chart(namespace, docker_image, should_cleanup=True):
             "env_config_maps": [TEST_CONFIGMAP_NAME],
             "env_secrets": [TEST_SECRET_NAME],
             "livenessProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "periodSeconds": 20,
                 "failureThreshold": 3,
             },
             "startupProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "failureThreshold": 6,
                 "periodSeconds": 10,
             },
@@ -362,12 +362,12 @@ def helm_chart_for_k8s_run_launcher(namespace, docker_image, should_cleanup=True
             "env_config_maps": [TEST_CONFIGMAP_NAME],
             "env_secrets": [TEST_SECRET_NAME],
             "livenessProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "periodSeconds": 20,
                 "failureThreshold": 3,
             },
             "startupProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "failureThreshold": 6,
                 "periodSeconds": 10,
             },
@@ -416,12 +416,12 @@ def helm_chart_for_user_deployments(namespace, docker_image, should_cleanup=True
             "env_config_maps": [TEST_CONFIGMAP_NAME],
             "env_secrets": [TEST_SECRET_NAME],
             "livenessProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "periodSeconds": 20,
                 "failureThreshold": 3,
             },
             "startupProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "failureThreshold": 6,
                 "periodSeconds": 10,
             },
@@ -497,12 +497,12 @@ def helm_chart_for_run_coordinator(namespace, docker_image, should_cleanup=True)
             "env_config_maps": [TEST_CONFIGMAP_NAME],
             "env_secrets": [TEST_SECRET_NAME],
             "livenessProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "periodSeconds": 20,
                 "failureThreshold": 3,
             },
             "startupProbe": {
-                "tcpSocket": {"port": "http"},
+                "httpGet": {"path": "/dagit_info", "port": 80},
                 "failureThreshold": 6,
                 "periodSeconds": 10,
             },
