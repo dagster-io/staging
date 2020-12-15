d2b1a00265a6914d96c6e4eb82b5e29e51d8dde1
Johann Miller <johann@elementl.com>
2020-12-15 15:23:34 -0500

    [RFC] Errors in daemon heartbeats
    Summary:
    
    Test Plan:
    Unit
    Reviewers:
    
    Subscribers:

--- python_modules/dagster/dagster/daemon/types.py
+++ python_modules/dagster/dagster/daemon/types.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from enum import Enum
 
 from dagster import check
+from dagster.serdes import whitelist_for_serdes
 
 
 class DaemonType(Enum):
@@ -15,14 +16,37 @@ class DaemonType(Enum):
     QUEUED_RUN_COORDINATOR = "QUEUED_RUN_COORDINATOR"
 
 
+@whitelist_for_serdes
+class DaemonHeartbeatInfo(namedtuple("_DaemonHeartbeatInfo", "error")):
+    """
+    Heartbeat info holds additional fields on heartbeats that are serialized into a single column
+    in storage.
+    """
+
+    def __new__(cls, error):
+        check.opt_str_param(error, "error")
+        return super(DaemonHeartbeatInfo, cls).__new__(cls, error)
+
+
+@whitelist_for_serdes
 class DaemonHeartbeat(namedtuple("_DaemonHeartbeat", "timestamp daemon_type daemon_id info")):
+    """
+    Heartbeats are placed in storage by the daemon to show liveness
+    """
+
     def __new__(cls, timestamp, daemon_type, daemon_id, info):
         check.inst_param(timestamp, "timestamp", datetime)
         check.inst_param(daemon_type, "daemon_type", DaemonType)
+        check.opt_inst_param(info, "info", DaemonHeartbeatInfo)
         return super(DaemonHeartbeat, cls).__new__(cls, timestamp, daemon_type, daemon_id, info)
 
 
 class DaemonStatus(namedtuple("_DaemonStatus", "daemon_type required healthy last_heartbeat")):
+    """
+    Daemon statuses are derived from daemon heartbeats and instance configuration to provide an
+    overview about the daemon's liveness.
+    """
+
     def __new__(cls, daemon_type, required, healthy, last_heartbeat):
         check.inst_param(daemon_type, "daemon_type", DaemonType)
         check.bool_param(required, "required")
