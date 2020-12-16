6aad3b8a4ba0e9bf5015389fc5569eb692b1aa2c
gibsondan <daniel@elementl.com>
2020-12-15 22:40:44 -0600

    Include dupicate table errors in the list of errors that we retry postgres connections
    Summary: Hoping this will squash the most commmon source of helm integration test flakiness (two pods spinning up instances at the same time and running into issues)
    
    Test Plan: Integration
    
    Differential Revision: https://dagster.phacility.com/D5636

--- python_modules/libraries/dagster-postgres/dagster_postgres/utils.py
+++ python_modules/libraries/dagster-postgres/dagster_postgres/utils.py
@@ -68,8 +68,35 @@ def retry_pg_connection_fn(fn, retry_limit=5, retry_wait=0.2):
             # These are broad, we may want to list out specific exceptions to capture
             psycopg2.DatabaseError,
             psycopg2.OperationalError,
+            psycopg2.ProgrammingError,
+            psycopg2.IntegrityError,
             sqlalchemy.exc.OperationalError,
+            sqlalchemy.exc.ProgrammingError,
+            sqlalchemy.exc.DatabaseError,
+            sqlalchemy.exc.IntegrityError,
         ) as exc:
+            if (
+                isinstance(exc, sqlalchemy.exc.ProgrammingError)
+                and exc.origin
+                and exc.origin.pgcode != psycopg2.errorcodes.DUPLICATE_TABLE
+            ) or (
+                isinstance(exc, psycopg2.ProgrammingError)
+                and exc.pgcode != psycopg2.errorcodes.DUPLICATE_TABLE
+            ):
+                # Only retry ProgrammingErrors on DuplicateTable errors, which can happen
+                # when a DagsterInstance is being created from two different processes
+
+                origin = (
+                    str(exc.origin)
+                    + ((" : CODE: " + str(exc.origin.pgcode)) if exc.origin else " : NO CODE")
+                    if isinstance(sqlalchemy.exc.ProgrammingError)
+                    else ("PG CODE: " + str(exc.pgcode))
+                )
+
+                six.raise_from(DagsterPostgresException("dgibson ORIGIN: " + origin), exc)
+
+                raise
+
             logging.warning("Retrying failed database connection")
             if retry_limit == 0:
                 six.raise_from(DagsterPostgresException("too many retries for DB connection"), exc)
