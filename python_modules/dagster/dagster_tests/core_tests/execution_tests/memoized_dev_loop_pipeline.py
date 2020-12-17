2a507671634b0434d63b3e87c5b26ee8df39f009
Sandy Ryza <sandy@elementl.com>
2020-12-17 15:59:49 -0800

    VersionedAssetStore -> MemoizableObjectManager
    Summary:
    This rename is based on two observations:
    * AssetStores are now caled ObjectManagers
    * Nothing about the VersionedAssetStore actually requires the user to use versioning.  If they have some other memoization scheme, e.g. they just care whether the file exists or not, they can use that as well.
    
    Test Plan: bk
    
    Reviewers: cdecarolis, yuhan
    
    Differential Revision: https://dagster.phacility.com/D5693

--- python_modules/dagster/dagster_tests/core_tests/execution_tests/memoized_dev_loop_pipeline.py
+++ python_modules/dagster/dagster_tests/core_tests/execution_tests/memoized_dev_loop_pipeline.py
@@ -8,7 +8,7 @@ from dagster import (
     repository,
     solid,
 )
-from dagster.core.storage.asset_store import versioned_filesystem_asset_store
+from dagster.core.storage.memoizable_object_manager import versioned_filesystem_object_manager
 
 
 @solid(
@@ -37,7 +37,7 @@ def take_string_1_asset(context, _string_input):
 @pipeline(
     mode_defs=[
         ModeDefinition(
-            "only_mode", resource_defs={"object_manager": versioned_filesystem_asset_store}
+            "only_mode", resource_defs={"object_manager": versioned_filesystem_object_manager}
         )
     ]
 )
