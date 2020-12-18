942761902e388ed45962b5f6038faa29da2e358d
Isaac Hellendag <dish@elementl.com>
2020-12-18 09:48:34 -0600

    [dagit] Force cancelation from Dagit UI
    Summary:
    
    Use the new termination policy to allow forcing run cancelation from the Dagit UI. Specifically, this diff adds the behavior to bulk actions on the Runs page. In a followup, we can add the same behavior to individual termination.
    
    On the Runs page, if I select a handful of runs, then use the bulk menu to terminate:
    
    - If I have selected runs with `canTerminate === true`, show a checkbox in the termination dialog to allow forcing cancelation, with a warning. If the checkbox is checked, change the language to indicate that it will be a force-cancelation.
    - If I have selected only runs with `canTerminate === false`, don't show the checkbox, just show the warning. The action will be to force termination for all of them.
    
    Add a handful of storybook examples to test.
    
    Test Plan:
    
    Storybook examples. View Runs page after kicking off a run, use bulk menu to cancel. Verify proper rendering and behavior of termination dialog.
    
    Reviewers:
    
    dgibson, prha, johann
    
    Subscribers:

--- js_modules/dagit/src/runs/TerminationDialog.tsx
+++ js_modules/dagit/src/runs/TerminationDialog.tsx
@@ -1,5 +1,5 @@
 import {useMutation} from '@apollo/client';
-import {Button, Classes, Colors, Dialog, Icon, ProgressBar} from '@blueprintjs/core';
+import {Checkbox, Button, Classes, Colors, Dialog, Icon, ProgressBar} from '@blueprintjs/core';
 import * as React from 'react';
 
 import {NavigationBlock} from 'src/runs/NavitationBlock';
@@ -10,10 +10,15 @@ import {
   Cancel_terminatePipelineExecution_PythonError,
   Cancel_terminatePipelineExecution_TerminatePipelineExecutionFailure,
 } from 'src/runs/types/Cancel';
+import {TerminatePipelinePolicy} from 'src/types/globalTypes';
+import {Box} from 'src/ui/Box';
 import {Group} from 'src/ui/Group';
 import {FontFamily} from 'src/ui/styles';
 
+type ForceTerminate = 'always' | 'never' | 'optional';
+
 export interface Props {
+  forceTerminate: ForceTerminate;
   isOpen: boolean;
   onClose: () => void;
   onComplete: () => void;
@@ -27,26 +32,26 @@ type Error =
   | undefined;
 
 type TerminationDialogState = {
+  mustForce: boolean;
   step: 'initial' | 'terminating' | 'completed';
   termination: {completed: number; errors: {[id: string]: Error}};
 };
 
-const initialState: TerminationDialogState = {
-  step: 'initial',
-  termination: {completed: 0, errors: {}},
+const initializeState = (forceTerminate: ForceTerminate): TerminationDialogState => {
+  return {
+    mustForce: forceTerminate === 'always',
+    step: 'initial',
+    termination: {completed: 0, errors: {}},
+  };
 };
 
 type TerminationDialogAction =
-  | {type: 'reset'}
+  | {type: 'reset'; forceTerminate: ForceTerminate}
+  | {type: 'toggle-force-terminate'; checked: boolean}
   | {type: 'start'}
   | {type: 'termination-success'}
   | {type: 'termination-error'; id: string; error: Error}
   | {type: 'complete'};
-
-type Reducer = (
-  prevState: TerminationDialogState,
-  action: TerminationDialogAction,
-) => TerminationDialogState;
 
 const terminationDialogReducer = (
   prevState: TerminationDialogState,
@@ -54,7 +59,9 @@ const terminationDialogReducer = (
 ): TerminationDialogState => {
   switch (action.type) {
     case 'reset':
-      return initialState;
+      return initializeState(action.forceTerminate);
+    case 'toggle-force-terminate':
+      return {...prevState, mustForce: action.checked};
     case 'start':
       return {...prevState, step: 'terminating'};
     case 'termination-success': {
@@ -83,25 +90,32 @@ const terminationDialogReducer = (
 };
 
 export const TerminationDialog = (props: Props) => {
-  const {isOpen, onClose, onComplete, selectedIDs} = props;
-  const [state, dispatch] = React.useReducer<Reducer>(terminationDialogReducer, initialState);
+  const {forceTerminate, isOpen, onClose, onComplete, selectedIDs} = props;
+  const [state, dispatch] = React.useReducer(
+    terminationDialogReducer,
+    forceTerminate,
+    initializeState,
+  );
 
   const count = selectedIDs.length;
 
   React.useEffect(() => {
     if (isOpen) {
-      dispatch({type: 'reset'});
+      dispatch({type: 'reset', forceTerminate});
     }
-  }, [isOpen]);
+  }, [forceTerminate, isOpen]);
 
   const [terminate] = useMutation<Cancel>(CANCEL_MUTATION);
+  const policy = state.mustForce
+    ? TerminatePipelinePolicy.MARK_AS_CANCELED_IMMEDIATELY
+    : TerminatePipelinePolicy.SAFE_TERMINATE;
 
   const mutate = async () => {
     dispatch({type: 'start'});
 
     for (let ii = 0; ii < count; ii++) {
       const runId = selectedIDs[ii];
-      const {data} = await terminate({variables: {runId}});
+      const {data} = await terminate({variables: {runId, terminatePolicy: policy}});
 
       if (data?.terminatePipelineExecution.__typename === 'TerminatePipelineExecutionSuccess') {
         dispatch({type: 'termination-success'});
@@ -114,20 +128,67 @@ export const TerminationDialog = (props: Props) => {
     onComplete();
   };
 
+  const onToggleForce = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({type: 'toggle-force-terminate', checked: event.target.checked});
+  };
+
   const progressContent = () => {
     switch (state.step) {
       case 'initial':
         return (
-          <div>{`${count} ${
-            count === 1 ? 'run' : 'runs'
-          } will be terminated. Do you wish to continue?`}</div>
+          <Group direction="column" spacing={16}>
+            <div>
+              {state.mustForce
+                ? `${count} ${
+                    count === 1 ? 'run' : 'runs'
+                  } will be force terminated. Do you wish to continue?`
+                : `${count} ${
+                    count === 1 ? 'run' : 'runs'
+                  } will be terminated. Do you wish to continue?`}
+            </div>
+            {forceTerminate === 'always' ? (
+              <Group direction="row" spacing={8}>
+                <div style={{position: 'relative', top: '-1px'}}>
+                  <Icon icon="warning-sign" iconSize={13} color={Colors.GOLD3} />
+                </div>
+                <div>
+                  <strong>Warning:</strong> computational resources created by runs may not be fully
+                  cleaned up.
+                </div>
+              </Group>
+            ) : null}
+            {forceTerminate === 'optional' ? (
+              <div>
+                <Checkbox
+                  checked={state.mustForce}
+                  labelElement={
+                    <Box
+                      flex={{display: 'inline-flex'}}
+                      style={{position: 'relative', top: '-2px'}}
+                    >
+                      <Group direction="row" spacing={8}>
+                        <div style={{position: 'relative', top: '-1px'}}>
+                          <Icon icon="warning-sign" iconSize={13} color={Colors.GOLD3} />
+                        </div>
+                        <div>
+                          Force termination immediately. <strong>Warning:</strong> computational
+                          resources created by runs may not be fully cleaned up.
+                        </div>
+                      </Group>
+                    </Box>
+                  }
+                  onChange={onToggleForce}
+                />
+              </div>
+            ) : null}
+          </Group>
         );
       case 'terminating':
       case 'completed':
         const value = count > 0 ? state.termination.completed / count : 1;
         return (
           <Group direction="column" spacing={8}>
-            <div>Terminating…</div>
+            <div>{state.mustForce ? 'Forcing termination…' : 'Terminating…'}</div>
             <ProgressBar intent="primary" value={Math.max(0.1, value)} animate={value < 1} />
             {state.step === 'terminating' ? (
               <NavigationBlock message="Termination in progress, please do not navigate away yet." />
@@ -148,14 +209,18 @@ export const TerminationDialog = (props: Props) => {
               Cancel
             </Button>
             <Button intent="danger" onClick={mutate}>
-              {`Terminate ${`${count} ${count === 1 ? 'run' : 'runs'}`}`}
+              {`${state.mustForce ? 'Force terminate' : 'Terminate'} ${`${count} ${
+                count === 1 ? 'run' : 'runs'
+              }`}`}
             </Button>
           </>
         );
       case 'terminating':
         return (
           <Button intent="danger" disabled>
-            {`Terminating ${`${count} ${count === 1 ? 'run' : 'runs'}...`}`}
+            {state.mustForce
+              ? `Forcing termination for ${`${count} ${count === 1 ? 'run' : 'runs'}...`}`
+              : `Terminating ${`${count} ${count === 1 ? 'run' : 'runs'}...`}`}
           </Button>
         );
       case 'completed':
@@ -185,17 +250,28 @@ export const TerminationDialog = (props: Props) => {
         {successCount ? (
           <Group direction="row" spacing={8} alignItems="flex-start">
             <Icon icon="tick-circle" iconSize={16} color={Colors.GREEN3} />
-            <div>{`Successfully requested termination for ${successCount}
-              ${successCount === 1 ? 'run' : `runs`}.`}</div>
+            <div>
+              {state.mustForce
+                ? `Successfully forced termination for ${successCount}
+                ${successCount === 1 ? 'run' : `runs`}.`
+                : `Successfully requested termination for ${successCount}
+              ${successCount === 1 ? 'run' : `runs`}.`}
+            </div>
           </Group>
         ) : null}
         {errorCount ? (
           <Group direction="column" spacing={8}>
             <Group direction="row" spacing={8} alignItems="flex-start">
               <Icon icon="warning-sign" iconSize={16} color={Colors.GOLD3} />
-              <div>{`Could not request termination for ${errorCount} ${
-                errorCount === 1 ? 'run' : 'runs'
-              }:`}</div>
+              <div>
+                {state.mustForce
+                  ? `Could not force termination for ${errorCount} ${
+                      errorCount === 1 ? 'run' : 'runs'
+                    }:`
+                  : `Could not request termination for ${errorCount} ${
+                      errorCount === 1 ? 'run' : 'runs'
+                    }:`}
+              </div>
             </Group>
             <ul>
               {Object.keys(errors).map((runId) => (
