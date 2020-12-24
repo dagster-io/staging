f607b559a5d5ea9c36172e7d9ec687229d6ccc3c
Nate Kupp <nate@elementl.com>
2020-12-24 14:24:33 -0800

    Add Windows tests
    Test Plan: buildkite
    
    Differential Revision: https://dagster.phacility.com/D5767

--- .buildkite/dagster-buildkite/dagster_buildkite/defines.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/defines.py
@@ -1,7 +1,21 @@
+import os
+from enum import Enum
+
+
 class SupportedPython:
     V3_8 = "3.8.7"
     V3_7 = "3.7.9"
     V3_6 = "3.6.12"
+
+
+class BuildkiteQueue(Enum):
+    DOCKER = "buildkite-docker-v5-0-1"
+    MEDIUM = "buildkite-medium-v5-0-1"
+    WINDOWS = "buildkite-windows-v5-0-1"
+
+    @classmethod
+    def contains(cls, value):
+        return isinstance(value, cls)
 
 
 SupportedPythons = [
@@ -16,6 +30,16 @@ TOX_MAP = {
     SupportedPython.V3_7: "py37",
     SupportedPython.V3_6: "py36",
 }
+
+# Default build step timeout
+TIMEOUT_IN_MIN = 20
+
+# Versions of Docker and ECR plugins to use
+DOCKER_PLUGIN = "docker#v3.7.0"
+ECR_PLUGIN = "ecr#v2.2.0"
+
+AWS_ACCOUNT_ID = os.environ.get("AWS_ACCOUNT_ID")
+AWS_ECR_REGION = "us-west-2"
 
 
 # https://github.com/dagster-io/dagster/issues/1662
