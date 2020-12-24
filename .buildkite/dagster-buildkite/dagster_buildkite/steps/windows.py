e0587ff038e670401c1fe5b8bcf0f452128ae1df
Nate Kupp <nate@elementl.com>
2020-12-24 14:24:33 -0800

    Add Windows tests
    Test Plan: buildkite
    
    Differential Revision: https://dagster.phacility.com/D5767

---
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/windows.py
@@ -0,0 +1,15 @@
+from ..step_builder import StepBuilder
+
+
+def windows_steps():
+    return [
+        StepBuilder("dagster py38-windows-core_tests")
+        .run(
+            "cd python_modules/dagster",
+            "pip install -r dev-requirements.txt",
+            "tox -vv -e py38-windows-core_tests",
+        )
+        .on_windows_image(env=["BUILDKITE_BUILD_CHECKOUT_PATH=C:\\"])
+        .with_timeout(30)
+        .build()
+    ]
