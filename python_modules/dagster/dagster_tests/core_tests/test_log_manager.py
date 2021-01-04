ce929c5fc49f5cfa4c49e229bd5368aca953e92d
Sandy Ryza <sandy@elementl.com>
2021-01-04 13:10:54 -0800

    stop swallowing the causes of exceptions in log strings
    Summary:
    Prior, if an exception was raised from another exception.  The sub-exception would not be included in
    the logs.
    
    Test Plan: bk
    
    Reviewers: nate, yuhan, catherinewu
    
    Subscribers:

--- python_modules/dagster/dagster_tests/core_tests/test_log_manager.py
+++ python_modules/dagster/dagster_tests/core_tests/test_log_manager.py
@@ -79,7 +79,64 @@ def test_construct_log_string_with_error():
 
         ValueError: some error
 
+        Stack Trace:
           File "
         """
     ).strip()
     assert log_string.startswith(expected_start)
+
+
+def test_construct_log_string_with_error_raise_from():
+    try:
+        try:
+            raise ValueError("inner error")
+        except ValueError as e:
+            raise ValueError("outer error") from e
+    except ValueError:
+        error = serializable_error_info_from_exc_info(sys.exc_info())
+
+    step_failure_event = DagsterEvent(
+        event_type_value="STEP_FAILURE",
+        pipeline_name="my_pipeline",
+        step_key="solid2",
+        solid_handle=SolidHandle("solid2", None),
+        step_kind_value="COMPUTE",
+        logging_tags={},
+        event_specific_data=StepFailureData(error=error, user_failure_data=None),
+        message='Execution of step "solid2" failed.',
+        pid=54348,
+    )
+
+    message_props = {"dagster_event": step_failure_event, "pipeline_name": "my_pipeline"}
+
+    synth_props = {
+        "orig_message": step_failure_event.message,
+        "run_id": "f79a8a93-27f1-41b5-b465-b35d0809b26d",
+    }
+    log_string = construct_log_string(
+        message_props=message_props, logging_tags={}, synth_props=synth_props
+    )
+    expected_start = textwrap.dedent(
+        """
+        my_pipeline - f79a8a93-27f1-41b5-b465-b35d0809b26d - 54348 - STEP_FAILURE - Execution of step "solid2" failed.
+
+        ValueError: outer error
+
+        Stack Trace:
+          File "
+        """
+    ).strip()
+
+    assert log_string.startswith(expected_start)
+
+    expected_substr = textwrap.dedent(
+        """
+        The above exception was the direct cause of the following exception:
+        ValueError: inner error
+
+        Stack Trace:
+          File "
+        """
+    ).strip()
+
+    assert expected_substr in log_string
