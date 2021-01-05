365517da3e5ce92ebe044a9e938c9579dd59d444
Max Gasner <max.gasner@gmail.com>
2021-01-04 18:16:40 -0800

    Modernize dagstermill file handling
    Summary:
    This drops the bespoke file handling that Dagstermill previously used
    for output notebooks and for passing outputs back to solid code from the
    Jupyter process, instead using the file_manager and object_manager facilities.
    Documentation changes will follow after review of this basic approach.
    
    Test Plan: Unit
    
    Reviewers: sandyryza, yuhan, schrockn, alangenfeld, prha
    
    Differential Revision: https://dagster.phacility.com/D5829

--- python_modules/dagster/dagster/utils/__init__.py
+++ python_modules/dagster/dagster/utils/__init__.py
@@ -14,6 +14,7 @@ import tempfile
 import threading
 from collections import namedtuple
 from enum import Enum
+from typing import Iterator
 from warnings import warn
 
 import _thread as thread
@@ -257,7 +258,7 @@ def check_cli_execute_file_pipeline(path, pipeline_fn_name, env_file=None):
             raise cpe
 
 
-def safe_tempfile_path_unmanaged():
+def safe_tempfile_path_unmanaged() -> str:
     # This gets a valid temporary file path in the safest possible way, although there is still no
     # guarantee that another process will not create a file at this path. The NamedTemporaryFile is
     # deleted when the context manager exits and the file object is closed.
@@ -273,7 +274,7 @@ def safe_tempfile_path_unmanaged():
 
 
 @contextlib.contextmanager
-def safe_tempfile_path():
+def safe_tempfile_path() -> Iterator[str]:
     try:
         path = safe_tempfile_path_unmanaged()
         yield path
---
+++ python_modules/libraries/dagstermill/dagstermill/io.py
@@ -0,0 +1,85 @@
+"""We use the IOManager facilities to pass input and output values across the process boundary
+to/from the Dagstermill kernel."""
+
+from dagster.core.execution.context.compute import AbstractComputeExecutionContext
+from dagster.core.execution.context.system import (
+    InputContext,
+    OutputContext,
+    SystemStepExecutionContext,
+)
+from dagster.core.execution.plan.outputs import StepOutput
+
+from .context import DagstermillRuntimeExecutionContext
+
+
+def synthetic_output_context(
+    solid_context: AbstractComputeExecutionContext,
+    step_context: SystemStepExecutionContext,
+    output_name: str,
+):
+    step_output = step_context.step.step_output_named(output_name)
+
+    return OutputContext(
+        step_key=step_context.step.key,
+        name=output_name,
+        pipeline_name=solid_context.pipeline_run.pipeline_name,
+        run_id=solid_context.run_id,
+        metadata=step_output.output_def.metadata,
+        # mapping_key would come from DynamicOutput, not clear how we would do that here
+        # risk of collision if we map over a dagstermill solid?
+        mapping_key=None,
+        config=(solid_context.solid_config or {}).get("outputs"),
+        solid_def=solid_context.solid_def,
+        dagster_type=step_output.output_def.dagster_type,
+        log_manager=solid_context.log,
+        version=None,
+    )
+
+
+def synthetic_input_context(
+    solid_context: AbstractComputeExecutionContext,
+    step_context: SystemStepExecutionContext,
+    output_name: str,
+):
+    step_output = step_context.step.step_output_named(output_name)
+
+    upstream_output = synthetic_output_context(
+        solid_context=solid_context, step_context=step_context, output_name=output_name,
+    )
+
+    return InputContext(
+        name=None,
+        pipeline_name=solid_context.pipeline_run.pipeline_name,
+        solid_def=None,
+        config=(solid_context.solid_config or {}).get("outputs"),
+        metadata=step_output.output_def.metadata,
+        upstream_output=upstream_output,
+        dagster_type=step_output.output_def.dagster_type,
+        log_manager=solid_context.log,
+        # Rather than construct a synthetic step context (since this is in fact not an input)
+        # we pass None
+        step_context=None,
+    )
+
+
+def write_notebook_output(
+    solid_context: DagstermillRuntimeExecutionContext, output_name: str, value
+):
+    output_context = synthetic_output_context(
+        solid_context=solid_context,
+        step_context=solid_context._step_context,  # pylint: disable=protected-access
+        output_name=output_name,
+    )
+    solid_context.resources.io_manager.handle_output(context=output_context, obj=value)
+
+
+def read_notebook_output(
+    solid_context: AbstractComputeExecutionContext,
+    step_context: SystemStepExecutionContext,
+    output_name: str,
+):
+    input_context = synthetic_input_context(
+        solid_context=solid_context, step_context=step_context, output_name=output_name
+    )
+
+    return solid_context.resources.io_manager.load_input(context=input_context)
