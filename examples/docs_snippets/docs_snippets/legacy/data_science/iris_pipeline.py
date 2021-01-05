fd74979ed6c60c867acf9e4fedfb0363f13ab1b4
Max Gasner <max.gasner@gmail.com>
2021-01-04 18:16:40 -0800

    Modernize dagstermill file handling
    Summary:
    This drops the bespoke file handling that Dagstermill previously used
    for output notebooks and for passing outputs back to solid code from the
    Jupyter process, instead using the file_manager and object_manager facilities.
    Documentation changes will follow after review of this basic approach.
    
    Test Plan: Unit
    
    Reviewers: sandyryza, yuhan, schrockn, alangenfeld, prha
    
    Differential Revision: https://dagster.phacility.com/D5829

--- examples/docs_snippets/docs_snippets/legacy/data_science/iris_pipeline.py
+++ examples/docs_snippets/docs_snippets/legacy/data_science/iris_pipeline.py
@@ -1,5 +1,5 @@
 import dagstermill as dm
-from dagster import pipeline
+from dagster import ModeDefinition, fs_object_manager, pipeline
 from dagster.utils import script_relative_path
 
 k_means_iris = dm.define_dagstermill_solid(
@@ -7,6 +7,6 @@ k_means_iris = dm.define_dagstermill_solid(
 )
 
 
-@pipeline
+@pipeline(mode_defs=[ModeDefinition(resource_defs={"object_manager": fs_object_manager})])
 def iris_pipeline():
     k_means_iris()
