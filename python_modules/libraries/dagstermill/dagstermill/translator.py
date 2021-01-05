6c68348047c0268c222e34acc958637c61e52ae0
Max Gasner <max.gasner@gmail.com>
2021-01-04 18:16:40 -0800

    Modernize dagstermill file handling
    Summary:
    This drops the bespoke file handling that Dagstermill previously used
    for output notebooks and for passing outputs back to solid code from the
    Jupyter process, instead using the file_manager and object_manager facilities.
    Documentation changes will follow after review of this basic approach.
    
    Test Plan: Unit
    
    Reviewers: sandyryza, yuhan, schrockn, alangenfeld, prha
    
    Differential Revision: https://dagster.phacility.com/D5829

--- python_modules/libraries/dagstermill/dagstermill/translator.py
+++ python_modules/libraries/dagstermill/dagstermill/translator.py
@@ -1,60 +1,53 @@
-import papermill
-from dagster import seven
+from typing import Any, Dict
 
-RESERVED_INPUT_NAMES = [
-    "__dm_context",
-    "__dm_dagstermill",
-    "__dm_executable_dict",
-    "__dm_json",
-    "__dm_pipeline_run_dict",
-    "__dm_solid_handle_kwargs",
-    "__dm_instance_ref_dict",
-]
+import papermill
+from dagster import check, seven
 
 INJECTED_BOILERPLATE = """
 # Injected parameters
-from dagster import seven as __dm_seven
-import dagstermill as __dm_dagstermill
-context = __dm_dagstermill._reconstitute_pipeline_context(
+from dagster import seven as __dagstermill_seven
+import dagstermill as __dagstermill_dagstermill
+context = __dagstermill_dagstermill._reconstitute_pipeline_context(
     **{{
-        key: __dm_seven.json.loads(value)
+        key: __dagstermill_seven.json.loads(value)
         for key, value
         in {pipeline_context_args}.items()
-    }}
+    }},
+    step_key="{step_key}",
 )
 """
 
 
 class DagsterTranslator(papermill.translators.PythonTranslator):
     @classmethod
-    def codify(cls, parameters):
-        assert "__dm_context" in parameters
-        assert "__dm_executable_dict" in parameters
-        assert "__dm_pipeline_run_dict" in parameters
-        assert "__dm_solid_handle_kwargs" in parameters
-        assert "__dm_instance_ref_dict" in parameters
+    def codify(cls, parameters: Dict[str, Any]):
+        check.dict_elem(parameters, "context_dict")
+        check.dict_elem(parameters, "executable_dict")
+        check.dict_elem(parameters, "pipeline_run_dict")
+        check.dict_elem(parameters, "solid_handle_kwargs")
+        check.dict_elem(parameters, "instance_ref_dict")
+        check.list_elem(parameters, "input_names")
+        check.str_elem(parameters, "step_key")
 
-        context_args = parameters["__dm_context"]
+        context_args = parameters["context_dict"]
         pipeline_context_args = dict(
-            executable_dict=parameters["__dm_executable_dict"],
-            pipeline_run_dict=parameters["__dm_pipeline_run_dict"],
-            solid_handle_kwargs=parameters["__dm_solid_handle_kwargs"],
-            instance_ref_dict=parameters["__dm_instance_ref_dict"],
+            executable_dict=parameters["executable_dict"],
+            pipeline_run_dict=parameters["pipeline_run_dict"],
+            solid_handle_kwargs=parameters["solid_handle_kwargs"],
+            instance_ref_dict=parameters["instance_ref_dict"],
             **context_args,
         )
 
         for key in pipeline_context_args:
             pipeline_context_args[key] = seven.json.dumps(pipeline_context_args[key])
 
-        content = INJECTED_BOILERPLATE.format(pipeline_context_args=pipeline_context_args)
+        content = INJECTED_BOILERPLATE.format(
+            pipeline_context_args=pipeline_context_args, step_key=parameters["step_key"]
+        )
 
-        for name, val in parameters.items():
-            if name in RESERVED_INPUT_NAMES:
-                continue
-            dm_unmarshal_call = "__dm_dagstermill._load_parameter('{name}', '{val}')".format(
-                name=name, val=seven.json.dumps(val)
-            )
-            content += "{}\n".format(cls.assign(name, dm_unmarshal_call))
+        for input_name in parameters["input_names"]:
+            dm_unmarshal_call = f"__dagstermill_dagstermill._load_input('{input_name}')"
+            content += "{}\n".format(cls.assign(input_name, dm_unmarshal_call))
 
         return content
 
