6da32e66c37fe7268df9afda120ee0fad294c005
Nate Kupp <nate@elementl.com>
2021-01-06 12:06:46 -0800

    RFC - Schema changes for future MySQL support
    Test Plan: TBD
    
    Differential Revision: https://dagster.phacility.com/D5805

---
+++ python_modules/libraries/dagster-postgres/dagster_postgres/event_log/alembic/versions/4ea2b1f6f67b_change_varchar_to_text.py
@@ -0,0 +1,52 @@
+"""change varchar to text
+
+Revision ID: 4ea2b1f6f67b
+Revises: f3e43ff66603
+Create Date: 2021-01-12 12:39:53.493651
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.engine import reflection
+
+# pylint: disable=no-member
+
+# revision identifiers, used by Alembic.
+revision = "4ea2b1f6f67b"
+down_revision = "f3e43ff66603"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+
+    if "runs" in has_tables:
+        with op.batch_alter_table("runs") as batch_op:
+            batch_op.alter_column(
+                sa.Column("pipeline_name"), type_=sa.Text, existing_type=sa.String
+            )
+            batch_op.alter_column(sa.Column("run_body"), type_=sa.Text, existing_type=sa.String)
+            batch_op.alter_column(sa.Column("partition"), type_=sa.Text, existing_type=sa.String)
+            batch_op.alter_column(
+                sa.Column("partition_set"), type_=sa.Text, existing_type=sa.String
+            )
+
+
+def downgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+
+    if "runs" in has_tables:
+        with op.batch_alter_table("runs") as batch_op:
+            batch_op.alter_column(
+                sa.Column("pipeline_name"), type_=sa.String, existing_type=sa.Text
+            )
+            batch_op.alter_column(sa.Column("run_body"), type_=sa.String, existing_type=sa.Text)
+            batch_op.alter_column(sa.Column("partition"), type_=sa.String, existing_type=sa.Text)
+            batch_op.alter_column(
+                sa.Column("partition_set"), type_=sa.String, existing_type=sa.Text
+            )
