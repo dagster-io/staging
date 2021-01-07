f393e1c8fffa7147002219f795842d15ef2e967f
Nate Kupp <nate@elementl.com>
2021-01-07 14:52:29 -0800

    Trigger internal pipeline build from dagster pipeline build
    Test Plan: buildkite
    
    Differential Revision: https://dagster.phacility.com/D5871

---
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/trigger.py
@@ -0,0 +1,17 @@
+from typing import Dict, List, Optional
+
+
+def trigger_step(
+    pipeline: str, branches: Optional[List[str]] = None, async_step: bool = False
+) -> Dict:
+    """trigger_step: Trigger a build of another pipeline.
+    """
+    step = {
+        "trigger": pipeline,
+        "label": f":rocket: {pipeline}",
+        "async": async_step,
+    }
+    if branches:
+        step["branches"] = " ".join(branches)
+
+    return step
