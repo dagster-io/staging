12a640a29a9510633a6a1638c853056fdb563c30
Yichen Dai <yichen@elementl.com>
2021-01-12 00:14:46 -0800

    [Draft] Add an example of how to trigger a pipeline by graphQL
    Summary: Use a script to kick off a pipeline run.
    
    Test Plan: Plan to add
    
    Differential Revision: https://dagster.phacility.com/D5949

---
+++ examples/trigger_pipeline/tests/test_trigger_pipeline.py
@@ -0,0 +1,34 @@
+from dagster import execute_pipeline
+from dagster_graphql.client.query import LAUNCH_PIPELINE_EXECUTION_MUTATION
+from dagster_graphql.test.utils import execute_dagster_graphql
+from trigger_pipeline.execute_pipeline_graphql.repo import do_math
+from trigger_pipeline.execute_pipeline_graphql.trigger import (
+    graphql_context,
+    run_config_data,
+    selector,
+)
+
+
+def test_trigger_do_math_pipeline():
+    res = execute_pipeline(
+        do_math,
+        run_config={
+            "solids": {"add_one": {"inputs": {"num": 5}}, "add_two": {"inputs": {"num": 6}},}
+        },
+    )
+    assert res.success
+
+
+def test_trigger_pipeline_by_gql():
+    result = execute_dagster_graphql(
+        graphql_context,
+        LAUNCH_PIPELINE_EXECUTION_MUTATION,
+        variables={
+            "executionParams": {
+                "selector": selector,
+                "runConfigData": run_config_data,
+                "mode": "default",
+            }
+        },
+    )
+    assert result.data["launchPipelineExecution"]["__typename"] == "LaunchPipelineRunSuccess"
