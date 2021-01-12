5f25a021e7a724333e9daae96ceb2d7b93f53d89
Yichen Dai <yichen@elementl.com>
2021-01-12 00:14:46 -0800

    [Draft] Add an example of how to trigger a pipeline by graphQL
    Summary: Use a script to kick off a pipeline run.
    
    Test Plan: Plan to add
    
    Differential Revision: https://dagster.phacility.com/D5949

---
+++ examples/trigger_pipeline/trigger_pipeline/trigger.py
@@ -0,0 +1,44 @@
+import sys
+
+from dagster import DagsterInstance, file_relative_path
+from dagster.cli.workspace.workspace import Workspace
+from dagster.core.host_representation.origin import ManagedGrpcPythonEnvRepositoryLocationOrigin
+from dagster.core.host_representation.selector import PipelineSelector
+from dagster.core.types.loadable_target_origin import LoadableTargetOrigin
+from dagster_graphql.client.query import LAUNCH_PIPELINE_EXECUTION_MUTATION
+from dagster_graphql.implementation.context import DagsterGraphQLContext
+from dagster_graphql.test.utils import execute_dagster_graphql
+
+from .repo import my_repo
+
+repo_location_origin = ManagedGrpcPythonEnvRepositoryLocationOrigin(
+    LoadableTargetOrigin(
+        executable_path=sys.executable, python_file=file_relative_path(__file__, "./repo.py"),
+    )
+)
+
+selector = PipelineSelector(
+    location_name=repo_location_origin.location_name,
+    repository_name=my_repo.name,
+    pipeline_name="do_math",
+    solid_selection=None,
+).to_graphql_input()
+
+workspace = Workspace([repo_location_origin])
+run_config_data = {
+    "solids": {"add_one": {"inputs": {"num": 5}}, "add_two": {"inputs": {"num": 6}},}
+}
+
+
+graphql_context = DagsterGraphQLContext(workspace=workspace, instance=DagsterInstance.get())
+result = execute_dagster_graphql(
+    graphql_context,
+    LAUNCH_PIPELINE_EXECUTION_MUTATION,
+    variables={
+        "executionParams": {
+            "selector": selector,
+            "runConfigData": run_config_data,
+            "mode": "default",
+        }
+    },
+)
