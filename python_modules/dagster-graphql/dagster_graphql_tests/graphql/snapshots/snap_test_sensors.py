a1175fc88a40ec1a76b7299cf6cfe5e5dcb21671
Sashank Thupukari <s@thupukari.com>
2021-01-13 11:17:24 -0500

    (1/n request-context-refactor) Split DagterGrqphQLContext into ProcessContext and RequestContext
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D5953

--- python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_sensors.py
+++ python_modules/dagster-graphql/dagster_graphql_tests/graphql/snapshots/snap_test_sensors.py
@@ -6,56 +6,35 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['TestSensors.test_get_sensor[readonly_in_memory_instance_managed_grpc_env] 1'] = {
-    '__typename': 'Sensor',
-    'id': 'always_no_config_sensor:no_config_pipeline',
-    'mode': 'default',
-    'name': 'always_no_config_sensor',
-    'nextTick': None,
-    'pipelineName': 'no_config_pipeline',
-    'sensorState': {
-        'runs': [
-        ],
-        'runsCount': 0,
-        'status': 'STOPPED',
-        'ticks': [
-        ]
-    },
-    'solidSelection': None
+snapshots["TestSensors.test_get_sensor[readonly_in_memory_instance_managed_grpc_env] 1"] = {
+    "__typename": "Sensor",
+    "id": "always_no_config_sensor:no_config_pipeline",
+    "mode": "default",
+    "name": "always_no_config_sensor",
+    "nextTick": None,
+    "pipelineName": "no_config_pipeline",
+    "sensorState": {"runs": [], "runsCount": 0, "status": "STOPPED", "ticks": []},
+    "solidSelection": None,
 }
 
-snapshots['TestSensors.test_get_sensor[readonly_in_memory_instance_multi_location] 1'] = {
-    '__typename': 'Sensor',
-    'id': 'always_no_config_sensor:no_config_pipeline',
-    'mode': 'default',
-    'name': 'always_no_config_sensor',
-    'nextTick': None,
-    'pipelineName': 'no_config_pipeline',
-    'sensorState': {
-        'runs': [
-        ],
-        'runsCount': 0,
-        'status': 'STOPPED',
-        'ticks': [
-        ]
-    },
-    'solidSelection': None
+snapshots["TestSensors.test_get_sensor[readonly_in_memory_instance_multi_location] 1"] = {
+    "__typename": "Sensor",
+    "id": "always_no_config_sensor:no_config_pipeline",
+    "mode": "default",
+    "name": "always_no_config_sensor",
+    "nextTick": None,
+    "pipelineName": "no_config_pipeline",
+    "sensorState": {"runs": [], "runsCount": 0, "status": "STOPPED", "ticks": []},
+    "solidSelection": None,
 }
 
-snapshots['TestSensors.test_get_sensor[readonly_sqlite_instance_in_process_env] 1'] = {
-    '__typename': 'Sensor',
-    'id': 'always_no_config_sensor:no_config_pipeline',
-    'mode': 'default',
-    'name': 'always_no_config_sensor',
-    'nextTick': None,
-    'pipelineName': 'no_config_pipeline',
-    'sensorState': {
-        'runs': [
-        ],
-        'runsCount': 0,
-        'status': 'STOPPED',
-        'ticks': [
-        ]
-    },
-    'solidSelection': None
+snapshots["TestSensors.test_get_sensor[readonly_sqlite_instance_in_process_env] 1"] = {
+    "__typename": "Sensor",
+    "id": "always_no_config_sensor:no_config_pipeline",
+    "mode": "default",
+    "name": "always_no_config_sensor",
+    "nextTick": None,
+    "pipelineName": "no_config_pipeline",
+    "sensorState": {"runs": [], "runsCount": 0, "status": "STOPPED", "ticks": []},
+    "solidSelection": None,
 }
