326415c3a2f5e78d650ec9bfc00fda0b740dd634
Nate Kupp <nate@elementl.com>
2021-01-13 15:38:09 -0800

    RFC - Schema changes for future MySQL support
    Test Plan: - Tested incorrect `db.String` column and new buildkite check fails for it: https://buildkite.com/dagster/dagster-diffs/builds/13894
    
    Subscribers:
    
    Differential Revision: https://dagster.phacility.com/D5805
    
    Co-authored-by: Rex Ledesma <rex@elementl.com>

---
+++ python_modules/libraries/dagster-postgres/dagster_postgres/schedule_storage/alembic/versions/4ea2b1f6f67b_change_varchar_to_text.py
@@ -0,0 +1,46 @@
+"""change varchar to text
+
+Revision ID: 4ea2b1f6f67b
+Revises: b32a4f3036d2
+Create Date: 2021-01-12 12:39:53.493651
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.engine import reflection
+
+# pylint: disable=no-member
+
+# revision identifiers, used by Alembic.
+revision = "4ea2b1f6f67b"
+down_revision = "b32a4f3036d2"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+
+    if "jobs" in has_tables:
+        with op.batch_alter_table("jobs") as batch_op:
+            batch_op.alter_column(sa.Column("job_body"), type_=sa.Text, existing_type=sa.String)
+
+    if "job_ticks" in has_tables:
+        with op.batch_alter_table("job_ticks") as batch_op:
+            batch_op.alter_column(sa.Column("tick_body"), type_=sa.Text, existing_type=sa.String)
+
+
+def downgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+
+    if "jobs" in has_tables:
+        with op.batch_alter_table("jobs") as batch_op:
+            batch_op.alter_column(sa.Column("job_body"), type_=sa.String, existing_type=sa.Text)
+
+    if "job_ticks" in has_tables:
+        with op.batch_alter_table("job_ticks") as batch_op:
+            batch_op.alter_column(sa.Column("tick_body"), type_=sa.String, existing_type=sa.Text)
