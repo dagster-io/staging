b93f702347b1a9f95ae1a02e7abd83645ccdc59d
Nate Kupp <nate@elementl.com>
2021-01-13 15:38:09 -0800

    RFC - Schema changes for future MySQL support
    Test Plan: - Tested incorrect `db.String` column and new buildkite check fails for it: https://buildkite.com/dagster/dagster-diffs/builds/13894
    
    Subscribers:
    
    Differential Revision: https://dagster.phacility.com/D5805

---
+++ python_modules/dagster/dagster/core/storage/schedules/sqlite/alembic/versions/0da417ae1b81_change_varchar_to_text.py
@@ -0,0 +1,52 @@
+"""change varchar to text
+
+Revision ID: 0da417ae1b81
+Revises: 375e95bad550
+Create Date: 2021-01-12 12:29:33.410870
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.engine import reflection
+
+# pylint: disable=no-member
+
+# revision identifiers, used by Alembic.
+revision = "0da417ae1b81"
+down_revision = "375e95bad550"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+
+    if "runs" in has_tables:
+        with op.batch_alter_table("runs") as batch_op:
+            batch_op.alter_column(
+                sa.Column("pipeline_name"), type_=sa.Text, existing_type=sa.String
+            )
+            batch_op.alter_column(sa.Column("run_body"), type_=sa.Text, existing_type=sa.String)
+            batch_op.alter_column(sa.Column("partition"), type_=sa.Text, existing_type=sa.String)
+            batch_op.alter_column(
+                sa.Column("partition_set"), type_=sa.Text, existing_type=sa.String
+            )
+
+
+def downgrade():
+    bind = op.get_context().bind
+    inspector = reflection.Inspector.from_engine(bind)
+    has_tables = inspector.get_table_names()
+
+    if "runs" in has_tables:
+        with op.batch_alter_table("runs") as batch_op:
+            batch_op.alter_column(
+                sa.Column("pipeline_name"), type_=sa.String, existing_type=sa.Text
+            )
+            batch_op.alter_column(sa.Column("run_body"), type_=sa.String, existing_type=sa.Text)
+            batch_op.alter_column(sa.Column("partition"), type_=sa.String, existing_type=sa.Text)
+            batch_op.alter_column(
+                sa.Column("partition_set"), type_=sa.String, existing_type=sa.Text
+            )
