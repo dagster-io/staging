7188659291046c887e70faace7afc479d2484969
Sandy Ryza <sandy@elementl.com>
2021-01-15 10:45:52 -0800

    indent error framing
    Summary: include context
    
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D6033

--- python_modules/dagster/dagster_tests/core_tests/test_errors.py
+++ python_modules/dagster/dagster_tests/core_tests/test_errors.py
@@ -1,5 +1,6 @@
 import sys
 
+from dagster import execute_pipeline, pipeline, solid
 from dagster.utils.error import serializable_error_info_from_exc_info
 
 
@@ -24,4 +25,29 @@ foo = bar
         ^
 SyntaxError: invalid syntax
 """
+    )
+
+
+def test_solid_raises_error_stdout():
+    @solid
+    def fails(_):
+        raise ValueError("abc")
+
+    @pipeline
+    def my_pipeline():
+        fails()
+
+    result = execute_pipeline(my_pipeline, raise_on_error=False)
+    assert not result.result_for_solid("fails").success
+    error_message = result.result_for_solid("fails").failure_data.error.message
+    assert (
+        error_message.strip()
+        == """
+Error occurred during the execution of step:
+    step key: "fails"
+    solid invocation: "fails"
+    solid definition: "fails"
+
+ValueError: abc
+""".strip()
     )
