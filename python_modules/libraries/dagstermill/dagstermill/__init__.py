fea683707b1e506c0e34c018f8c72f79e79a5ae1
Max Gasner <max.gasner@gmail.com>
2021-01-20 15:50:31 -0800

    wip
    Test Plan: Unit
    
    Differential Revision: https://dagster.phacility.com/D6098

--- python_modules/libraries/dagstermill/dagstermill/__init__.py
+++ python_modules/libraries/dagstermill/dagstermill/__init__.py
@@ -1,9 +1,10 @@
 from dagster.core.utils import check_dagster_package_version
 
 from .context import DagstermillExecutionContext
+from .deprecated import define_dagstermill_solid
 from .errors import DagstermillError
 from .manager import MANAGER_FOR_NOTEBOOK_INSTANCE as _MANAGER_FOR_NOTEBOOK_INSTANCE
-from .solids import define_dagstermill_solid
+from .solids import dagstermill_solid
 from .version import __version__
 
 check_dagster_package_version("dagstermill", __version__)
@@ -13,6 +14,8 @@ get_context = _MANAGER_FOR_NOTEBOOK_INSTANCE.get_context
 yield_result = _MANAGER_FOR_NOTEBOOK_INSTANCE.yield_result
 
 yield_event = _MANAGER_FOR_NOTEBOOK_INSTANCE.yield_event
+
+_load_input = _MANAGER_FOR_NOTEBOOK_INSTANCE.load_input
 
 _reconstitute_pipeline_context = _MANAGER_FOR_NOTEBOOK_INSTANCE.reconstitute_pipeline_context
 
