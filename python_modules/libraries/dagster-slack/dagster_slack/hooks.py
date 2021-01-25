1104db93e4df457103d17ba09bbf78c779b55682
Owen Alexander Kephart <owen@owens-mbp.lan>
2021-01-25 07:40:46 -0800

    Initial commit of adding hooks to some of the integration libraries
    Summary: Add hooks for integration libraries
    
    Test Plan: Adding pytests (will add PD tests in next commit)
    
    Reviewers: prha
    
    Differential Revision: https://dagster.phacility.com/D6140

---
+++ python_modules/libraries/dagster-slack/dagster_slack/hooks.py
@@ -0,0 +1,55 @@
+from typing import Callable
+
+from dagster.core.definitions import failure_hook, success_hook
+from dagster.core.definitions.decorators.hook import failure_message, success_message
+from dagster.core.execution.context.system import HookContext
+
+
+def send_slack_message_on_failure(
+    channel: str, message_fn: Callable[[HookContext], str] = failure_message
+):
+    @failure_hook(required_resource_keys={'slack'})
+    def _hook(context: HookContext):
+        context.resources.slack.chat_postMessage(channel=channel, text=message_fn(context))
+
+    return _hook
+
+
+def send_slack_message_on_success(
+    channel: str, message_fn: Callable[[HookContext], str] = success_message
+):
+    """Create a hook on step success events that will message the given Slack channel.
+
+    Args:
+        channel (str): The channel to send the message to (e.g. "#my_channel")
+        message_fun (Optional(Callable[[HookContext], str])): Function which takes in the HookContext
+            outputs the message you want to send.
+
+    Examples:
+        .. code-block:: python
+
+            @slack_on_success("#foo")
+            @pipeline(...)
+            def my_pipeline():
+                pass
+
+        .. code-block:: python
+
+            def my_message_fn(context: HookContext) -> str:
+                return "TODO: come up with something somewhat interesting"
+
+            @solid
+            def a_solid(context):
+                pass
+
+            @pipeline(...)
+            def my_pipeline():
+                a_solid.with_hooks(hook_defs={slack_on_success("#foo", my_message_fn)})
+
+    """
+
+    @success_hook(required_resource_keys={'slack'})
+    def _hook(context: HookContext):
+        context.resources.slack.chat_postMessage(channel=channel, text=message_fn(context))
+
+    return _hook
---
+++ python_modules/libraries/dagster-slack/dagster_slack_tests/test_hooks.py
@@ -0,0 +1,124 @@
+from dagster import ModeDefinition, execute_pipeline, execute_solid, pipeline, solid
+from dagster_slack import slack_resource
+from dagster_slack.hooks import send_slack_message_on_failure, send_slack_message_on_success
+from mock import patch
+
+
+class SomeUserException(Exception):
+    pass
+
+
+@patch("slack.web.base_client.BaseClient._perform_urllib_http_request")
+def test_failure_hook_on_solid_instance(mock_urllib_http_request):
+    @solid
+    def pass_solid(_):
+        pass
+
+    @solid
+    def fail_solid(_):
+        raise SomeUserException()
+
+    @pipeline(mode_defs=[ModeDefinition(resource_defs={"slack": slack_resource})])
+    def a_pipeline():
+        pass_solid.with_hooks(hook_defs={send_slack_message_on_failure('#foo')})()
+        pass_solid.alias("solid_with_hook").with_hooks(
+            hook_defs={send_slack_message_on_failure('#foo')}
+        )()
+        fail_solid.alias("fail_solid_without_hook")()
+        fail_solid.with_hooks(hook_defs={send_slack_message_on_failure('#foo')})()
+
+    result = execute_pipeline(
+        a_pipeline,
+        run_config={
+            "resources": {"slack": {"config": {"token": "xoxp-1234123412341234-12341234-1234"}}}
+        },
+        raise_on_error=False,
+    )
+    assert not result.success
+    assert mock_urllib_http_request.call_count == 1
+
+
+@patch("slack.web.base_client.BaseClient._perform_urllib_http_request")
+def test_failure_hook_decorator(mock_urllib_http_request):
+    @solid
+    def pass_solid(_):
+        pass
+
+    @solid
+    def fail_solid(_):
+        raise SomeUserException()
+
+    @send_slack_message_on_failure("#foo")
+    @pipeline(mode_defs=[ModeDefinition(resource_defs={"slack": slack_resource})])
+    def a_pipeline():
+        pass_solid()
+        fail_solid()
+        fail_solid.alias("another_fail_solid")()
+
+    result = execute_pipeline(
+        a_pipeline,
+        run_config={
+            "resources": {"slack": {"config": {"token": "xoxp-1234123412341234-12341234-1234"}}}
+        },
+        raise_on_error=False,
+    )
+    assert not result.success
+    assert mock_urllib_http_request.call_count == 2
+
+
+@patch("slack.web.base_client.BaseClient._perform_urllib_http_request")
+def test_success_hook_on_solid_instance(mock_urllib_http_request):
+    @solid
+    def pass_solid(_):
+        pass
+
+    @solid
+    def fail_solid(_):
+        raise SomeUserException()
+
+    @pipeline(mode_defs=[ModeDefinition(resource_defs={"slack": slack_resource})])
+    def a_pipeline():
+        pass_solid.with_hooks(hook_defs={send_slack_message_on_success('#foo')})()
+        pass_solid.alias("solid_with_hook").with_hooks(
+            hook_defs={send_slack_message_on_success('#foo')}
+        )()
+        pass_solid.alias("solid_without_hook")()
+        fail_solid.with_hooks(hook_defs={send_slack_message_on_success('#foo')})()
+
+    result = execute_pipeline(
+        a_pipeline,
+        run_config={
+            "resources": {"slack": {"config": {"token": "xoxp-1234123412341234-12341234-1234"}}}
+        },
+        raise_on_error=False,
+    )
+    assert not result.success
+    assert mock_urllib_http_request.call_count == 2
+
+
+@patch("slack.web.base_client.BaseClient._perform_urllib_http_request")
+def test_success_hook_decorator(mock_urllib_http_request):
+    @solid
+    def pass_solid(_):
+        pass
+
+    @solid
+    def fail_solid(_):
+        raise SomeUserException()
+
+    @send_slack_message_on_success("#foo")
+    @pipeline(mode_defs=[ModeDefinition(resource_defs={"slack": slack_resource})])
+    def a_pipeline():
+        pass_solid()
+        pass_solid.alias("another_pass_solid")()
+        fail_solid()
+
+    result = execute_pipeline(
+        a_pipeline,
+        run_config={
+            "resources": {"slack": {"config": {"token": "xoxp-1234123412341234-12341234-1234"}}}
+        },
+        raise_on_error=False,
+    )
+    assert not result.success
+    assert mock_urllib_http_request.call_count == 2
