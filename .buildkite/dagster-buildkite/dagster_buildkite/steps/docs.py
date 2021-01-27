c2f416861b3db5d9bf4791e11df929033bc82f2d
Sashank Thupukari <s@thupukari.com>
2021-01-27 13:47:32 -0500

    (new-docs-6) Syntax highligting
    Summary: Syntax highlighting and snapshotting for literalincludes
    
    Test Plan: unit
    
    Reviewers: yuhan
    
    Differential Revision: https://dagster.phacility.com/D6190

--- .buildkite/dagster-buildkite/dagster_buildkite/steps/docs.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/steps/docs.py
@@ -1,7 +1,9 @@
+# pylint: disable=anomalous-backslash-in-string
+
 from typing import List
 
-# from ..defines import SupportedPython
-# from ..step_builder import StepBuilder
+from ..defines import SupportedPython
+from ..step_builder import StepBuilder
 
 
 def docs_steps() -> List[dict]:
@@ -16,4 +18,16 @@ def docs_steps() -> List[dict]:
         # )
         # .on_integration_image(SupportedPython.V3_7)
         # .build(),
+        StepBuilder("docs code snapshots")
+        .run(
+            "pushd docs; make snapshot",
+            "export DAGSTER_REPO='\$(pwd)",
+            "git diff --exit-code",
+            "[[ \$? = 1 ]] && echo -e '--- Snapshot failed' && echo 'This test is failing because you have either "
+            "(1) Updated the code used in a literalinclude or "
+            "(2) Updated a literalinclude snapshot instead of updating the reference. "
+            "Run make snapshots in the /docs directory to fix this error.'",
+        )
+        .on_integration_image(SupportedPython.V3_7)
+        .build(),
     ]
