32e0304185c1383d251567b239ef08eb3bea5e77
Bob Chen <bob@elementl.com>
2021-01-27 10:04:42 -0500

    WIP: dagster new Project Skeletons
    Summary: Some examples of Dagster project skeletons that would be generated by `dagster new`.
    
    Test Plan: n/a
    
    Reviewers:
    
    Subscribers:

---
+++ project-skeletons/dagster-style/my_project/pipelines.py
@@ -0,0 +1,12 @@
+from dagster import ModeDefinition, pipeline
+
+from .solids import hello
+
+MODE_DEV = ModeDefinition(name="dev", resource_defs={})
+MODE_TEST = ModeDefinition(name="test", resource_defs={})
+MODE_PROD = ModeDefinition(name="prod", resource_defs={})
+
+
+@pipeline(mode_defs=[MODE_DEV, MODE_TEST, MODE_PROD])
+def my_pipeline():
+    hello()
