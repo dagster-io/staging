b54f1a279df89d80c1bd48a1b7b13d2fbcf35d4c
Bob Chen <bob@elementl.com>
2021-02-04 17:07:56 -0500

    Changes Mode's multiline string to Python comment.

--- project-skeletons/examplerepo/examplerepo/pipelines/my_pipeline.py
+++ project-skeletons/examplerepo/examplerepo/pipelines/my_pipeline.py
@@ -2,13 +2,9 @@ from dagster import ModeDefinition, pipeline
 
 from ..solids import hello
 
-"""
-Mode definitions allow you to configure the behavior of your pipelines and solids at execution
-time.
-
-For hints on creating modes in Dagster, see our documentation overview on Modes and Resources:
-https://docs.dagster.io/overview/modes-resources-presets/modes-resources
-"""
+# Mode definitions allow you to configure the behavior of your pipelines and solids at execution
+# time. For hints on creating modes in Dagster, see our documentation overview on Modes and
+# Resources: https://docs.dagster.io/overview/modes-resources-presets/modes-resources
 MODE_DEV = ModeDefinition(name="dev", resource_defs={})
 MODE_TEST = ModeDefinition(name="test", resource_defs={})
 
