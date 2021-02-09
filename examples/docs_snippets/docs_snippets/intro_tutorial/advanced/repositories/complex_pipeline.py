e75f159a4184e39495ddc52331169a574555b0b3
yuhan <yuhan@elementl.com>
2021-02-09 15:01:42 -0800

    [new-docs] migrate-4 fix literalincldues -> new code snippet format
    Summary:
    
    Test Plan:
    eyes
    Reviewers:
    sashank
    Subscribers:

--- examples/docs_snippets/docs_snippets/intro_tutorial/advanced/repositories/complex_pipeline.py
+++ examples/docs_snippets/docs_snippets/intro_tutorial/advanced/repositories/complex_pipeline.py
@@ -14,9 +14,7 @@ def load_cereals(_):
 
 @solid
 def sort_by_calories(_, cereals):
-    sorted_cereals = list(
-        sorted(cereals, key=lambda cereal: cereal["calories"])
-    )
+    sorted_cereals = list(sorted(cereals, key=lambda cereal: cereal["calories"]))
     least_caloric = sorted_cereals[0]["name"]
     most_caloric = sorted_cereals[-1]["name"]
     return (least_caloric, most_caloric)
@@ -24,9 +22,7 @@ def sort_by_calories(_, cereals):
 
 @solid
 def sort_by_protein(_, cereals):
-    sorted_cereals = list(
-        sorted(cereals, key=lambda cereal: cereal["protein"])
-    )
+    sorted_cereals = list(sorted(cereals, key=lambda cereal: cereal["protein"]))
     least_protein = sorted_cereals[0]["name"]
     most_protein = sorted_cereals[-1]["name"]
     return (least_protein, most_protein)
@@ -34,34 +30,15 @@ def sort_by_protein(_, cereals):
 
 @solid
 def display_results(context, calorie_results, protein_results):
-    context.log.info(
-        "Least caloric cereal: {least_caloric}".format(
-            least_caloric=calorie_results[0]
-        )
-    )
-    context.log.info(
-        "Most caloric cereal: {most_caloric}".format(
-            most_caloric=calorie_results[-1]
-        )
-    )
-    context.log.info(
-        "Least protein-rich cereal: {least_protein}".format(
-            least_protein=protein_results[0]
-        )
-    )
-    context.log.info(
-        "Most protein-rich cereal: {most_protein}".format(
-            most_protein=protein_results[-1]
-        )
-    )
+    context.log.info(f"Most caloric cereal: {calorie_results[-1]}")
+    context.log.info(f"Most protein-rich cereal: {protein_results[-1]}")
 
 
 @pipeline
 def complex_pipeline():
     cereals = load_cereals()
     display_results(
-        calorie_results=sort_by_calories(cereals),
-        protein_results=sort_by_protein(cereals),
+        calorie_results=sort_by_calories(cereals), protein_results=sort_by_protein(cereals),
     )
 
 
