8eea386fdb08c3ad9d588ead2d04a84a2906cdc3
yuhan <yuhan@elementl.com>
2021-02-09 15:01:42 -0800

    [new-docs] migrate-4 fix literalincldues -> new code snippet format
    Test Plan: eyes
    
    Reviewers: sashank
    
    Differential Revision: https://dagster.phacility.com/D6396

--- examples/docs_snippets/docs_snippets/intro_tutorial/basics/e04_quality/inputs_typed.py
+++ examples/docs_snippets/docs_snippets/intro_tutorial/basics/e04_quality/inputs_typed.py
@@ -11,7 +11,7 @@ def read_csv(context, csv_path: str):
     with open(csv_path, "r") as fd:
         lines = [row for row in csv.DictReader(fd)]
 
-    context.log.info("Read {n_lines} lines".format(n_lines=len(lines)))
+    context.log.info(f"Read {len(lines)} lines")
     return lines
 
 
@@ -22,14 +22,10 @@ def read_csv(context, csv_path: str):
 def sort_by_calories(context, cereals):
     sorted_cereals = sorted(cereals, key=lambda cereal: cereal["calories"])
     context.log.info(
-        "Least caloric cereal: {least_caloric}".format(
-            least_caloric=sorted_cereals[0]["name"]
-        )
+        "Least caloric cereal: {least_caloric}".format(least_caloric=sorted_cereals[0]["name"])
     )
     context.log.info(
-        "Most caloric cereal: {most_caloric}".format(
-            most_caloric=sorted_cereals[-1]["name"]
-        )
+        "Most caloric cereal: {most_caloric}".format(most_caloric=sorted_cereals[-1]["name"])
     )
 
 
@@ -39,10 +35,8 @@ def inputs_pipeline():
 
 
 if __name__ == "__main__":
-    run_config = {
-        "solids": {
-            "read_csv": {"inputs": {"csv_path": {"value": "cereal.csv"}}}
-        }
-    }
-    result = execute_pipeline(inputs_pipeline, run_config=run_config)
+    result = execute_pipeline(
+        inputs_pipeline,
+        run_config={"solids": {"read_csv": {"inputs": {"csv_path": {"value": "cereal.csv"}}}}},
+    )
     assert result.success
