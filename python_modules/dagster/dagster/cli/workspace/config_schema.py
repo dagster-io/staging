616e4dbd2f07731877c897e227ca4e4494cc11d4
gibsondan <daniel@elementl.com>
2021-02-12 08:58:53 -0600

    Allow env vars for grpc port/sockets
    Summary: User request. Also fix an issue where we weren't actually translating env vars for StringSource/IntSource workspace config fields.
    
    Test Plan: BK
    
    Reviewers: rexledesma, catherinewu, alangenfeld

--- python_modules/dagster/dagster/cli/workspace/config_schema.py
+++ python_modules/dagster/dagster/cli/workspace/config_schema.py
@@ -2,7 +2,7 @@ import os
 
 from dagster import check
 from dagster.config import Field, ScalarUnion, Selector
-from dagster.config.source import StringSource
+from dagster.config.source import IntSource, StringSource
 from dagster.config.validate import process_config
 from dagster.core.errors import DagsterInvalidConfigError
 from dagster.utils import merge_dicts
@@ -27,7 +27,7 @@ def ensure_workspace_config(workspace_config, yaml_path):
             validation_result.errors,
             workspace_config,
         )
-    return validation_result
+    return validation_result.value
 
 
 def _maybe_include_executable_path(config_dict, include_executable_path):
@@ -96,9 +96,9 @@ WORKSPACE_CONFIG_SCHEMA = {
                             "target": Selector(_get_target_config(include_executable_path=False)),
                         },
                         "grpc_server": {
-                            "host": Field(str, is_required=False),
-                            "socket": Field(str, is_required=False),
-                            "port": Field(int, is_required=False),
+                            "host": Field(StringSource, is_required=False),
+                            "socket": Field(StringSource, is_required=False),
+                            "port": Field(IntSource, is_required=False),
                             "location_name": Field(str, is_required=False),
                         },
                     },
