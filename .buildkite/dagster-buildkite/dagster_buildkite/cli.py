44965b18e7494449de3ead867af93baaf3568d47
Sashank Thupukari <s@thupukari.com>
2021-02-17 13:20:37 -0500

    Turn on sphinx and next build tests
    Summary: Title
    
    Test Plan: bk
    
    Reviewers: yuhan
    
    Differential Revision: https://dagster.phacility.com/D6540

--- .buildkite/dagster-buildkite/dagster_buildkite/cli.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/cli.py
@@ -1,10 +1,11 @@
 from .defines import DO_COVERAGE
 from .steps.dagit import dagit_steps
 from .steps.dagster import coverage_step, dagster_steps
+from .steps.docs import docs_steps
 from .steps.integration import integration_steps
 from .steps.trigger import trigger_step
 from .steps.wait import wait_step
-from .utils import buildkite_yaml_for_steps, is_phab_and_dagit_only
+from .utils import buildkite_yaml_for_steps, is_changes_scoped_to_path
 
 CLI_HELP = """This CLI is used for generating Buildkite YAML.
 """
@@ -12,11 +13,18 @@ CLI_HELP = """This CLI is used for generating Buildkite YAML.
 
 def dagster():
     all_steps = dagit_steps()
-    dagit_only = is_phab_and_dagit_only()
+    dagit_only = is_changes_scoped_to_path("js_modules/dagit")
+    docs_only = is_changes_scoped_to_path("docs")
 
     # If we're in a Phabricator diff and are only making dagit changes, skip the
     # remaining steps since they're not relevant to the diff.
-    if not dagit_only:
+    if dagit_only:
+        all_steps = dagit_steps()
+    elif docs_only:
+        all_steps = docs_steps()
+    else:
+        all_steps += dagit_steps
+        all_steps += docs_steps()
         all_steps += dagster_steps()
 
         all_steps += [wait_step()]
