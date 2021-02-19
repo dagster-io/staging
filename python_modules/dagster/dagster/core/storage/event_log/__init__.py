27291c892cdf913ff85f807a4f7f28f0f837d6f7
Sidharth Menon <menon.sid.k@gmail.com>
2021-02-19 13:28:38 -0500

    [core] Event watcher polling implementation
    Summary:
    Adds implementations for `SqlPollingEventWatcher`
    
    Logically, the way the polling works is that the `SqlPollingEventWatcher` holds a bunch of `SqlPollingRunIdEventWatcherThread`(s), each of which is a thread that polls a given run_id for changes.
    
    The `SqlPollingRunIdEventWatcherThread` is responsible for updating each `Observer` by firing a callback (passed via `SqlPollingEventWatcher::watch_run`)
    
    Test Plan: Integration + bk + test using Sqllite Event Log w/ `SqlPollingEventWatcher`
    
    Differential Revision: https://dagster.phacility.com/D6616

--- python_modules/dagster/dagster/core/storage/event_log/__init__.py
+++ python_modules/dagster/dagster/core/storage/event_log/__init__.py
@@ -1,5 +1,10 @@
 from .base import AssetAwareEventLogStorage, EventLogStorage
 from .in_memory import InMemoryEventLogStorage
+from .polling_event_watcher import SqlPollingEventWatchdog
 from .schema import AssetKeyTable, SqlEventLogStorageMetadata, SqlEventLogStorageTable
 from .sql_event_log import AssetAwareSqlEventLogStorage, SqlEventLogStorage
-from .sqlite import ConsolidatedSqliteEventLogStorage, SqliteEventLogStorage
+from .sqlite import (
+    ConsolidatedSqliteEventLogStorage,
+    SqliteEventLogStorage,
+    SqlitePollingEventLogStorage,
+)
