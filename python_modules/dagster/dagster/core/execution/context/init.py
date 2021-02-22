aedcd251f55445992fdd2ad80222acb58e45abd2
Chris DeCarolis <cdguitar817@gmail.com>
2021-02-22 15:09:53 -0500

    Resource initialization with just resources and config
    Summary: This diff adds a new code path for resource initialization before execution begins. This enables memoization with more "complex" io managers that require other resources.
    
    Test Plan: Additional unit tests with more sophisticated IO managers
    
    Reviewers: sandyryza, alangenfeld, max
    
    Differential Revision: https://dagster.phacility.com/D6513
    
    logger init
    
    blah resources init
    
    more resource refactoring

--- python_modules/dagster/dagster/core/execution/context/init.py
+++ python_modules/dagster/dagster/core/execution/context/init.py
@@ -13,8 +13,9 @@ class InitResourceContext(
     namedtuple(
         "InitResourceContext",
         (
-            "resource_config resource_def pipeline_run log_manager resources "
-            "instance_for_backwards_compat pipeline_def_for_backwards_compat"
+            "resource_config resource_def log_manager resources "
+            "instance_for_backwards_compat pipeline_def_for_backwards_compat "
+            "pipeline_run"
         ),
     )
 ):
@@ -26,23 +27,23 @@ class InitResourceContext(
             :py:class:`ResourceDefinition`.
         resource_def (ResourceDefinition): The definition of the resource currently being
             constructed.
-        pipeline_run (PipelineRun): The pipeline run in context.
-        run_id (str): The id for this run of the pipeline.
         log_manager (DagsterLogManager): The log manager for this run of the pipeline
         resources (ScopedResources): The resources that are available to the resource that we are
             initalizing.
+        pipeline_run (Optional[PipelineRun]): The pipeline run to use. When initializing resources
+            outside of execution context, this will be None.
     """
 
     def __new__(
         cls,
         resource_config: Any,
         resource_def: ResourceDefinition,
-        pipeline_run: PipelineRun,
         log_manager: Optional[DagsterLogManager] = None,
         resource_instance_dict: Optional[Dict[str, Any]] = None,
         required_resource_keys: Optional[Set[str]] = None,
         instance_for_backwards_compat: Optional[DagsterInstance] = None,
         pipeline_def_for_backwards_compat: Optional[PipelineDefinition] = None,
+        pipeline_run: Optional[PipelineRun] = None,
     ):
         check.opt_dict_param(resource_instance_dict, "resource_instance_dict")
         required_resource_keys = check.opt_set_param(
@@ -55,7 +56,6 @@ class InitResourceContext(
             cls,
             resource_config,
             check.inst_param(resource_def, "resource_def", ResourceDefinition),
-            check.inst_param(pipeline_run, "pipeline_run", PipelineRun),
             check.opt_inst_param(log_manager, "log_manager", DagsterLogManager),
             resources=scoped_resources_builder.build(required_resource_keys),
             # The following are used internally for adapting intermediate storage defs to resources
@@ -67,21 +67,18 @@ class InitResourceContext(
                 "pipeline_def_for_backwards_compat",
                 PipelineDefinition,
             ),
+            pipeline_run=check.opt_inst_param(pipeline_run, "pipeline_run", PipelineRun),
         )
 
     @property
     def log(self) -> DagsterLogManager:
         return self.log_manager
 
-    @property
-    def run_id(self) -> str:
-        return self.pipeline_run.run_id
-
     def replace_config(self, config: Any) -> "InitResourceContext":
         return InitResourceContext(
             resource_config=config,
             resource_def=self.resource_def,
-            pipeline_run=self.pipeline_run,
             log_manager=self.log_manager,
             instance_for_backwards_compat=self.instance_for_backwards_compat,
+            pipeline_run=self.pipeline_run,
         )
