0bfbaddb2b9c4850744297cbd34a754b68f04781
prha <prha@elementl.com>
2021-02-25 11:04:01 -0800

    add tags schema to store, fetch tags
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D6804

--- python_modules/dagster/dagster/core/storage/event_log/in_memory.py
+++ python_modules/dagster/dagster/core/storage/event_log/in_memory.py
@@ -19,6 +19,7 @@ class InMemoryEventLogStorage(EventLogStorage, ConfigurableClass):
         self._logs = defaultdict(EventLogSequence)
         self._handlers = defaultdict(set)
         self._inst_data = inst_data
+        self._asset_tags = defaultdict(dict)
         if preload:
             for payload in preload:
                 self._logs[payload.pipeline_run.run_id] = EventLogSequence(payload.event_list)
@@ -87,7 +88,7 @@ class InMemoryEventLogStorage(EventLogStorage, ConfigurableClass):
                     return True
         return False
 
-    def get_all_asset_keys(self, prefix_path=None):
+    def get_asset_keys(self, tags=None, prefix_path=None):
         asset_records = []
         for records in self._logs.values():
             asset_records += [
@@ -103,9 +104,18 @@ class InMemoryEventLogStorage(EventLogStorage, ConfigurableClass):
                 )
             ]
 
+        filtered = []
+        for record in asset_records:
+            if tags:
+                asset_tags = self._asset_tags[record.dagster_event.asset_key.to_string()]
+                if all(asset_tags.get(k) == v for k, v in tags.items()):
+                    filtered.append(record)
+            else:
+                filtered.append(record)
+
         asset_events = [
             record.dagster_event
-            for record in sorted(asset_records, key=lambda x: x.timestamp, reverse=True)
+            for record in sorted(filtered, key=lambda x: x.timestamp, reverse=True)
         ]
         asset_keys = OrderedDict()
         for event in asset_events:
@@ -193,3 +203,9 @@ class InMemoryEventLogStorage(EventLogStorage, ConfigurableClass):
                     updated_record = record._replace(dagster_event=updated_dagster_event)
                     updated_records.append(updated_record)
             self._logs[run_id] = updated_records
+
+    def get_asset_tags(self, asset_key: AssetKey) -> dict:
+        return self._asset_tags[asset_key.to_string()]
+
+    def set_asset_tags(self, asset_key: AssetKey, tags: dict):
+        self._asset_tags[asset_key.to_string()] = tags
