f5c42230b775dc0069984459cfa4544520644c83
yuhan <yuhan@elementl.com>
2021-02-25 02:18:25 -0800

    [docs-infra] broken link test
    Summary:
    v1 for validating links
    Todo:
    - links to api docs
    - links with anchors
    
    Test Plan:
    `yarn test` locally captured 486 broken links (yay!)
    
    will enable the test in bk later (when we are pretty much done with the concept content)
    
    Reviewers:
    sashank
    
    Subscribers:

---
+++ docs/next/__tests__/mdxInternalLinks.test.ts
@@ -0,0 +1,179 @@
+import fs from 'fs';
+import path from 'path';
+
+import glob from 'glob';
+import { createCompiler } from '@mdx-js/mdx';
+// import { getAnchorLinkFromHeadingContent } from 'components/AnchorHeading';
+
+const { parse: parseMdxContent } = createCompiler();
+
+const ROOT_DIR = path.resolve(__dirname, '../');
+const DOCS_DIR = path.resolve(ROOT_DIR, 'content');
+
+type MdxAstNode = any;
+
+test('no dead links', async () => {
+  const allMdxFilePaths = await findAllMdxFileRelativePaths();
+
+  const astStore: { [filePath: string]: MdxAstNode } = {};
+  const allInternalLinksStore: { [filePath: string]: Array<string> } = {};
+
+  // parse mdx files to find all internal links and populate the store
+  await Promise.all(
+    allMdxFilePaths.map(async (relativeFilePath) => {
+      const absolutePath = path.resolve(DOCS_DIR, relativeFilePath);
+      const fileContent = await fs.promises.readFile(absolutePath, 'utf-8');
+      astStore[relativeFilePath] = parseMdxContent(fileContent);
+    }),
+  );
+
+  for (const filePath in astStore) {
+    const internalLinks = collectInternalLinks(astStore[filePath], filePath);
+    allInternalLinksStore[filePath] = internalLinks;
+  }
+
+  const allMdxFileSet = new Set(allMdxFilePaths);
+  const deadLinks: Array<{ sourceFile: string; deadLink: string }> = [];
+
+  let linkCount = 0;
+
+  for (const source in allInternalLinksStore) {
+    const linkList = allInternalLinksStore[source];
+
+    for (const link of linkList) {
+      linkCount++;
+      if (!isLinkLegit(link, allMdxFileSet, astStore)) {
+        deadLinks.push({
+          sourceFile: path.resolve(DOCS_DIR, source),
+          deadLink: link,
+        });
+      }
+    }
+  }
+
+  // sanity check to make sure the parser is working
+  expect(linkCount).toBeGreaterThan(0);
+
+  expect(deadLinks).toEqual([]);
+});
+
+function getMatchCandidates(targetPath: string): Array<string> {
+  return [`${targetPath}.mdx`, `${targetPath}/index.mdx`];
+}
+
+function isLinkLegit(
+  rawTarget: string,
+  allMdxFileSet: Set<string>,
+  astStore: { [filePath: string]: MdxAstNode },
+): boolean {
+  // TODO: Validate links to API Docs
+  if (rawTarget.startsWith('_apidocs/')) {
+    // NOTE: this currently ignore all anchors in links to `_apidocs` (e.g. `_apidocs/foo#bar`)
+    // FIXME: enable testing the anchor https://github.com/dagster-io/dagster/issues/3540
+    // const [target, anchor] = rawTarget.split('#');
+    // return fileExists(
+    //   path.resolve(
+    //     ROOT_DIR,
+    //     '../sections/api/apidocs',
+    //     `${target.replace(/^_apidocs\//, '')}.rst`,
+    //   ),
+    // );
+    return true;
+  }
+
+  // Validate links to public assets
+  if (rawTarget.startsWith('assets/')) {
+    return fileExists(path.resolve(ROOT_DIR, 'public', rawTarget));
+  }
+
+  // Validate regular content links
+  if (!rawTarget.includes('#')) {
+    // the link target doesn't have a "#" anchor
+    return getMatchCandidates(rawTarget).some((name) =>
+      allMdxFileSet.has(name),
+    );
+  }
+
+  // TODO: Validate links with anchors
+  const [target, anchor] = rawTarget.split('#');
+  const targetFilePath = getMatchCandidates(target).find((name) =>
+    allMdxFileSet.has(name),
+  );
+  if (targetFilePath) {
+    return true
+  }
+
+  return false;
+}
+
+// recursively find all filepaths relative to `DOCS_DIR`
+function findAllMdxFileRelativePaths(): Promise<Array<string>> {
+  const options = {
+    cwd: DOCS_DIR,
+  };
+
+  return new Promise((resolve, reject) => {
+    // NOTE: assuming all sources are `.mdx` files
+    glob('**/*.mdx', options, (error, files) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(files);
+    });
+  });
+}
+
+// traverse the mdx ast to find all internal links
+function collectInternalLinks(
+  rootAstNode: MdxAstNode,
+  currentFilePath: string,
+): Array<string> {
+  const externalLinkRegex = /^https?:\/\//;
+
+  const queue = [rootAstNode];
+  const result: Array<string> = [];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    if (!node) {
+      continue;
+    }
+    if (Array.isArray(node.children)) {
+      queue.push(...node.children);
+    }
+
+    if (!((node.type === 'link' || node.type === 'image') && node.url)) {
+      continue;
+    }
+
+    const { url } = node;
+    if (url.match(externalLinkRegex)) {
+      continue;
+    }
+
+    if (url.startsWith('#')) {
+      // is a self-referencing anchor link
+      result.push(`${currentFilePath.replace(/\.mdx$/, '')}${url}`);
+    } else if (!url.startsWith('/')) {
+      console.log(currentFilePath);
+      throw new Error(
+        `Do not use relative references ('${url}' in ${currentFilePath}). All links should start with '/'`,
+      );
+    } else {
+      // remove the leading `/` from the link target
+      result.push(url.substr(1));
+    }
+  }
+
+  return result;
+}
+
+function fileExists(filePath: string): boolean {
+  try {
+    fs.statSync(filePath);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
