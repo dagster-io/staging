f2e6dd3ac0ed348d79c4d69843b8a2bb45b388b1
prha <prha@elementl.com>
2021-03-04 11:54:07 -0800

    remove tqdm unless printing
    Summary: kind of hacky, but actually checking if we're passing in a print_fn to determine
    whether or not to use tqdm
    
    Test Plan: bk, ran back_compat tests and checked output
    
    Reviewers: alangenfeld, dgibson
    
    Subscribers:

--- python_modules/dagster/dagster/core/instance/__init__.py
+++ python_modules/dagster/dagster/core/instance/__init__.py
@@ -495,7 +495,7 @@ class DagsterInstance:
 
             print_fn("Updating run storage...")
             self._run_storage.upgrade()
-            self._run_storage.build_missing_indexes()
+            self._run_storage.build_missing_indexes(print_fn=print_fn)
 
             print_fn("Updating event storage...")
             self._event_storage.upgrade()
