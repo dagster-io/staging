8d7c29e24e933f973e3eff235079eb24f4405702
Chris DeCarolis <cdguitar817@gmail.com>
2021-03-05 10:07:37 -0500

    add instance arg to `build_resources` to ensure fs_io_manager can be initialized.
    Summary: The fs_io_manager initialization process requires a DagsterInstance to get the default directory. Initializing IO managers is one of the most important functions of the `build_resources` API, so this adds an instance arg and confirms that the fs_io_manager can be instantiated and used to retrieve inputs.
    
    Test Plan:
    Added additional test that initializes fs_io_manager.
    integration
    
    Reviewers: alangenfeld
    
    Differential Revision: https://dagster.phacility.com/D6808

--- python_modules/dagster/dagster_tests/core_tests/resource_tests/test_build_resources.py
+++ python_modules/dagster/dagster_tests/core_tests/resource_tests/test_build_resources.py
@@ -1,7 +1,9 @@
 import pytest
-from dagster import Field, resource
+from dagster import Field, InputContext, OutputContext, fs_io_manager, resource
 from dagster.core.errors import DagsterResourceFunctionError
-from dagster.core.execution.build_resources import build_resources
+from dagster.core.execution.build_resources import build_resources, initialize_console_manager
+from dagster.core.instance import DagsterInstance
+from dagster.core.test_utils import instance_for_test
 
 
 def test_basic_resource():
@@ -9,8 +11,12 @@ def test_basic_resource():
     def basic_resource(_):
         return "foo"
 
-    with build_resources(resource_defs={"basic_resource": basic_resource}) as resources:
-        assert resources.resource_instance_dict["basic_resource"] == "foo"
+    with DagsterInstance.ephemeral() as dagster_instance:
+        with build_resources(
+            resource_defs={"basic_resource": basic_resource},
+            instance=dagster_instance,
+        ) as resources:
+            assert resources.resource_instance_dict["basic_resource"] == "foo"
 
 
 def test_resource_with_config():
@@ -22,13 +28,16 @@ def test_resource_with_config():
         animal = init_context.resource_config["animal"]
         return f"plant: {plant}, animal: {animal}"
 
-    with build_resources(
-        resource_defs={"basic_resource": basic_resource},
-        run_config={"basic_resource": {"config": {"plant": "maple tree"}}},
-    ) as resources:
-        assert (
-            resources.resource_instance_dict["basic_resource"] == "plant: maple tree, animal: dog"
-        )
+    with DagsterInstance.ephemeral() as dagster_instance:
+        with build_resources(
+            resource_defs={"basic_resource": basic_resource},
+            instance=dagster_instance,
+            run_config={"basic_resource": {"config": {"plant": "maple tree"}}},
+        ) as resources:
+            assert (
+                resources.resource_instance_dict["basic_resource"]
+                == "plant: maple tree, animal: dog"
+            )
 
 
 def test_resource_with_dependencies():
@@ -40,12 +49,14 @@ def test_resource_with_dependencies():
     def has_deps(init_context):
         return f"{init_context.resources.no_deps} is an animal."
 
-    with build_resources(
-        resource_defs={"no_deps": no_deps, "has_deps": has_deps},
-        run_config={"no_deps": {"config": {"animal": "dog"}}},
-    ) as resources:
-        assert resources.resource_instance_dict["no_deps"] == "dog"
-        assert resources.resource_instance_dict["has_deps"] == "dog is an animal."
+    with DagsterInstance.ephemeral() as dagster_instance:
+        with build_resources(
+            resource_defs={"no_deps": no_deps, "has_deps": has_deps},
+            instance=dagster_instance,
+            run_config={"no_deps": {"config": {"animal": "dog"}}},
+        ) as resources:
+            assert resources.resource_instance_dict["no_deps"] == "dog"
+            assert resources.resource_instance_dict["has_deps"] == "dog is an animal."
 
 
 def test_error_in_resource_initialization():
@@ -53,9 +64,46 @@ def test_error_in_resource_initialization():
     def i_will_fail(_):
         raise Exception("Failed.")
 
-    with pytest.raises(
-        DagsterResourceFunctionError,
-        match="Error executing resource_fn on ResourceDefinition i_will_fail",
-    ):
-        with build_resources(resource_defs={"i_will_fail": i_will_fail}):
-            pass
+    with DagsterInstance.ephemeral() as dagster_instance:
+        with pytest.raises(
+            DagsterResourceFunctionError,
+            match="Error executing resource_fn on ResourceDefinition i_will_fail",
+        ):
+            with build_resources(
+                resource_defs={"i_will_fail": i_will_fail}, instance=dagster_instance
+            ):
+                pass
+
+
+def fs_io_manager_with_instance_test(instance):
+    with build_resources(
+        resource_defs={"io_manager": fs_io_manager}, instance=instance
+    ) as resources:
+        log_manager = initialize_console_manager(None)
+        output_context = OutputContext(
+            step_key="test_step_key",
+            name="result",
+            pipeline_name="fake_pipeline",
+            run_id="run_id",
+            log_manager=log_manager,
+        )
+        resources.resource_instance_dict["io_manager"].handle_output(
+            output_context,
+            "foo",
+        )
+        assert (
+            resources.resource_instance_dict["io_manager"].load_input(
+                InputContext(
+                    name="result",
+                    pipeline_name="fake_pipeline",
+                    upstream_output=output_context,
+                    log_manager=log_manager,
+                ),
+            )
+            == "foo"
+        )
+
+
+def test_build_fs_io_manager():
+    with instance_for_test() as instance:
+        fs_io_manager_with_instance_test(instance)
