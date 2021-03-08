aefca6327957192fd34d3ef517c5177fa6ba9dff
Ben Gotow <ben@foundry376.com>
2021-03-08 10:59:33 -0600

    Add snapshotId support to PiplineAndMode, display mode in more places
    Summary: This diff refactors PiplineAndMode a bit so that you can optionally include the snapshotId, which is rendered after the pipeline name and before the mode (per discussion with Alex).
    
    I also expanded the places PipelineAndMode is used so that the mode is visible in a few more places, and renamed it PipelineReference which I think makes more sense now that renders the full specification of name+snapshot+mode with linking.
    
    I also gave the component more ownership of the link generation, because I think asking people to provide a RepoAddress or explicitly type "repo-unknown" will encourage use of the RepoAddress whenever it's available, and also make sure you have to explicitly opt-out of linking anywhere the pipleine is referenced.
    
    Test Plan: Review screenshots
    
    Reviewers: @dish
    
    Subscribers:

--- js_modules/dagit/src/pipelines/PipelineAndMode.tsx
+++ js_modules/dagit/src/pipelines/PipelineAndMode.tsx
@@ -2,17 +2,40 @@ import {Colors} from '@blueprintjs/core';
 import * as React from 'react';
 import {Link} from 'react-router-dom';
 
+import {PipelineSnapshotLink} from 'src/pipelines/PipelinePathUtils';
+import {RepoAddress} from 'src/workspace/types';
+import {workspacePathFromAddress} from 'src/workspace/workspacePath';
+
 interface Props {
   pipelineName: string;
-  pipelineHref?: string;
+  pipelineHrefContext: 'repo-unknown' | RepoAddress | 'no-link';
+  snapshotId?: string | null;
   mode: string;
 }
 
-export const PipelineAndMode: React.FC<Props> = ({pipelineName, pipelineHref, mode}) => {
-  const pipeline = pipelineHref ? <Link to={pipelineHref}>{pipelineName}</Link> : pipelineName;
+export const PipelineReference: React.FC<Props> = ({
+  pipelineName,
+  pipelineHrefContext,
+  mode,
+  snapshotId,
+}) => {
+  mode = 'production';
+  const pipeline =
+    pipelineHrefContext === 'repo-unknown' ? (
+      <Link to={`/workspace/pipelines/${pipelineName}`}>{pipelineName}</Link>
+    ) : pipelineHrefContext === 'no-link' ? (
+      pipelineName
+    ) : (
+      <Link to={workspacePathFromAddress(pipelineHrefContext, `/pipelines/${pipelineName}/`)}>
+        {pipelineName}
+      </Link>
+    );
+
   return (
     <>
       {pipeline}
+      {snapshotId && ' @ '}
+      {snapshotId && <PipelineSnapshotLink snapshotId={snapshotId} pipelineName={pipelineName} />}
       {mode === 'default' ? null : <span style={{color: Colors.GRAY3}}>{`: ${mode}`}</span>}
     </>
   );
