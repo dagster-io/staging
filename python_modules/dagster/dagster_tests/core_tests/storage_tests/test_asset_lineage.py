aed085060e4309c8a5a18b37d55825468e33abb5
Owen Kephart <owen@elementl.com>
2021-03-09 13:11:37 -0800

    removed transitive lineage, updated examples

--- python_modules/dagster/dagster_tests/core_tests/storage_tests/test_asset_lineage.py
+++ python_modules/dagster/dagster_tests/core_tests/storage_tests/test_asset_lineage.py
@@ -31,54 +31,6 @@ def check_materialization(materialization, asset_key, parent_assets=None, metada
     assert event_data.asset_lineage == (parent_assets or [])
 
 
-def test_output_definition_transitive_lineage():
-
-    entry1 = EventMetadataEntry.int(123, "nrows")
-    entry2 = EventMetadataEntry.float(3.21, "some value")
-
-    @solid(output_defs=[OutputDefinition(name="output1", asset_key=lambda _: AssetKey("table1"))])
-    def solid1(_):
-        return Output(None, "output1", metadata_entries=[entry1])
-
-    @solid
-    def solidX(_, _input):
-        return 1
-
-    @solid(output_defs=[OutputDefinition(name="output3", asset_key=AssetKey("table3"))])
-    def solid3(_, _input):
-        yield Output(
-            7,
-            "output3",
-            metadata_entries=[entry2],
-        )
-
-    @pipeline
-    def my_pipeline():
-        # attach an asset to an output
-        out1 = solid1()
-        outX = out1
-        # 2 solids later,
-        for i in range(2):
-            outX = solidX.alias(f"solidX_{i}")(outX)
-        solid3(outX)
-
-    result = execute_pipeline(my_pipeline)
-    events = result.step_event_list
-    materializations = [
-        event for event in events if event.event_type_value == "STEP_MATERIALIZATION"
-    ]
-    assert len(materializations) == 2
-
-    check_materialization(materializations[0], AssetKey(["table1"]), metadata_entries=[entry1])
-
-    check_materialization(
-        materializations[1],
-        AssetKey(["table3"]),
-        parent_assets=[AssetLineageInfo(AssetKey(["table1"]))],
-        metadata_entries=[entry2],
-    )
-
-
 def test_io_manager_diamond_lineage():
     class MyIOManager(IOManager):
         def handle_output(self, context, obj):
@@ -105,7 +57,7 @@ def test_io_manager_diamond_lineage():
         yield Output(None, "outputA")
         yield Output(None, "outputB")
 
-    @solid
+    @solid(output_defs=[OutputDefinition(name="outputT", io_manager_key="asset_io_manager")])
     def solid_transform(_, _input):
         return None
 
@@ -125,18 +77,18 @@ def test_io_manager_diamond_lineage():
     materializations = [
         event for event in events if event.event_type_value == "STEP_MATERIALIZATION"
     ]
-    assert len(materializations) == 3
+    assert len(materializations) == 5
 
     check_materialization(materializations[0], AssetKey(["solid_produce", "outputA"]))
     check_materialization(materializations[1], AssetKey(["solid_produce", "outputB"]))
     check_materialization(
-        materializations[2],
+        materializations[-1],
         AssetKey(
             ["solid_combine", "outputC"],
         ),
         parent_assets=[
-            AssetLineageInfo(AssetKey(["solid_produce", "outputA"])),
-            AssetLineageInfo(AssetKey(["solid_produce", "outputB"])),
+            AssetLineageInfo(AssetKey(["a_transform", "outputT"])),
+            AssetLineageInfo(AssetKey(["b_transform", "outputT"])),
         ],
     )
 
@@ -277,16 +229,6 @@ def test_mixed_asset_definition_lineage():
 
     @solid(
         output_defs=[
-            OutputDefinition(name="a"),
-            OutputDefinition(name="b"),
-        ]
-    )
-    def passthrough_solid(_, a, b):
-        yield Output(a, "a")
-        yield Output(b, "b")
-
-    @solid(
-        output_defs=[
             OutputDefinition(name="a", asset_key=AssetKey(["output_def_table", "combine_solid"])),
             OutputDefinition(name="b", io_manager_key="asset_io_manager"),
         ]
@@ -299,9 +241,6 @@ def test_mixed_asset_definition_lineage():
     def my_pipeline():
         a = io_manager_solid()
         b = output_def_solid()
-        # TODO: This makes the test fail because we get duplicate AssetKey information
-        # probably worthwhile to think about how to deal with this case.
-        a, b = passthrough_solid(a, b)
         combine_solid(a, b)
 
     result = execute_pipeline(my_pipeline)
