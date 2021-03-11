47e570a22dd7c9f946110ea18ed51576e252bb67
Rex Ledesma <rex@elementl.com>
2021-03-10 19:04:51 -0800

    Update user deployment subchart schema
    Summary:
    Depends on D6507.
    
    Just some restructuring of the files that generate the
    json schema for the Helm charts. Each chart now has their own
    json schema file.
    
    Test Plan:
    pytest
    integration
    
    Differential Revision: https://dagster.phacility.com/D6888

---
+++ helm/dagster/schema/schema/charts/dagster_user_deployments/values.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
+
+from .subschema.user_deployments import UserDeployment
+
+
+class DagsterUserDeploymentsHelmValues(BaseModel):
+    enabled: bool
+    enableSubchart: bool
+    deployments: List[UserDeployment]
