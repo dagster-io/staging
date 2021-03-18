26ab963f8c8322604d5b9743b55c55f01d3e45a0
yuhan <yuhan@elementl.com>
2021-03-17 22:26:39 -0700

    [docs-infra] SEO - turn off noindex
    Summary: will land once we switch to docs.dagster.io
    
    Test Plan: -
    
    Reviewers: sashank
    
    Differential Revision: https://dagster.phacility.com/D7018

--- docs/next/pages/_app.tsx
+++ docs/next/pages/_app.tsx
@@ -15,9 +15,6 @@ import { useVersion } from "../util/useVersion";
 const BASE_URL = "https://dagster.vercel.app";
 const DEFAULT_SEO = {
   title: "Dagster Docs",
-  // TODO: unset this
-  // while dark launch, we mark all pages to noindex
-  dangerouslySetAllPagesToNoIndex: true,
   twitter: {
     site: "@dagsterio",
     cardType: "summary_large_image",
