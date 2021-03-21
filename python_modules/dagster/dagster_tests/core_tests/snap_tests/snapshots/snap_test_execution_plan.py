afa25322ac33b99acc1952fc8e79190be7712a2e
gibsondan <daniel@elementl.com>
2021-03-21 14:23:24 -0500

    Reconstruct ExecutionPlan from the ExecutionPlanSnapshot in the run worker if one exists (take 2)
    Summary:
    When a persisted ExecutionPlanSnapshot is on the run, re-create it from the snapshot rather than rebuilding it from scratch. This is one step on the journey towards being able to have a run worker that doesn't execute any user code and could run in the Dagster cloud, interfacing with step workers in the user cloud.
    
    Test Plan: Integration
    
    Differential Revision: https://dagster.phacility.com/D6133
    
    Reviewers: alangenfeld, schrockn

--- python_modules/dagster/dagster_tests/core_tests/snap_tests/snapshots/snap_test_execution_plan.py
+++ python_modules/dagster/dagster_tests/core_tests/snap_tests/snapshots/snap_test_execution_plan.py
@@ -28,10 +28,31 @@ snapshots['test_create_execution_plan_with_dep 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Any",
-          "name": "result"
+          "name": "result",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "solid_one",
+            "parent": null
+          }
         }
       ],
-      "solid_handle_id": "solid_one"
+      "solid_handle_id": "solid_one",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "solid_one",
+          "parent": null
+        }
+      },
+      "tags": {}
     },
     {
       "__class__": "ExecutionStepSnap",
@@ -40,6 +61,22 @@ snapshots['test_create_execution_plan_with_dep 1'] = '''{
           "__class__": "ExecutionStepInputSnap",
           "dagster_type_key": "Any",
           "name": "num",
+          "source": {
+            "__class__": "FromStepOutput",
+            "fan_in": false,
+            "input_name": "num",
+            "solid_handle": {
+              "__class__": "SolidHandle",
+              "name": "solid_two",
+              "parent": null
+            },
+            "step_output_handle": {
+              "__class__": "StepOutputHandle",
+              "mapping_key": null,
+              "output_name": "result",
+              "step_key": "solid_one"
+            }
+          },
           "upstream_output_handles": [
             {
               "__class__": "StepOutputHandle",
@@ -59,10 +96,31 @@ snapshots['test_create_execution_plan_with_dep 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Any",
-          "name": "result"
+          "name": "result",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "solid_two",
+            "parent": null
+          }
         }
       ],
-      "solid_handle_id": "solid_two"
+      "solid_handle_id": "solid_two",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "solid_two",
+          "parent": null
+        }
+      },
+      "tags": {}
     }
   ]
 }'''
@@ -88,10 +146,31 @@ snapshots['test_create_noop_execution_plan 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Any",
-          "name": "result"
+          "name": "result",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "noop_solid",
+            "parent": null
+          }
         }
       ],
-      "solid_handle_id": "noop_solid"
+      "solid_handle_id": "noop_solid",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "noop_solid",
+          "parent": null
+        }
+      },
+      "tags": {}
     }
   ]
 }'''
@@ -128,10 +207,34 @@ snapshots['test_create_noop_execution_plan_with_tags 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Any",
-          "name": "result"
+          "name": "result",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "noop_solid",
+            "parent": null
+          }
         }
       ],
-      "solid_handle_id": "noop_solid"
+      "solid_handle_id": "noop_solid",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "noop_solid",
+          "parent": null
+        }
+      },
+      "tags": {
+        "bar": "baaz",
+        "foo": "bar"
+      }
     }
   ]
 }'''
@@ -156,6 +259,22 @@ snapshots['test_create_with_composite 1'] = '''{
           "__class__": "ExecutionStepInputSnap",
           "dagster_type_key": "Any",
           "name": "num_one",
+          "source": {
+            "__class__": "FromStepOutput",
+            "fan_in": false,
+            "input_name": "num_one",
+            "solid_handle": {
+              "__class__": "SolidHandle",
+              "name": "add",
+              "parent": null
+            },
+            "step_output_handle": {
+              "__class__": "StepOutputHandle",
+              "mapping_key": null,
+              "output_name": "result",
+              "step_key": "comp_1.add_one"
+            }
+          },
           "upstream_output_handles": [
             {
               "__class__": "StepOutputHandle",
@@ -169,6 +288,22 @@ snapshots['test_create_with_composite 1'] = '''{
           "__class__": "ExecutionStepInputSnap",
           "dagster_type_key": "Any",
           "name": "num_two",
+          "source": {
+            "__class__": "FromStepOutput",
+            "fan_in": false,
+            "input_name": "num_two",
+            "solid_handle": {
+              "__class__": "SolidHandle",
+              "name": "add",
+              "parent": null
+            },
+            "step_output_handle": {
+              "__class__": "StepOutputHandle",
+              "mapping_key": null,
+              "output_name": "result",
+              "step_key": "comp_2.add_one"
+            }
+          },
           "upstream_output_handles": [
             {
               "__class__": "StepOutputHandle",
@@ -188,10 +323,31 @@ snapshots['test_create_with_composite 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Any",
-          "name": "result"
+          "name": "result",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "add",
+            "parent": null
+          }
         }
       ],
-      "solid_handle_id": "add"
+      "solid_handle_id": "add",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "add",
+          "parent": null
+        }
+      },
+      "tags": {}
     },
     {
       "__class__": "ExecutionStepSnap",
@@ -200,6 +356,26 @@ snapshots['test_create_with_composite 1'] = '''{
           "__class__": "ExecutionStepInputSnap",
           "dagster_type_key": "Int",
           "name": "num",
+          "source": {
+            "__class__": "FromStepOutput",
+            "fan_in": false,
+            "input_name": "num",
+            "solid_handle": {
+              "__class__": "SolidHandle",
+              "name": "add_one",
+              "parent": {
+                "__class__": "SolidHandle",
+                "name": "comp_1",
+                "parent": null
+              }
+            },
+            "step_output_handle": {
+              "__class__": "StepOutputHandle",
+              "mapping_key": null,
+              "output_name": "out_num",
+              "step_key": "comp_1.return_one"
+            }
+          },
           "upstream_output_handles": [
             {
               "__class__": "StepOutputHandle",
@@ -219,10 +395,39 @@ snapshots['test_create_with_composite 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Int",
-          "name": "result"
+          "name": "result",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "add_one",
+            "parent": {
+              "__class__": "SolidHandle",
+              "name": "comp_1",
+              "parent": null
+            }
+          }
         }
       ],
-      "solid_handle_id": "comp_1.add_one"
+      "solid_handle_id": "comp_1.add_one",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "add_one",
+          "parent": {
+            "__class__": "SolidHandle",
+            "name": "comp_1",
+            "parent": null
+          }
+        }
+      },
+      "tags": {}
     },
     {
       "__class__": "ExecutionStepSnap",
@@ -236,10 +441,39 @@ snapshots['test_create_with_composite 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Int",
-          "name": "out_num"
+          "name": "out_num",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "return_one",
+            "parent": {
+              "__class__": "SolidHandle",
+              "name": "comp_1",
+              "parent": null
+            }
+          }
         }
       ],
-      "solid_handle_id": "comp_1.return_one"
+      "solid_handle_id": "comp_1.return_one",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "return_one",
+          "parent": {
+            "__class__": "SolidHandle",
+            "name": "comp_1",
+            "parent": null
+          }
+        }
+      },
+      "tags": {}
     },
     {
       "__class__": "ExecutionStepSnap",
@@ -248,6 +482,26 @@ snapshots['test_create_with_composite 1'] = '''{
           "__class__": "ExecutionStepInputSnap",
           "dagster_type_key": "Int",
           "name": "num",
+          "source": {
+            "__class__": "FromStepOutput",
+            "fan_in": false,
+            "input_name": "num",
+            "solid_handle": {
+              "__class__": "SolidHandle",
+              "name": "add_one",
+              "parent": {
+                "__class__": "SolidHandle",
+                "name": "comp_2",
+                "parent": null
+              }
+            },
+            "step_output_handle": {
+              "__class__": "StepOutputHandle",
+              "mapping_key": null,
+              "output_name": "out_num",
+              "step_key": "comp_2.return_one"
+            }
+          },
           "upstream_output_handles": [
             {
               "__class__": "StepOutputHandle",
@@ -267,10 +521,39 @@ snapshots['test_create_with_composite 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Int",
-          "name": "result"
+          "name": "result",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "add_one",
+            "parent": {
+              "__class__": "SolidHandle",
+              "name": "comp_2",
+              "parent": null
+            }
+          }
         }
       ],
-      "solid_handle_id": "comp_2.add_one"
+      "solid_handle_id": "comp_2.add_one",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "add_one",
+          "parent": {
+            "__class__": "SolidHandle",
+            "name": "comp_2",
+            "parent": null
+          }
+        }
+      },
+      "tags": {}
     },
     {
       "__class__": "ExecutionStepSnap",
@@ -284,10 +567,39 @@ snapshots['test_create_with_composite 1'] = '''{
         {
           "__class__": "ExecutionStepOutputSnap",
           "dagster_type_key": "Int",
-          "name": "out_num"
+          "name": "out_num",
+          "properties": {
+            "__class__": "StepOutputProperties",
+            "is_asset": false,
+            "is_dynamic": false,
+            "is_required": true,
+            "should_materialize": false
+          },
+          "solid_handle": {
+            "__class__": "SolidHandle",
+            "name": "return_one",
+            "parent": {
+              "__class__": "SolidHandle",
+              "name": "comp_2",
+              "parent": null
+            }
+          }
         }
       ],
-      "solid_handle_id": "comp_2.return_one"
+      "solid_handle_id": "comp_2.return_one",
+      "step_handle": {
+        "__class__": "StepHandle",
+        "solid_handle": {
+          "__class__": "SolidHandle",
+          "name": "return_one",
+          "parent": {
+            "__class__": "SolidHandle",
+            "name": "comp_2",
+            "parent": null
+          }
+        }
+      },
+      "tags": {}
     }
   ]
 }'''
