04f4fb6d7aa10c9751deeb6c6f927a0c6615e26e
dpeng817 <cdguitar817@gmail.com>
2021-04-02 13:37:07 -0400

    Add method to convert pipeline definition to composite solid definition
    Summary:
    This diff allows you to represent a pipeline as a composite solid.
    
    The aim is to provide an easy way to use pipelines with the execute_in_process testing API, which is targeted towards solids and composite solids.
    
    [execute_in_process improvements 3/n] provide resource instances, split out config and inputs.
    
    This diff does four things:
    `execute.py` -> `execute_in_process.py`
    `execute_in_process` now accepts directly initialized resource values instead of resource definitions.
    `run_config` argument has been split into more specific things:
    - solid_config for satisfying config schema of the top-level node being executed
    - composed_config for satisfying config schema of the nodes internal to a graph being executed (this arg only works when a graph is being executed).
    
    The goal was overall to be more explicit with what this API was for (directly testing business logic), and be more opinionated about the way inputs should be provided via this function.
    
    changed airflow ingest csv tests to use execute_in_process
    
    Test Plan:
    Added unit test
    
    Added a test for executing w/resources. Going to be testing this out in the dogfooding + demo pipelines
    
    Reviewers: alangenfeld, sandyryza, schrockn, yuhan, sashank
    
    Differential Revision: https://dagster.phacility.com/D7282

--- examples/airline_demo/airline_demo_tests/unit_tests/test_unzip_file_handle.py
+++ examples/airline_demo/airline_demo_tests/unit_tests/test_unzip_file_handle.py
@@ -3,15 +3,18 @@ import zipfile
 import boto3
 from airline_demo.unzip_file_handle import unzip_file_handle
 from dagster import (
+    InputDefinition,
     LocalFileHandle,
     ModeDefinition,
     OutputDefinition,
     ResourceDefinition,
+    composite_solid,
     execute_pipeline,
     local_file_manager,
     pipeline,
     solid,
 )
+from dagster.core.execution.execute_in_process import execute_in_process
 from dagster.utils.test import get_temp_file_name
 from dagster_aws.s3 import S3FileHandle, S3FileManager, s3_intermediate_storage
 from moto import mock_s3
@@ -34,19 +37,14 @@ def test_unzip_file_handle():
         def to_zip_file_handle(_):
             return LocalFileHandle(zip_file_name)
 
-        @pipeline(mode_defs=[ModeDefinition(resource_defs={"file_manager": local_file_manager})])
-        def do_test_unzip_file_handle():
-            return unzip_file_handle(to_zip_file_handle())
+        @composite_solid(input_defs=[InputDefinition("archive_member", dagster_type=str)])
+        def do_test_unzip_file_handle(archive_member):
+            return unzip_file_handle(to_zip_file_handle(), archive_member)
 
-        result = execute_pipeline(
+        result = execute_in_process(
             do_test_unzip_file_handle,
-            run_config={
-                "solids": {
-                    "unzip_file_handle": {
-                        "inputs": {"archive_member": {"value": "some_archive_member"}}
-                    }
-                }
-            },
+            resources={"file_manager": local_file_manager},
+            input_values={"archive_member": "some_archive_member"},
         )
         assert result.success
 
@@ -58,16 +56,19 @@ def test_unzip_file_handle_on_fake_s3():
     @solid(required_resource_keys={"file_manager"}, output_defs=[OutputDefinition(S3FileHandle)])
     def write_zipped_file_to_s3_store(context):
         with get_temp_file_name() as zip_file_name:
-            write_zip_file_to_disk(zip_file_name, "an_archive_member", foo_bytes)
+            write_zip_file_to_disk(
+                zip_file_name, "an_archive_member", foo_bytes)
             with open(zip_file_name, "rb") as ff:
-                s3_file_handle = context.resources.file_manager.write_data(ff.read())
+                s3_file_handle = context.resources.file_manager.write_data(
+                    ff.read())
                 return s3_file_handle
 
     # Uses mock S3
     # https://github.com/spulec/moto/issues/3292
     s3 = boto3.client("s3", region_name="us-east-1")
     s3.create_bucket(Bucket="some-bucket")
-    file_manager = S3FileManager(s3_session=s3, s3_bucket="some-bucket", s3_base_key="dagster")
+    file_manager = S3FileManager(
+        s3_session=s3, s3_bucket="some-bucket", s3_base_key="dagster")
 
     @pipeline(
         mode_defs=[
@@ -95,9 +96,12 @@ def test_unzip_file_handle_on_fake_s3():
 
     assert result.success
 
-    zipped_s3_file = result.result_for_solid("write_zipped_file_to_s3_store").output_value()
-    unzipped_s3_file = result.result_for_solid("unzip_file_handle").output_value()
-    bucket_keys = [obj["Key"] for obj in s3.list_objects(Bucket="some-bucket")["Contents"]]
+    zipped_s3_file = result.result_for_solid(
+        "write_zipped_file_to_s3_store").output_value()
+    unzipped_s3_file = result.result_for_solid(
+        "unzip_file_handle").output_value()
+    bucket_keys = [obj["Key"]
+                   for obj in s3.list_objects(Bucket="some-bucket")["Contents"]]
 
     assert zipped_s3_file.s3_key in bucket_keys
     assert unzipped_s3_file.s3_key in bucket_keys
