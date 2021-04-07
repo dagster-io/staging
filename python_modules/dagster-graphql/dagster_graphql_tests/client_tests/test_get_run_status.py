ca92d70e0fbb6bbf5f7c869f1af4f7cec188227c
Sidharth Menon <menon.sid.k@gmail.com>
2021-04-07 12:40:31 -0400

    [RFC] [dagster-graphql] Python Client Implementation [1/3]
    Summary:
    Added implementations for `get_run_status` and `reload_repository_location`
    
    In forthcoming diffs:
    	1. Backcompatability scaffolding
    	2. Submitting Pipeline Execution
    
    Test Plan: Added testing for success conditions as well as for 1) query validation failures and 2) runtime errors on the server side (i.e. a requested run does not exist, etc.)
    
    Reviewers: rexledesma
    
    Differential Revision: https://dagster.phacility.com/D7409

---
+++ python_modules/dagster-graphql/dagster_graphql_tests/client_tests/test_get_run_status.py
@@ -0,0 +1,42 @@
+import pytest
+from dagster_graphql.client import DagsterGraphQLClientError
+
+from .conftest import MockClient
+
+
+def test_get_run_status_success(mock_client: MockClient):
+    expected_result = "SUCCESS"
+    response = {"pipelineRunOrError": {"__typename": "PipelineRun", "status": expected_result}}
+    mock_client.mock_gql_client.execute.return_value = response
+
+    actual_result = mock_client.python_client.get_run_status("foo")
+    assert actual_result == expected_result
+
+
+def test_get_run_status_fails_with_python_error(mock_client: MockClient):
+    error_type, error_msg = "PythonError", "something exploded"
+    response = {"pipelineRunOrError": {"__typename": error_type, "message": error_msg}}
+    mock_client.mock_gql_client.execute.return_value = response
+
+    with pytest.raises(DagsterGraphQLClientError) as exc_info:
+        mock_client.python_client.get_run_status("foo")
+
+    assert exc_info.value.args == (error_type, error_msg)
+
+
+def test_get_run_status_fails_with_pipeline_run_not_found_error(mock_client: MockClient):
+    error_type, error_msg = "PipelineRunNotFoundError", "The specified pipeline run does not exist"
+    response = {"pipelineRunOrError": {"__typename": error_type, "message": error_msg}}
+    mock_client.mock_gql_client.execute.return_value = response
+
+    with pytest.raises(DagsterGraphQLClientError) as exc_info:
+        mock_client.python_client.get_run_status("foo")
+
+    assert exc_info.value.args == (error_type, error_msg)
+
+
+def test_get_run_status_fails_with_query_error(mock_client: MockClient):
+    mock_client.mock_gql_client.execute.side_effect = Exception("foo")
+
+    with pytest.raises(DagsterGraphQLClientError) as _:
+        mock_client.python_client.get_run_status("foo")
