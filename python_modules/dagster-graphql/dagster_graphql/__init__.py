036157ad3ea24fa7cf6015c328b1e9e07fab754a
Sidharth Menon <menon.sid.k@gmail.com>
2021-04-07 12:40:31 -0400

    [RFC] [dagster-graphql] Python Client Implementation [1/4]
    Summary:
    Added implementations for `get_run_status` and `reload_repository_location`
    
    In forthcoming diffs:
    	1. Backcompatability scaffolding
    	2. Submitting Pipeline Execution
    
    Test Plan: Added testing for success conditions as well as for 1) query validation failures and 2) runtime errors on the server side (i.e. a requested run does not exist, etc.)
    
    Reviewers: rexledesma
    
    Differential Revision: https://dagster.phacility.com/D7409

--- python_modules/dagster-graphql/dagster_graphql/__init__.py
+++ python_modules/dagster-graphql/dagster_graphql/__init__.py
@@ -1,5 +1,8 @@
 from dagster.core.utils import check_dagster_package_version
 
+from .client import DagsterGraphQLClient
 from .version import __version__
 
 check_dagster_package_version("dagster-graphql", __version__)
+
+__all__ = ["DagsterGraphQLClient"]
