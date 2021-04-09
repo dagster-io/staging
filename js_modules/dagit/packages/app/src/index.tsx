3b5b448d0650ddf2e40397eeb605dd2ce3b65f7b
prha <prha@elementl.com>
2021-04-09 10:40:54 -0700

    fix the app_path_prefix on the dev server by ignoring the uninterpolated value
    Summary: on the react dev server, we do not apply the jinja template to grab the appropriate prefix
    value.  Instead of figuring out how to correctly interpolate this, just ignore it.
    
    Test Plan: loaded dev server, did not see URL abomination
    
    Reviewers: max, dish
    
    Subscribers:

--- js_modules/dagit/packages/app/src/index.tsx
+++ js_modules/dagit/packages/app/src/index.tsx
@@ -2,8 +2,10 @@ import {App, AppProvider, AppTopNav} from '@dagit/core';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-const APP_PATH_PREFIX =
+const UNSET_APP_PATH_PREFIX_VALUE = '{{ app_path_prefix }}';
+const META_APP_PATH =
   document.querySelector('meta[name=dagit-path-prefix]')?.getAttribute('content') || '';
+const APP_PATH_PREFIX = META_APP_PATH !== UNSET_APP_PATH_PREFIX_VALUE ? META_APP_PATH : '';
 
 const config = {
   basePath: APP_PATH_PREFIX,
