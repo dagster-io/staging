94358848cf7eafdb2d64986658bddba736a670cb
Rex Ledesma <rex@elementl.com>
2021-04-16 00:11:29 -0700

    [Helm] Allow configuration of postgres connection string on storage classes
    Summary:
    In response to a user request from https://dagster.slack.com/archives/CCCR6P2UR/p1618395836169300.
    
    We already allow the postgres storage classes to be configured by a postgres connection string, but
    we don't expose this capability in Kubernetes. Here, we implement the threading for this config value.
    
    Test Plan:
    pytest
    integration (TODO)
    
    Reviewers: johann, prha
    
    Differential Revision: https://dagster.phacility.com/D7478

---
+++ helm/dagster/schema/schema_tests/test_instance.py
@@ -0,0 +1,47 @@
+import pytest
+import yaml
+from kubernetes.client import models
+from schema.charts.dagster.subschema.global_ import Global, PostgreSQLConnectionString
+from schema.charts.dagster.values import DagsterHelmValues
+
+from .helm_template import HelmTemplate
+
+
+@pytest.fixture(name="template")
+def helm_template() -> HelmTemplate:
+    return HelmTemplate(
+        output="templates/configmap-instance.yaml",
+        model=models.V1ConfigMap,
+    )
+
+
+def test_storage_postgres_url_config(template: HelmTemplate):
+    helm_values = DagsterHelmValues.construct(
+        global_=Global.construct(
+            postgresqlConnectionString=PostgreSQLConnectionString.construct(enabled=True)
+        )
+    )
+
+    configmaps = template.render(helm_values)
+
+    assert len(configmaps) == 1
+
+    instance = yaml.full_load(configmaps[0].data["dagster.yaml"])
+
+    assert instance["schedule_storage"]["config"]["postgres_url"]
+    assert instance["run_storage"]["config"]["postgres_url"]
+    assert instance["event_log_storage"]["config"]["postgres_url"]
+
+
+def test_storage_postgres_db_config(template: HelmTemplate):
+    helm_values = DagsterHelmValues.construct()
+
+    configmaps = template.render(helm_values)
+
+    assert len(configmaps) == 1
+
+    instance = yaml.full_load(configmaps[0].data["dagster.yaml"])
+
+    assert instance["schedule_storage"]["config"]["postgres_db"]
+    assert instance["run_storage"]["config"]["postgres_db"]
+    assert instance["event_log_storage"]["config"]["postgres_db"]
