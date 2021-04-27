374871caeb490a6e58f360b4e727cd9d54d22418
Sandy Ryza <sandy@elementl.com>
2021-04-27 15:11:11 -0700

    remove unit test on connecting solids tutorial page
    Summary: It's redundant with the tests that are on the next page in the tutorial.  Also, it's a little bit confusing because it implies the pipeline could succeed without all the solids succeeding.
    
    Test Plan: bk
    
    Reviewers: yuhan, sashank, owen
    
    Differential Revision: https://dagster.phacility.com/D7631

--- examples/docs_snippets/docs_snippets/intro_tutorial/basics/connecting_solids/complex_pipeline.py
+++ examples/docs_snippets/docs_snippets/intro_tutorial/basics/connecting_solids/complex_pipeline.py
@@ -1,7 +1,7 @@
 import csv
 
 import requests
-from dagster import execute_pipeline, pipeline, solid
+from dagster import pipeline, solid
 
 
 # start_complex_pipeline_marker_0
@@ -16,17 +16,13 @@ def download_cereals(_):
 
 @solid
 def find_highest_calorie_cereal(_, cereals):
-    sorted_cereals = list(
-        sorted(cereals, key=lambda cereal: cereal["calories"])
-    )
+    sorted_cereals = list(sorted(cereals, key=lambda cereal: cereal["calories"]))
     return sorted_cereals[-1]["name"]
 
 
 @solid
 def find_highest_protein_cereal(_, cereals):
-    sorted_cereals = list(
-        sorted(cereals, key=lambda cereal: cereal["protein"])
-    )
+    sorted_cereals = list(sorted(cereals, key=lambda cereal: cereal["protein"]))
     return sorted_cereals[-1]["name"]
 
 
@@ -46,14 +42,3 @@ def complex_pipeline():
 
 
 # end_complex_pipeline_marker_0
-
-# start_complex_pipeline_marker_1
-def test_complex_pipeline():
-    res = execute_pipeline(complex_pipeline)
-    assert res.success
-    assert len(res.solid_result_list) == 4
-    for solid_res in res.solid_result_list:
-        assert solid_res.success
-
-
-# end_complex_pipeline_marker_1
