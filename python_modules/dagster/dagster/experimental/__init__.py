ca295971339703643d221c3b0ac4a0ed0a52f0c7
alangenfeld <alex@elementl.com>
2021-04-28 16:31:15 -0500

    RFC solid RetryPolicy
    Summary: prototype if introducing policy object on solid that can do things like cause retries to be raised.
    
    Test Plan: wip
    
    Reviewers: sandyryza, schrockn, cdecarolis, yuhan
    
    Differential Revision: https://dagster.phacility.com/D7646

--- python_modules/dagster/dagster/experimental/__init__.py
+++ python_modules/dagster/dagster/experimental/__init__.py
@@ -1,3 +1,4 @@
 from dagster.core.definitions.events import DynamicOutput
 from dagster.core.definitions.output import DynamicOutputDefinition
+from dagster.core.definitions.policy import RetryPolicy
 from dagster.core.definitions.schedule import build_schedule_context
