81d32225b1504abe409e8115b1bf090f44284f12
prha <prha@elementl.com>
2021-05-03 17:43:14 -0700

    Add documentation for manually scheduling partition, adding to a repo
    Test Plan: bk
    
    Reviewers: dgibson, sandyryza
    
    Differential Revision: https://dagster.phacility.com/D7714

--- examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/partition_definition.py
+++ examples/docs_snippets/docs_snippets/concepts/partitions_schedules_sensors/partition_definition.py
@@ -1,4 +1,6 @@
-from dagster import Partition, PartitionSetDefinition
+import random
+
+from dagster import Partition, PartitionSetDefinition, pipeline, repository, solid
 
 
 # start_def
@@ -19,3 +21,62 @@ date_partition_set = PartitionSetDefinition(
     run_config_fn_for_partition=run_config_for_date_partition,
 )
 # end_def
+
+
+@solid
+def my_solid(_):
+    pass
+
+
+@pipeline
+def my_data_pipeline():
+    my_solid()
+
+
+# start_repo_include
+@repository
+def my_repository():
+    return [
+        my_data_pipeline,
+        date_partition_set,
+    ]
+
+
+# end_repo_include
+
+my_partition_set = PartitionSetDefinition(
+    name="my_partition_set",
+    pipeline_name="my_data_pipeline",
+    partition_fn=lambda: [
+        Partition("a"),
+        Partition("b"),
+        Partition("c"),
+    ],
+    run_config_fn_for_partition=lambda _: {},
+)
+
+# start_manual_partition_schedule
+def random_partition(_, partition_set_definition):
+    # select a single, random partition
+    partitions = partition_set_definition.get_partitions()
+    return random.choice(partitions)
+
+
+my_schedule = my_partition_set.create_schedule_definition(
+    "my_schedule",
+    "5 0 * * *",
+    partition_selector=random_partition,
+    execution_timezone="US/Eastern",
+)
+
+
+@repository
+def my_repository_with_schedule():
+    return [
+        my_data_pipeline,
+        my_partition_set,
+        my_schedule,
+    ]
+
+
+# end_manual_partition_schedule
