9eaa293c58d873923d7771cc5fc2ce5479cb0f1c
Sandy Ryza <sandy@elementl.com>
2021-05-04 07:57:19 -0700

    abstract sensors
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D7762

---
+++ python_modules/dagster-test/dagster_test/toys/crag/abstract_sensor.py
@@ -0,0 +1,64 @@
+"""
+In my staging environment, I want a sensor that kicks off my pipeline using my staging resources.
+
+In my production environment, I want a sensor that kicks off my pipeline using my production
+resources.
+"""
+from dagster import (
+    ResourceDefinition,
+    RunRequest,
+    create_sensor_context,
+    pipeline,
+    repository,
+    sensor,
+    solid,
+    validate_sensor,
+)
+
+
+@solid(required_resource_keys={"my_resource"}, config_schema=int)
+def my_solid():
+    pass
+
+
+@solid(required_resource_keys={"my_resource"})
+def my_other_solid(_input1):
+    pass
+
+
+@pipeline
+def my_pipeline():
+    my_other_solid(my_solid())
+
+
+@sensor
+def my_pipeline_sensor():
+    yield RunRequest(None, run_config={"solids": {"my_solid": {"config": {7}}}})
+
+
+my_pipeline_sensor_staging = my_pipeline_sensor.pipeline(
+    my_pipeline.bind({"my_resource": ResourceDefinition.hardcoded_resource(5)}, suffix="staging")
+)
+
+
+my_pipeline_sensor_prod = my_pipeline_sensor.pipeline(
+    my_pipeline.bind({"my_resource": ResourceDefinition.hardcoded_resource(5)}, suffix="prod")
+)
+
+
+@repository
+def staging_repo():
+    return [my_pipeline_sensor_staging]
+
+
+@repository
+def prod_repo():
+    return [my_pipeline_sensor_prod]
+
+
+def test_my_pipeline_sensor():
+    """
+    Make sure the sensor produces config that the pipeline accepts.
+    """
+    assert validate_sensor(my_pipeline_sensor_staging, create_sensor_context())
+    assert validate_sensor(my_pipeline_sensor_prod, create_sensor_context())
