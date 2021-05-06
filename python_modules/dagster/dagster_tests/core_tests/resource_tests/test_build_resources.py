c8b7cd31ab82a9fd46216c8e2553fdc630125fa3
Chris DeCarolis <cdguitar817@gmail.com>
2021-05-06 13:41:27 -0400

    Make the constructed resources object not break mypy and add a flag for whether a resource is a generator
    Summary: We previously used a workaround to get our resources objects to play nicely with mypy's type system. We can do better by just making the resource class subclass namedtuple, and vend out attributes based upon the provided instance dictionary.
    
    Test Plan: Unit tests, mypy
    
    Reviewers: alangenfeld
    
    Subscribers:

--- python_modules/dagster/dagster_tests/core_tests/resource_tests/test_build_resources.py
+++ python_modules/dagster/dagster_tests/core_tests/resource_tests/test_build_resources.py
@@ -2,7 +2,6 @@ import pytest
 from dagster import Field, resource
 from dagster.core.errors import DagsterResourceFunctionError
 from dagster.core.execution.build_resources import build_resources
-from dagster.core.storage.mem_io_manager import InMemoryIOManager
 
 
 def test_basic_resource():
@@ -13,6 +12,7 @@ def test_basic_resource():
     with build_resources(
         resources={"basic_resource": basic_resource},
     ) as resources:
+        assert resources._is_generator == False  # pylint: disable=protected-access
         assert resources.basic_resource == "foo"
 
 
@@ -82,5 +82,18 @@ def test_resource_init_values():
         assert resources.bar == "bar"
 
 
-def test_resource_init_io_manager():
-    build_resources({"io_manager": InMemoryIOManager})
+def test_context_manager_resource():
+    tore_down = []
+
+    @resource
+    def cm_resource(_):
+        try:
+            yield "foo"
+        finally:
+            tore_down.append("yes")
+
+    with build_resources({"cm_resource": cm_resource}) as resources:
+        assert resources._is_generator == True  # pylint: disable=protected-access
+        assert resources.cm_resource == "foo"
+
+    assert tore_down == ["yes"]
