153331f281e5a488fba3a0ce1db304034d2dad78
Johann Miller <johann@elementl.com>
2021-05-12 17:27:19 -0400

    Refactor step handler
    Test Plan: integration
    
    Differential Revision: https://dagster.phacility.com/D7901

--- python_modules/dagster/dagster/core/executor/step_delegating/step_handler/base.py
+++ python_modules/dagster/dagster/core/executor/step_delegating/step_handler/base.py
@@ -1,51 +1,73 @@
 import abc
-from typing import List
+from typing import List, NamedTuple, Optional
 
-from dagster import DagsterEvent
-from dagster.core.execution.context.system import IStepContext, PlanOrchestrationContext
-from dagster.core.execution.plan.state import KnownExecutionState
+from dagster import DagsterEvent, DagsterInstance, check
 from dagster.core.execution.retries import RetryMode
+from dagster.core.storage.pipeline_run import PipelineRun
+from dagster.grpc.types import ExecuteStepArgs
+from dagster.serdes.serdes import whitelist_for_serdes
+
+
+@whitelist_for_serdes
+class PersistedStepHanderContext(
+    NamedTuple(
+        "_PersistedStepHanderContext",
+        [
+            ("execute_step_args", ExecuteStepArgs),
+        ],
+    )
+):
+    def __new__(cls, execute_step_args: ExecuteStepArgs):
+        return super(PersistedStepHanderContext, cls).__new__(
+            cls, check.inst_param(execute_step_args, "execute_step_args", ExecuteStepArgs)
+        )
+
+
+class StepHandlerContext:
+    def __init__(
+        self,
+        instance: DagsterInstance,
+        execute_step_args: ExecuteStepArgs,
+        pipeline_run: Optional[PipelineRun] = None,
+    ) -> None:
+        self._instance = instance
+        self._execute_step_args = execute_step_args
+        self._pipeline_run = pipeline_run
+
+    @property
+    def execute_step_args(self) -> ExecuteStepArgs:
+        return self._execute_step_args
+
+    @property
+    def pipeline_run(self) -> PipelineRun:
+        # lazy load
+        if not self._pipeline_run:
+            run = self._instance.get_run_by_id(self.execute_step_args.pipeline_run_id)
+            self._pipeline_run = run
+
+        return self._pipeline_run
+
+    def to_tuple(self) -> PersistedStepHanderContext:
+        return PersistedStepHanderContext(execute_step_args=self._execute_step_args)
+
+    @classmethod
+    def from_tuple(cls, instance: DagsterInstance, ctx_tuple: PersistedStepHanderContext):
+        return cls(instance=instance, execute_step_args=ctx_tuple.execute_step_args)
 
 
 class StepHandler(abc.ABC):  # pylint: disable=no-init
-    def __init__(self, retries: RetryMode):
-        self._retries = retries
-
-    def initialize_for_execution(self, pipeline_context: PlanOrchestrationContext):
-        self.pipeline_context = pipeline_context  # pylint: disable=attribute-defined-outside-init
-        self._event_cursor = -1  # pylint: disable=attribute-defined-outside-init
-
     @abc.abstractproperty
     def name(self) -> str:
         pass
 
-    @property
-    def retries(self) -> RetryMode:
-        return self._retries
-
-    def pop_events(self) -> List[DagsterEvent]:
-        events = self.pipeline_context.instance.logs_after(
-            self.pipeline_context.pipeline_run.run_id, self._event_cursor
-        )
-        self._event_cursor += len(events)
-        return [event.dagster_event for event in events if event.is_dagster_event]
-
-    @abc.abstractmethod
-    def launch_steps(
-        self,
-        step_contexts: List[IStepContext],
-        known_state: KnownExecutionState,
-    ):
+    @abc.abstractclassmethod
+    def launch_step(cls, step_handler_context: StepHandlerContext) -> List[DagsterEvent]:
         pass
 
-    @abc.abstractmethod
-    def check_step_health(
-        self,
-        step_contexts: List[IStepContext],
-        known_state: KnownExecutionState,
-    ) -> List[DagsterEvent]:
+    @abc.abstractclassmethod
+    def check_step_health(cls, step_handler_context: StepHandlerContext) -> List[DagsterEvent]:
         pass
 
-    @abc.abstractmethod
-    def terminate_steps(self, step_keys: List[str]):
+    @abc.abstractclassmethod
+    def terminate_step(cls, step_handler_context: StepHandlerContext) -> List[DagsterEvent]:
         pass
