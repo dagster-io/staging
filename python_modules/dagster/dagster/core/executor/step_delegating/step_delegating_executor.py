87d8fd03eeef9cd78115f379b434ae74538a5b8c
Johann Miller <johann@elementl.com>
2021-05-12 17:17:59 -0400

    Allow dagster/priority tag in pipeline definition
    Summary:
    
    Test Plan:
    todo
    Reviewers:
    
    Subscribers:

--- python_modules/dagster/dagster/core/executor/step_delegating/step_delegating_executor.py
+++ python_modules/dagster/dagster/core/executor/step_delegating/step_delegating_executor.py
@@ -1,5 +1,5 @@
 import time
-from typing import Dict
+from typing import Dict, List
 
 from dagster import check
 from dagster.core.events import DagsterEvent, EngineEventData
@@ -29,17 +29,22 @@ class StepDelegatingExecutor(Executor):
     def retries(self):
         return self._retries
 
+    def pop_events(self, instance, run_id) -> List[DagsterEvent]:
+        events = instance.logs_after(run_id, self._event_cursor)
+        self._event_cursor += len(events)
+        return [event.dagster_event for event in events if event.is_dagster_event]
+
     def execute(self, pipeline_context: PlanOrchestrationContext, execution_plan: ExecutionPlan):
         check.inst_param(pipeline_context, "pipeline_context", PlanOrchestrationContext)
         check.inst_param(execution_plan, "execution_plan", ExecutionPlan)
+
+        self._event_cursor = -1  # pylint: disable=attribute-defined-outside-init
 
         yield DagsterEvent.engine_event(
             pipeline_context,
             f"Starting execution with step handler {self._step_handler.name}",
             EngineEventData(),
         )
-
-        self._step_handler.initialize_for_execution(pipeline_context)
 
         with execution_plan.start(retry_mode=self.retries) as active_execution:
             stopping = False
@@ -55,9 +60,21 @@ class StepDelegatingExecutor(Executor):
                     stopping = True
                     active_execution.mark_interrupted()
                     for step_key in running_steps:
+                        ExecuteStepArgs(
+                            pipeline_origin=self.pipeline_context.reconstructable_pipeline.get_python_origin(),
+                            pipeline_run_id=self.pipeline_context.pipeline_run.run_id,
+                            step_keys_to_execute=[step_context.step.key],
+                            instance_ref=None,
+                            retry_mode=RetryMode.DISABLED,
+                            known_state=known_state,
+                            should_verify_step=True,
+                        )
                         self._step_handler.terminate_steps([step_key])
 
-                events = self._step_handler.pop_events()
+                events = self._pop_events(
+                    pipeline_context.plan_data.instance,
+                    pipeline_context.plan_data.pipeline_run.run_id,
+                )
 
                 for step in running_steps.values():
                     events.extend(
