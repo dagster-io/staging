e780c8b4c034e92d47a50b999d7df53a64dcbfe2
Chris DeCarolis <cdguitar817@gmail.com>
2021-05-14 17:40:13 -0400

    [RFC] Context managers for building input/output contexts
    Summary: Right now, we don't have a compelling testing story for IO managers. In order to test the functionality of an IO manager class, you need to manually construct InputContext and OutputContext, but there is no simple way for a user to provide resources to these. In addition, having a user directly create contexts makes it harder to support changing them around. This diff adds `build_input_context` and `build_output_context` to try to address these issues.
    
    Test Plan: Unit
    
    Reviewers: sandyryza
    
    Differential Revision: https://dagster.phacility.com/D7926

--- python_modules/dagster/dagster/experimental/__init__.py
+++ python_modules/dagster/dagster/experimental/__init__.py
@@ -1,2 +1,4 @@
 from dagster.core.definitions.events import DynamicOutput
 from dagster.core.definitions.output import DynamicOutputDefinition
+from dagster.core.execution.context.input import build_input_context
+from dagster.core.execution.context.output import build_output_context
