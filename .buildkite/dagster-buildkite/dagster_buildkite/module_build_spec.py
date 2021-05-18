6b247d6168bcd2f9ffddb54e8a14fcb5a93458b8
Max Gasner <max.gasner@gmail.com>
2021-05-18 14:37:21 -0700

    papermill 2.0 compatibility
    Summary: Resolves https://github.com/dagster-io/dagster/issues/2154
    
    Test Plan: Unit
    
    Reviewers: dgibson, prha, alangenfeld, schrockn
    
    Differential Revision: https://dagster.phacility.com/D7970

--- .buildkite/dagster-buildkite/dagster_buildkite/module_build_spec.py
+++ .buildkite/dagster-buildkite/dagster_buildkite/module_build_spec.py
@@ -140,8 +140,9 @@ class ModuleBuildSpec(
             tests.append(
                 StepBuilder(f":mypy: {package}")
                 .run(
-                    "pip install mypy==0.790",
-                    f"mypy --config-file mypy/config {self.directory}",
+                    "pip install mypy==0.812",
+                    # https://github.com/buildkite/agent/issues/1102
+                    f"mypy --config-file mypy/config --exclude 'setup.py$$' {self.directory}",
                 )
                 .on_integration_image(SupportedPython.V3_7)
                 .build()
