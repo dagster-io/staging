d32f8e496de0dd2cbdd24f23f40ce4a363bd36ac
Max Gasner <max.gasner@gmail.com>
2021-05-18 14:37:21 -0700

    papermill 2.0 compatibility
    Summary: Resolves https://github.com/dagster-io/dagster/issues/2154
    
    Test Plan: Unit
    
    Reviewers: dgibson, prha, alangenfeld, schrockn
    
    Differential Revision: https://dagster.phacility.com/D7970

---
+++ python_modules/libraries/dagstermill/dagstermill/compat.py
@@ -0,0 +1,15 @@
+import packaging
+import papermill
+from papermill.exceptions import PapermillExecutionError
+
+IS_PAPERMILL_2 = packaging.version.parse(papermill.__version__).major == 2
+
+
+
+if IS_PAPERMILL_2:
+    ExecutionError = PapermillExecutionError
+
+else:
+    from nbclient.exceptions import CellExecutionError  # pylint: disable=import-error
+
+    ExecutionError = (PapermillExecutionError, CellExecutionError)
