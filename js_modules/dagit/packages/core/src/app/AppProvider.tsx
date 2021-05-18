062f0d87d42710ba9c621de1cfeda79a0850c74c
Isaac Hellendag <dish@elementl.com>
2021-05-18 09:47:57 -0500

    [dagit] Turn on eqeqeq lint rule
    Summary:
    
    Enable `eqeqeq` lint rule with "error" configuration, ignoring `null` checks (which I'm not crazy about, but don't feel strongly about).
    
    Test Plan:
    
    yarn lint
    
    Reviewers:
    
    bengotow
    
    Subscribers:

--- js_modules/dagit/packages/core/src/app/AppProvider.tsx
+++ js_modules/dagit/packages/core/src/app/AppProvider.tsx
@@ -142,7 +142,7 @@ export const AppProvider: React.FC<Props> = (props) => {
     const splitLink = split(
       ({query}) => {
         const definition = getMainDefinition(query);
-        return definition.kind == 'OperationDefinition' && definition.operation == 'subscription';
+        return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
       },
       websocketLink,
       httpLink,
