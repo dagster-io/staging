f980e41d1975ed57ea458c3caf9eb10981aeb327
Chris DeCarolis <cdguitar817@gmail.com>
2021-05-19 09:06:20 -0400

    [RFC] build_sensor_context
    Summary: This diff adds a `build_sensor_context` method, designed to improve the testing story around sensors.
    
    Test Plan: transfer over unit tests
    
    Reviewers: sandyryza, alangenfeld, prha
    
    Differential Revision: https://dagster.phacility.com/D7984

--- python_modules/dagster/dagster/experimental/__init__.py
+++ python_modules/dagster/dagster/experimental/__init__.py
@@ -1,3 +1,4 @@
 from dagster.core.definitions.events import DynamicOutput
 from dagster.core.definitions.output import DynamicOutputDefinition
 from dagster.core.definitions.schedule import build_schedule_context
+from dagster.core.definitions.sensor import build_sensor_context
