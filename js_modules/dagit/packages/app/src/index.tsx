d9267d65088b55eab3b0a9607bd204cf9e0b2a93
helloworld <s@thupukari.com>
2021-05-19 19:05:26 -0400

    Add --read-only flag to Dagit that turns off all mutations
    Summary: This diff adds a --read-only flag, which turns off all mutations in Dagit and shows client side tooltips for all buttons that causes mutations.
    
    Test Plan: unit
    
    Reviewers: max, dgibson, dish
    
    Differential Revision: https://dagster.phacility.com/D8004

--- js_modules/dagit/packages/app/src/index.tsx
+++ js_modules/dagit/packages/app/src/index.tsx
@@ -5,7 +5,7 @@ import {App} from '@dagit/core/app/App';
 import {AppCache} from '@dagit/core/app/AppCache';
 import {AppProvider} from '@dagit/core/app/AppProvider';
 import {AppTopNav} from '@dagit/core/app/AppTopNav';
-import {PERMISSIONS_ALLOW_ALL} from '@dagit/core/app/Permissions';
+import {Permissions} from '@dagit/core/app/Permissions';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -13,10 +13,24 @@ import {extractPathPrefix} from './extractPathPrefix';
 
 const pathPrefix = extractPathPrefix();
 
+const persmissionElement = document.getElementById('permissions');
+
+export interface PermissionsJson {
+  permissions: Permissions;
+}
+
+const identity: PermissionsJson = persmissionElement
+  ? JSON.parse(persmissionElement.textContent || '')
+  : {
+      permissions: {},
+    };
+
+const permissions = identity.permissions === '[permissions here]' ? {} : identity.permissions;
+
 const config = {
   basePath: pathPrefix,
   graphqlURI: process.env.REACT_APP_GRAPHQL_URI || '',
-  permissions: PERMISSIONS_ALLOW_ALL,
+  permissions,
 };
 
 ReactDOM.render(
