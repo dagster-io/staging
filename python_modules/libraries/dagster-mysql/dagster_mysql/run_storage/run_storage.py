8dee8884f3f48f19292defa8de54fd13b142934e
Sidharth Menon <menon.sid.k@gmail.com>
2021-05-20 15:34:59 -0400

    [dagster-mysql] `wait_timeout` fix for MySQL Engine Connections
    Summary: By default, SQLAlchemy keeps the Engine's pooled connections open forever (`pool_recycle=-1`). This is a problem in MySQL because of a config value called `wait_timeout`, which sets the MySQL server to kill idle connections after 8 hours. Therefore, it was possible for code to check out a connection from SQLAlchemy (via `self.connect()` on one of the storages) where the underlying DB connection had been removed. To rectify this, I am setting the pooled connections to recycle after 1 hour.
    
    Test Plan: bk and Integration
    
    Reviewers: prha, catherinewu
    
    Subscribers: owen

--- python_modules/libraries/dagster-mysql/dagster_mysql/run_storage/run_storage.py
+++ python_modules/libraries/dagster-mysql/dagster_mysql/run_storage/run_storage.py
@@ -8,6 +8,7 @@ from dagster.utils import utc_datetime_from_timestamp
 from dagster.utils.backcompat import experimental_class_warning
 
 from ..utils import (
+    MYSQL_POOL_RECYCLE,
     create_mysql_connection,
     mysql_alembic_config,
     mysql_config,
@@ -45,6 +46,7 @@ class MySQLRunStorage(SqlRunStorage, ConfigurableClass):
             self.mysql_url,
             isolation_level="AUTOCOMMIT",
             poolclass=db.pool.NullPool,
+            pool_recycle=MYSQL_POOL_RECYCLE,
         )
 
         self._index_migration_cache = {}
@@ -62,7 +64,12 @@ class MySQLRunStorage(SqlRunStorage, ConfigurableClass):
     def optimize_for_dagit(self, statement_timeout):
         # When running in dagit, hold 1 open connection
         # https://github.com/dagster-io/dagster/issues/3719
-        self._engine = create_engine(self.mysql_url, isolation_level="AUTOCOMMIT", pool_size=1)
+        self._engine = create_engine(
+            self.mysql_url,
+            isolation_level="AUTOCOMMIT",
+            pool_size=1,
+            pool_recycle=MYSQL_POOL_RECYCLE,
+        )
 
     @property
     def inst_data(self):
