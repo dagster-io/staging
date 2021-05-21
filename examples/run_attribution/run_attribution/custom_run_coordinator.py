d093f25cbe5b7da02ee465f8d89ca1692a9fa404
Sidharth Menon <menon.sid.k@gmail.com>
2021-05-21 13:53:50 -0400

    [examples] Adding a Run Attribution Example via a Custom Run Coordinator
    Summary:
    As part of an ask by a user, this is an example of how one would perform Run Attribution by reading off of HTTP headers in the Run Coordinator.
    
    Depends on D8036
    
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D8043

---
+++ examples/run_attribution/run_attribution/custom_run_coordinator.py
@@ -0,0 +1,25 @@
+import warnings
+from typing import Optional
+
+from dagster.core.host_representation import ExternalPipeline
+from dagster.core.run_coordinator import QueuedRunCoordinator
+from dagster.core.storage.pipeline_run import PipelineRun
+from flask import has_request_context, request
+
+
+class CustomRunCoordinator(QueuedRunCoordinator):
+    def get_email(self, session_token: Optional[str]) -> Optional[str]:
+        return session_token
+
+    def submit_run(
+        self, pipeline_run: PipelineRun, external_pipeline: ExternalPipeline
+    ) -> PipelineRun:
+        session_token = (
+            request.headers.get("Dagster-Session-Token", None) if has_request_context() else None
+        )
+        email = self.get_email(session_token)
+        if email:
+            self._instance.add_run_tags(pipeline_run.run_id, {"user": email})
+        else:
+            warnings.warn(f"Couldn't decode session token {email}")
+        return super().submit_run(pipeline_run, external_pipeline)
