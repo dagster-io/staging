b8bcb2a1b5dd4ded585b93d52f8b98e8858bfb6d
Sidharth Menon <menon.sid.k@gmail.com>
2021-05-21 14:17:07 -0400

    [core][tests] Shared RunCoordinator test suite (2/n)
    Summary: Adding a shared RunCoordinator test suite in anticipation for a Run Attribution example (using a Run Coordinator).
    
    Test Plan: bk
    
    Reviewers: jordansanders, rexledesma, catherinewu
    
    Differential Revision: https://dagster.phacility.com/D8036

--- python_modules/dagster/dagster_tests/core_tests/run_coordinator_tests/test_default_run_coordinator.py
+++ python_modules/dagster/dagster_tests/core_tests/run_coordinator_tests/test_default_run_coordinator.py
@@ -1,56 +1,27 @@
 import pytest
-from dagster.check import CheckError
 from dagster.core.run_coordinator.default_run_coordinator import DefaultRunCoordinator
 from dagster.core.storage.pipeline_run import PipelineRunStatus
-from dagster.core.test_utils import create_run_for_test, instance_for_test
-from dagster.utils import merge_dicts
 from dagster_tests.api_tests.utils import get_foo_external_pipeline
+from dagster_tests.core_tests.run_coordinator_tests.utils import TestRunCoordinator
 
 
-@pytest.fixture()
-def instance():
-    overrides = {
-        "run_launcher": {"module": "dagster.core.test_utils", "class": "MockedRunLauncher"}
-    }
-    with instance_for_test(overrides=overrides) as inst:
-        yield inst
+class TestDefaultRunCoordinator(TestRunCoordinator):
+    __test__ = True
 
+    @pytest.fixture(name="coordinator", scope="function")
+    def coordinator_fixture(self, instance):  # pylint: disable=arguments-differ
+        coordinator = DefaultRunCoordinator()
+        coordinator.register_instance(instance)
+        yield coordinator
 
-@pytest.fixture()
-def coodinator(instance):  # pylint: disable=redefined-outer-name
-    run_coordinator = DefaultRunCoordinator()
-    run_coordinator.register_instance(instance)
-    yield run_coordinator
+    def test_submit_run(self, instance, coordinator):
+        with get_foo_external_pipeline() as external_pipeline:
+            run = self.create_run(instance, external_pipeline, run_id="foo-1")
+            returned_run = coordinator.submit_run(run, external_pipeline)
+            assert returned_run.run_id == "foo-1"
+            assert returned_run.status == PipelineRunStatus.STARTING
 
-
-def create_run(instance, external_pipeline, **kwargs):  # pylint: disable=redefined-outer-name
-    pipeline_args = merge_dicts(
-        {
-            "pipeline_name": "foo",
-            "external_pipeline_origin": external_pipeline.get_external_origin(),
-        },
-        kwargs,
-    )
-    return create_run_for_test(instance, **pipeline_args)
-
-
-def test_submit_run(instance, coodinator):  # pylint: disable=redefined-outer-name
-    with get_foo_external_pipeline() as external_pipeline:
-        run = create_run(instance, external_pipeline, run_id="foo-1")
-        returned_run = coodinator.submit_run(run, external_pipeline)
-        assert returned_run.run_id == "foo-1"
-        assert returned_run.status == PipelineRunStatus.STARTING
-
-        assert len(instance.run_launcher.queue()) == 1
-        assert instance.run_launcher.queue()[0].run_id == "foo-1"
-        assert instance.run_launcher.queue()[0].status == PipelineRunStatus.STARTING
-        assert instance.get_run_by_id("foo-1")
-
-
-def test_submit_run_checks_status(instance, coodinator):  # pylint: disable=redefined-outer-name
-    with get_foo_external_pipeline() as external_pipeline:
-        run = create_run(
-            instance, external_pipeline, run_id="foo-1", status=PipelineRunStatus.STARTED
-        )
-        with pytest.raises(CheckError):
-            coodinator.submit_run(run, external_pipeline)
+            assert len(instance.run_launcher.queue()) == 1
+            assert instance.run_launcher.queue()[0].run_id == "foo-1"
+            assert instance.run_launcher.queue()[0].status == PipelineRunStatus.STARTING
+            assert instance.get_run_by_id("foo-1")
