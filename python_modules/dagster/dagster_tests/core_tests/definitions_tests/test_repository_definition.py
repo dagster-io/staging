0a9654bc14b600c5ef6dcb38567624cf5325479d
alangenfeld <alex@elementl.com>
2021-05-24 15:27:20 -0500

    [crag] support bare graph in repository
    Summary: Coerce a bare graph in to a "executable"
    
    Test Plan: added tests
    
    Reviewers: sandyryza, cdecarolis, prha, schrockn
    
    Subscribers:

--- python_modules/dagster/dagster_tests/core_tests/definitions_tests/test_repository_definition.py
+++ python_modules/dagster/dagster_tests/core_tests/definitions_tests/test_repository_definition.py
@@ -11,9 +11,10 @@ from dagster import (
     pipeline,
     repository,
     schedule,
+    sensor,
     solid,
 )
-from dagster.core.definitions import sensor
+from dagster.core.definitions.decorators.graph import graph
 
 
 def create_single_node_pipeline(name, called):
@@ -241,3 +242,36 @@ def test_direct_sensor_target():
         return [direct_sensor]
 
     assert test
+
+
+def test_bare_graph():
+    @solid
+    def ok():
+        return "sure"
+
+    @graph
+    def bare():
+        ok()
+
+    @repository
+    def test():
+        return [bare]
+
+    # should get updated once "executable" exists
+    assert test.get_pipeline("bare")
+
+
+def test_bare_graph_with_resources():
+    @solid(required_resource_keys={"stuff"})
+    def needy(context):
+        return context.resources.stuff
+
+    @graph
+    def bare():
+        needy()
+
+    with pytest.raises(DagsterInvalidDefinitionError, match='Resource "stuff" is required'):
+
+        @repository
+        def _test():
+            return [bare]
