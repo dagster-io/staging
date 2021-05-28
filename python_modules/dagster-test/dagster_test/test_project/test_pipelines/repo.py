bedf1317ae78eba55cfb63be9b14efded6f5fb2c
Sandy Ryza <sandy@elementl.com>
2021-05-28 08:12:51 -0700

    use IO managers in dagster_test pipelines
    Test Plan: bk
    
    Reviewers: yuhan, johann
    
    Differential Revision: https://dagster.phacility.com/D8159

--- python_modules/dagster-test/dagster_test/test_project/test_pipelines/repo.py
+++ python_modules/dagster-test/dagster_test/test_project/test_pipelines/repo.py
@@ -32,9 +32,9 @@ from dagster.core.definitions.decorators import daily_schedule, schedule
 from dagster.core.test_utils import nesting_composite_pipeline
 from dagster.utils import merge_dicts, segfault
 from dagster.utils.yaml_utils import merge_yamls
-from dagster_aws.s3 import s3_plus_default_intermediate_storage_defs, s3_resource
+from dagster_aws.s3 import s3_pickle_io_manager, s3_resource
+from dagster_gcp.gcs import gcs_pickle_io_manager
 from dagster_gcp.gcs.resources import gcs_resource
-from dagster_gcp.gcs.system_storage import gcs_plus_default_intermediate_storage_defs
 
 IS_BUILDKITE = bool(os.getenv("BUILDKITE"))
 
@@ -45,8 +45,9 @@ def celery_mode_defs(resources=None):
 
     return [
         ModeDefinition(
-            intermediate_storage_defs=s3_plus_default_intermediate_storage_defs,
-            resource_defs=resources if resources else {"s3": s3_resource},
+            resource_defs=dict(
+                **{"io_manager": s3_pickle_io_manager, "s3": s3_resource}, **(resources or {})
+            ),
             executor_defs=default_executors + [celery_executor, celery_k8s_job_executor],
         )
     ]
@@ -57,8 +58,9 @@ def k8s_mode_defs(resources=None):
 
     return [
         ModeDefinition(
-            intermediate_storage_defs=s3_plus_default_intermediate_storage_defs,
-            resource_defs=resources if resources else {"s3": s3_resource},
+            resource_defs=dict(
+                **{"io_manager": s3_pickle_io_manager, "s3": s3_resource}, **(resources or {})
+            ),
             executor_defs=default_executors + [dagster_k8s_executor],
         )
     ]
@@ -90,10 +92,7 @@ def hanging_solid(_):
 
 @pipeline(
     mode_defs=[
-        ModeDefinition(
-            intermediate_storage_defs=s3_plus_default_intermediate_storage_defs,
-            resource_defs={"s3": s3_resource},
-        )
+        ModeDefinition(resource_defs={"io_manager": s3_pickle_io_manager, "s3": s3_resource})
     ]
 )
 def hanging_pipeline():
@@ -102,10 +101,7 @@ def hanging_pipeline():
 
 @pipeline(
     mode_defs=[
-        ModeDefinition(
-            intermediate_storage_defs=s3_plus_default_intermediate_storage_defs,
-            resource_defs={"s3": s3_resource},
-        )
+        ModeDefinition(resource_defs={"io_manager": s3_pickle_io_manager, "s3": s3_resource})
     ]
 )
 def demo_pipeline():
@@ -126,8 +122,7 @@ def define_docker_celery_pipeline():
     @pipeline(
         mode_defs=[
             ModeDefinition(
-                intermediate_storage_defs=s3_plus_default_intermediate_storage_defs,
-                resource_defs={"s3": s3_resource},
+                resource_defs={"io_manager": s3_pickle_io_manager, "s3": s3_resource},
                 executor_defs=default_executors + [celery_docker_executor],
             )
         ]
@@ -140,10 +135,7 @@ def define_docker_celery_pipeline():
 
 @pipeline(
     mode_defs=[
-        ModeDefinition(
-            intermediate_storage_defs=gcs_plus_default_intermediate_storage_defs,
-            resource_defs={"gcs": gcs_resource},
-        )
+        ModeDefinition(resource_defs={"io_manager": gcs_pickle_io_manager, "gcs": gcs_resource})
     ]
 )
 def demo_pipeline_gcs():
@@ -152,10 +144,7 @@ def demo_pipeline_gcs():
 
 @pipeline(
     mode_defs=[
-        ModeDefinition(
-            intermediate_storage_defs=s3_plus_default_intermediate_storage_defs,
-            resource_defs={"s3": s3_resource},
-        )
+        ModeDefinition(resource_defs={"io_manager": gcs_pickle_io_manager, "gcs": gcs_resource})
     ]
 )
 def demo_error_pipeline():
@@ -461,10 +450,7 @@ def emit_airflow_execution_date(context):
 
 @pipeline(
     mode_defs=[
-        ModeDefinition(
-            intermediate_storage_defs=s3_plus_default_intermediate_storage_defs,
-            resource_defs={"s3": s3_resource},
-        )
+        ModeDefinition(resource_defs={"io_manager": s3_pickle_io_manager, "s3": s3_resource})
     ]
 )
 def demo_airflow_execution_date_pipeline():
