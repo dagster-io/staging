cc42e5541ccedbde552355877d3869fe9d86a173
Sandy Ryza <sandy@elementl.com>
2021-06-03 09:28:17 -0700

    make_log_handler_resource
    Summary:
    Depends on D8209.
    
    This could replace LoggerDefinitions.
    
    Test Plan: bk
    
    Differential Revision: https://dagster.phacility.com/D8222

--- python_modules/dagster/dagster_tests/core_tests/execution_tests/test_python_logging.py
+++ python_modules/dagster/dagster_tests/core_tests/execution_tests/test_python_logging.py
@@ -1,6 +1,7 @@
 import logging
 
-from dagster import DagsterInstance, execute_pipeline, pipeline, solid
+from dagster import DagsterInstance, ModeDefinition, execute_pipeline, pipeline, solid
+from dagster.core.execution.plan.python_logging import make_log_handler_resource
 
 
 def test_logging_capture_logger_defined_outside():
@@ -44,3 +45,31 @@ def test_logging_capture_logger_defined_inside():
     log_event_record = log_event_records[0]
     assert log_event_record.step_key == "my_solid"
     assert log_event_record.level == 30
+
+
+def test_make_log_handler_resource():
+    logger = logging.getLogger("some_logger")
+    mock_rollbar = []
+
+    class MockRollbarHandler(logging.Handler):
+        def emit(self, record):
+            mock_rollbar.append(record.msg)
+
+    @solid(required_resource_keys={"rollbar_log_handler"})
+    def my_solid(_):
+        logger.warning("some warning")
+
+    @pipeline(
+        mode_defs=[
+            ModeDefinition(
+                resource_defs={
+                    "rollbar_log_handler": make_log_handler_resource(MockRollbarHandler())
+                }
+            )
+        ]
+    )
+    def my_pipeline():
+        my_solid()
+
+    execute_pipeline(my_pipeline)
+    assert mock_rollbar == ["some warning"]
