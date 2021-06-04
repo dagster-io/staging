0878d78a0e3da127b924a5ea315b54c151051de0
Johann Miller <johann@elementl.com>
2021-06-04 15:45:05 -0400

    K8s executor use pipeline origin image
    Summary:
    
    Test Plan:
    Integration
    Reviewers:
    
    Subscribers:

--- python_modules/libraries/dagster-k8s/dagster_k8s/executor.py
+++ python_modules/libraries/dagster-k8s/dagster_k8s/executor.py
@@ -75,7 +75,7 @@ def k8s_job_executor(init_context: InitExecutorContext) -> Executor:
         dagster_home=run_launcher.dagster_home,
         instance_config_map=run_launcher.instance_config_map,
         postgres_password_secret=run_launcher.postgres_password_secret,
-        job_image=exc_cfg.get("job_image") or os.getenv("DAGSTER_CURRENT_IMAGE"),
+        job_image=exc_cfg.get("job_image"),
         image_pull_policy=exc_cfg.get("image_pull_policy"),
         image_pull_secrets=exc_cfg.get("image_pull_secrets"),
         service_account_name=exc_cfg.get("service_account_name"),
