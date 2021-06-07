b7564f985fa22a132108bbe022c0605460b1fb93
Owen Kephart <owen@elementl.com>
2021-06-07 15:20:51 -0700

    Hackernews demo in public repo
    Summary: This is just an initial commit to see if I can get tests passing. Still need to remove some things here and there to simplify this a bit (ex: the dynamic download pipeline stuff)
    
    Test Plan: bk
    
    Reviewers: sandyryza
    
    Differential Revision: https://dagster.phacility.com/D8278

---
+++ examples/hacker-news/hacker_news/repo.py
@@ -0,0 +1,29 @@
+from dagster import repository
+from hacker_news.pipelines.story_recommender import story_recommender
+
+from .pipelines.download_pipeline import download_pipeline, dynamic_download_pipeline
+from .schedules.hourly_hn_download_schedule import (
+    hourly_hn_download_prod_schedule,
+    hourly_hn_download_staging_schedule,
+    hourly_hn_dynamic_download_prod_schedule,
+)
+from .sensors.hn_tables_updated_sensor import story_recommender_on_hn_table_update
+
+
+@repository
+def hacker_news_repository():
+    pipelines = [
+        download_pipeline,
+        dynamic_download_pipeline,
+        story_recommender,
+    ]
+    schedules = [
+        hourly_hn_download_staging_schedule,
+        hourly_hn_download_prod_schedule,
+        hourly_hn_dynamic_download_prod_schedule,
+    ]
+    sensors = [
+        story_recommender_on_hn_table_update,
+    ]
+
+    return pipelines + schedules + sensors
