74f3dee0795953b62722bd3994b530370bcbb80b
prha <prha@elementl.com>
2021-06-09 09:39:07 -0700

    RFC: add instance log manager interface
    Summary:
    Rather than add a legacy flag onto the existing ComputeLogManager (D7945),
    this diff creates a separate interface for a InstanceLogManager class.  We could
    either do inheritance checks on the instance compute log manager, or pull this out as
    as a separate property on the instance.
    
    The new API explicitly separates out the log file data and the log file metadata.
    
    It also adds an abstractmethod `log_event`, which can either initialize an
    in-process DagsterLogManager and call log (outside of a pipeline context), or initialize
    the python logging handlers (@sandyryza, RFC).  This should be called from
    `DagsterInstance.handle_new_event` (e.g. whenever we are creating an event outside
    of a pipeline context)
    
    Test Plan: none
    
    Reviewers: alangenfeld, sandyryza
    
    Subscribers: sandyryza
    
    Differential Revision: https://dagster.phacility.com/D8385

---
+++ python_modules/dagster/dagster/core/storage/captured_log_manager.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractmethod
+from contextlib import contextmanager
+from typing import NamedTuple, Optional
+
+from dagster import check
+
+
+class CapturedLogData(
+    NamedTuple(
+        "_CapturedLogData",
+        [("data", Optional[bytes]), ("cursor", Optional[str])],
+    )
+):
+    def __new__(cls, data=None, cursor=None):
+        check.opt_bytes_param(data, "data")
+        check.opt_str_param(cursor, "cursor")
+        return super(CapturedLogData, cls).__new__(cls, data, cursor)
+
+
+class CapturedLogMetadata(
+    NamedTuple(
+        "_CapturedLogMetadata",
+        [("location", Optional[str]), ("download_url", Optional[str])],
+    )
+):
+    def __new__(cls, location=None, download_url=None):
+        check.opt_str_param(location, "location")
+        check.opt_str_param(download_url, "download_url")
+        return super(CapturedLogMetadata, cls).__new__(cls, location, download_url)
+
+
+class CapturedLogManager(ABC):
+    """Abstract base class for storing unstructured compute logs (stdout/stderr) from the compute
+    steps of pipeline solids."""
+
+    @abstractmethod
+    @contextmanager
+    def capture_logs(self, log_key: str, namespace: Optional[str] = None):
+        pass
+
+    @abstractmethod
+    def is_capture_complete(self, log_key: str, namespace: Optional[str] = None):
+        pass
+
+    @abstractmethod
+    def read_stdout(
+        self,
+        log_key: str,
+        namespace: Optional[str] = None,
+        cursor: str = None,
+        max_bytes: int = None,
+    ) -> CapturedLogData:
+        pass
+
+    @abstractmethod
+    def read_stderr(
+        self,
+        log_key: str,
+        namespace: Optional[str] = None,
+        cursor: str = None,
+        max_bytes: int = None,
+    ) -> CapturedLogData:
+        pass
+
+    @abstractmethod
+    def get_stdout_metadata(
+        self, log_key: str, namespace: Optional[str] = None
+    ) -> CapturedLogMetadata:
+        pass
+
+    @abstractmethod
+    def get_stderr_metadata(
+        self, log_key: str, namespace: Optional[str] = None
+    ) -> CapturedLogMetadata:
+        pass
+
+    def should_capture_run_by_step(self) -> bool:
+        return False
