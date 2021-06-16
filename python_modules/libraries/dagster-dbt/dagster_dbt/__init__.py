ca8e0d5f73ae8104842dd0beec9b6fcfbdabedb8
Owen Kephart <owen@elementl.com>
2021-06-15 17:54:56 -0700

    Refactor dagster-dbt integration / add dbt cli resource
    Summary:
    Refactored / reworked a lot of the cli side of the dagster-dbt integration. List of changes:
    
    * changed output data types so that we aren't passing around untyped dictionaries as much and so that we can have more consistent return types
    * removed the "summary" thing from DbtCliOutput. I can absolutely guarantee that nobody was using this because there was a bug that would make it empty regardless of the input :)
    * removed the weird asset materializations that represent just normal outputs. I feel like these make absolutely zero sense with the story that we want to tell around asset materializations, and will just clutter up the asset catalog while providing basically zero value.
    
    There are a couple breaking changes mixed in here (mostly related to the output types), but migration would be fairly easy for that stuff. Technically people might have been depending on the weird asset materializations, in which case this change would be more disruptive, but tbh I kinda doubt that this was that useful to anyone (famous last words).
    
    To be honest, I'd ideally like to remove a lot of the solids outside of the main path dbt commands (like test and run). These add a ton of maintenance overhead (if dbt adds a flag to any of these, people will be blocked on using it until we update the library), whereas the resource workflow is a lot more flexible (it has a permissive flags dictionary, so you can pass in whatever flag you want, even if we're not aware of it yet).
    
    The main value-add here is the dbt_cli_resource, which will solve basically all the ergonomic issues that I was experiencing w/ the dbt pipeline in demo-land. Now that this is in a working state,I'll try to put out a quick diff on that pipeline and link it to this to give a better sense of how that would look.
    
    Test Plan: bk
    
    Reviewers: sandyryza, cdecarolis
    
    Differential Revision: https://dagster.phacility.com/D8392

--- python_modules/libraries/dagster-dbt/dagster_dbt/__init__.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/__init__.py
@@ -4,6 +4,7 @@ from .cli import (
     DbtCliOutput,
     dbt_cli_compile,
     dbt_cli_docs_generate,
+    dbt_cli_resource,
     dbt_cli_run,
     dbt_cli_run_operation,
     dbt_cli_seed,
--- python_modules/libraries/dagster-dbt/dagster_dbt/cli/utils.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt/cli/utils.py
@@ -2,7 +2,7 @@ import json
 import os
 import re
 import subprocess
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List
 
 from dagster import check
 
@@ -11,26 +11,29 @@ from ..errors import (
     DagsterDbtCliHandledRuntimeError,
     DagsterDbtCliOutputsNotFoundError,
 )
+from .constants import DBT_RUN_RESULTS_COMMANDS, DEFAULT_DBT_TARGET_PATH
+from .types import DbtCliOutput, DbtResult
 
 
 def execute_cli(
     executable: str,
-    command: Tuple[str, ...],
+    command: str,
     flags_dict: Dict[str, Any],
     log: Any,
     warn_error: bool,
     ignore_handled_error: bool,
-) -> Dict[str, Any]:
+    target_path: str,
+) -> DbtCliOutput:
     """Executes a command on the dbt CLI in a subprocess."""
     check.str_param(executable, "executable")
-    check.tuple_param(command, "command", of_type=str)
+    check.str_param(command, "command")
     check.dict_param(flags_dict, "flags_dict", key_type=str)
     check.bool_param(warn_error, "warn_error")
     check.bool_param(ignore_handled_error, "ignore_handled_error")
 
     # Format the dbt CLI flags in the command..
     warn_error = ["--warn-error"] if warn_error else []
-    command_list = [executable, "--log-format", "json", *warn_error, *command]
+    command_list = [executable, "--log-format", "json", *warn_error, *command.split(" ")]
 
     for flag, value in flags_dict.items():
         if not value:
@@ -54,15 +57,15 @@ def execute_cli(
         command_list.append(str(value))
 
     # Execute the dbt CLI command in a subprocess.
-    command = " ".join(command_list)
-    log.info(f"Executing command: {command}")
+    full_command = " ".join(command_list)
+    log.info(f"Executing command: {full_command}")
 
     return_code = 0
     process = subprocess.Popen(command_list, stdout=subprocess.PIPE)
     logs = []
 
     output = []
-    for raw_line in process.stdout:
+    for raw_line in process.stdout or []:
         line = raw_line.decode("utf-8")
         output.append(line)
         try:
@@ -90,13 +93,18 @@ def execute_cli(
     if return_code == 1 and not ignore_handled_error:
         raise DagsterDbtCliHandledRuntimeError(logs=logs, raw_output=raw_output)
 
-    return {
-        "command": command,
-        "return_code": return_code,
-        "logs": logs,
-        "raw_output": raw_output,
-        "summary": extract_summary(logs),
-    }
+    result = None
+    if command in DBT_RUN_RESULTS_COMMANDS:
+        raw_run_results = parse_run_results(flags_dict["project-dir"], target_path)
+        result = DbtResult.from_run_results(raw_run_results)
+
+    return DbtCliOutput(
+        command=full_command,
+        return_code=return_code,
+        raw_output=raw_output,
+        logs=logs,
+        result=result,
+    )
 
 
 SUMMARY_RE = re.compile(r"PASS=(\d+) WARN=(\d+) ERROR=(\d+) SKIP=(\d+) TOTAL=(\d+)")
@@ -125,7 +133,7 @@ def extract_summary(logs: List[Dict[str, str]]):
     return dict(zip(SUMMARY_LABELS, summary))
 
 
-def parse_run_results(path: str, target_path: str = "target") -> Dict[str, Any]:
+def parse_run_results(path: str, target_path: str = DEFAULT_DBT_TARGET_PATH) -> Dict[str, Any]:
     """Parses the `target/run_results.json` artifact that is produced by a dbt process."""
     run_results_path = os.path.join(path, target_path, "run_results.json")
     try:
