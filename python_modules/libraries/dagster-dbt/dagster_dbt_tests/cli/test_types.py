84e93233f4074db80c4fc8c35634fcfa22804748
Owen Kephart <owen@elementl.com>
2021-06-17 10:21:18 -0700

    wip dry run

--- python_modules/libraries/dagster-dbt/dagster_dbt_tests/cli/test_types.py
+++ python_modules/libraries/dagster-dbt/dagster_dbt_tests/cli/test_types.py
@@ -1,7 +1,6 @@
 import pickle
 
 from dagster_dbt import DbtCliOutput
-from dagster_dbt.types import DbtResult
 
 from ..test_types import DBT_RESULT_DICT
 
@@ -12,17 +11,17 @@ class TestDbtCliOutput:
             command="dbt run",
             return_code=0,
             raw_output="The raw output (stdout).",
-            result=DbtResult.from_run_results(DBT_RESULT_DICT),
+            result=DBT_RESULT_DICT,
             logs=[],
         )
-        assert len(dco.result) == len(DBT_RESULT_DICT["results"])
+        assert len(dco.result["results"]) == len(DBT_RESULT_DICT["results"])
 
     def test_pickle_roundtrip(self):  # pylint: disable=unused-argument
         dco = DbtCliOutput(
             command="dbt run",
             return_code=0,
             raw_output="The raw output (stdout).",
-            result=DbtResult.from_run_results(DBT_RESULT_DICT),
+            result=DBT_RESULT_DICT,
             logs=[{"some": {"nested": {"logs"}}}, {"other": "log"}],
         )
-        assert pickle.loads(pickle.dumps(dco)) == dco
+        assert vars(pickle.loads(pickle.dumps(dco))) == vars(dco)
