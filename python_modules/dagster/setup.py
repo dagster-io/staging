02f24b0525bbc227e10d4d9181176899d77f0358
Jordan Sanders <jordan@elementl.com>
2021-06-23 10:53:58 -0500

    Pin sqlalchemy
    Summary:
    https://github.com/sqlalchemy/sqlalchemy/releases/tag/rel_1_4_19 is
    breaking pylint builds:
    
    https://buildkite.com/dagster/dagster-diffs/builds/19300#7197ed85-3561-44f1-9428-75a8b1a02877
    
    Pinning to a lower version while I figure out what's actually happening.
    
    Test Plan:
    unit
    
    Reviewers:
    alangenfeld, max, dgibson
    
    Subscribers:

--- python_modules/dagster/setup.py
+++ python_modules/dagster/setup.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
             "tabulate",
             "tqdm",
             "typing_compat",
-            "sqlalchemy>=1.0",
+            "sqlalchemy>=1.0,<1.4.19",
             "toposort>=1.0",
             "watchdog>=0.8.3",
             'psutil >= 1.0; platform_system=="Windows"',
