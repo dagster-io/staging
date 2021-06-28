dagster-io/staging

commit e1482f34ae228519a515f8bde48a1052870fb3dd

correctly process configs of io managers on solids inside composite
Summary:
depends on D8272 (which bans io managers on composite solids)

this diff makes sure solids inside composite can use their own custom io managers and the configs are processed correctly.

for example https://github.com/dagster-io/dagster/issues/3656
the code will be:
```
@root_input_manager(input_config_schema={"test": str})
def my_root(context):
    return context.config["test"]

@solid(input_defs=[InputDefinition("data", dagster_type=str, root_manager_key="my_root")])
def inner_solid(_, data):
    return data

@composite_solid
def my_composite():
    return inner_solid()
```

and the correct run config would be:
```
solids:
  my_composite:
    solids:
      inner_solid:
        inputs:
	  data:
            test: hello # instead of composite:inputs, we get the input config from inner solid
```

this should also address https://github.com/dagster-io/dagster/issues/4130 where we will only respect the io managers set on inner solids

Test Plan: units

Reviewers: sandyryza, alangenfeld, cdecarolis

Reviewed By: alangenfeld

Differential Revision: https://dagster.phacility.com/D8365

python_modules/dagster/dagster/core/definitions/pipeline.py
@@ -833,6 +833,15 @@ def _checked_input_resource_reqs_for_mode(
     )
 
     for solid in solid_dict.values():
+        if solid.is_composite:
+            # check inner solids
+            resource_reqs.update(
+                _checked_input_resource_reqs_for_mode(
+                    dependency_structure=solid.definition.dependency_structure,
+                    solid_dict=solid.definition.solid_dict,
+                    mode_def=mode_def,
+                )
+            )
         for handle in solid.input_handles():
             if dependency_structure.has_deps(handle):
                 for source_output_handle in dependency_structure.get_deps_list(handle):

python_modules/dagster/dagster_tests/core_tests/storage_tests/test_io_manager_composites.py
@@ -117,3 +117,61 @@ def test_composite_solid_upstream_output():
 
     result = execute_pipeline(my_pipeline)
     assert result.success
+
+
+def test_io_manager_config_inside_composite():
+    stored_dict = {}
+
+    @io_manager(output_config_schema={"output_suffix": str})
+    def inner_manager(_):
+        class MyHardcodedIOManager(IOManager):
+            def handle_output(self, context, obj):
+                keys = tuple(
+                    context.get_run_scoped_output_identifier() + [context.config["output_suffix"]]
+                )
+                stored_dict[keys] = obj
+
+            def load_input(self, context):
+                keys = tuple(
+                    context.upstream_output.get_run_scoped_output_identifier()
+                    + [context.upstream_output.config["output_suffix"]]
+                )
+                return stored_dict[keys]
+
+        return MyHardcodedIOManager()
+
+    @solid(output_defs=[OutputDefinition(io_manager_key="inner_manager")])
+    def my_solid(_):
+        return "hello"
+
+    @solid
+    def my_solid_takes_input(_, x):
+        assert x == "hello"
+        return x
+
+    @composite_solid
+    def my_composite_solid():
+        return my_solid_takes_input(my_solid())
+
+    @pipeline(
+        mode_defs=[ModeDefinition(name="default", resource_defs={"inner_manager": inner_manager})]
+    )
+    def my_pipeline():
+        my_composite_solid()
+
+    result = execute_pipeline(
+        my_pipeline,
+        run_config={
+            "solids": {
+                "my_composite_solid": {
+                    "solids": {"my_solid": {"outputs": {"result": {"output_suffix": "my_suffix"}}}},
+                }
+            }
+        },
+    )
+    assert result.success
+    assert result.output_for_solid("my_composite_solid.my_solid") == "hello"
+    assert (
+        stored_dict.get((result.run_id, "my_composite_solid.my_solid", "result", "my_suffix"))
+        == "hello"
+    )

python_modules/dagster/dagster/core/definitions/graph.py
@@ -159,6 +159,11 @@ class GraphDefinition(NodeDefinition):
         return list(set(self._solid_dict.values()))
 
     @property
+    def solid_dict(self) -> Dict[str, Solid]:
+        """Dict[str, Solid]: A dict of top-level solids in the graph, keyed on solid names."""
+        return self._solid_dict
+
+    @property
     def node_defs(self) -> List[NodeDefinition]:
         """List[NodeDefinition]: List of nodes in the graph."""
         return self._node_defs

python_modules/dagster/dagster_tests/core_tests/storage_tests/test_input_manager.py
@@ -372,3 +372,34 @@ def test_no_root_manager_composite():
         )
         def _(data):
             _ = inner_solid(data=data)
+
+
+def test_root_manager_inside_composite():
+    @root_input_manager(input_config_schema={"test": str})
+    def my_root(context):
+        return context.config["test"]
+
+    @solid(input_defs=[InputDefinition("data", dagster_type=str, root_manager_key="my_root")])
+    def inner_solid(_, data):
+        return data
+
+    @composite_solid
+    def my_composite_solid():
+        return inner_solid()
+
+    @pipeline(mode_defs=[ModeDefinition(name="default", resource_defs={"my_root": my_root})])
+    def my_pipeline():
+        my_composite_solid()
+
+    result = execute_pipeline(
+        my_pipeline,
+        run_config={
+            "solids": {
+                "my_composite_solid": {
+                    "solids": {"inner_solid": {"inputs": {"data": {"test": "hello"}}}},
+                }
+            }
+        },
+    )
+
+    assert result.output_for_solid("my_composite_solid") == "hello"

commit 2106fef46469a350d3897d30ea06d43ad043a763

when checking if inputs are connected, account for mapped input hooked with outer graph
Summary:
fix bugs introduced in D8365
user impact:
1. incorrect definition error https://dagster.slack.com/archives/C01U954MEER/p1624642083152400 : covered in `test_inner_inputs_connected_to_outer_dependency`
2. incorrect runtime error https://github.com/dagster-io/dagster/issues/3656 : covered in `test_inner_inputs_connected_to_outer_dependency_with_root_input_manager`

details
1. when checking `dependency_structure.has_deps` in pipeline def, we should also check if the input handle is a mapped input (inside a composite solid) that is connected to the outer dependency structure. otherwise, the pipeline definition validation will mistakely think the inputs aren't connected.
2. when checking whether it's a `FromRootInputManager`, we should also exclude inputs that are connected to outer dependency structure. otherwise, when there's root_manager_key, the inner solids will always connect to the root manager as opposed to the upstream output

Test Plan: sandyryza, alangenfeld

Reviewers:

Subscribers:

python_modules/dagster/dagster/core/definitions/dependency.py
@@ -189,7 +189,7 @@ class Solid:
             is not None
         )
 
-    def container_mapped_input(self, input_name: str) -> InputMapping:
+    def container_mapped_input(self, input_name: str) -> Optional[InputMapping]:
         return self.graph_definition.input_mapping_for_pointer(InputPointer(self.name, input_name))
 
     def container_maps_fan_in_input(self, input_name: str, fan_in_index: int) -> bool:

python_modules/dagster/dagster/core/definitions/pipeline.py
@@ -30,6 +30,7 @@ from .dependency import (
     MultiDependencyDefinition,
     Solid,
     SolidHandle,
+    SolidInputHandle,
     SolidInvocation,
 )
 from .graph import GraphDefinition
@@ -823,6 +824,8 @@ def _checked_input_resource_reqs_for_mode(
     dependency_structure: DependencyStructure,
     solid_dict: Dict[str, Solid],
     mode_def: ModeDefinition,
+    outer_dependency_structure: Optional[DependencyStructure] = None,
+    outer_solid: Optional[Solid] = None,
 ) -> Set[str]:
     resource_reqs = set()
     mode_root_input_managers = set(
@@ -839,11 +842,26 @@ def _checked_input_resource_reqs_for_mode(
                     dependency_structure=solid.definition.dependency_structure,
                     solid_dict=solid.definition.solid_dict,
                     mode_def=mode_def,
+                    outer_dependency_structure=dependency_structure,
+                    outer_solid=solid,
                 )
             )
         for handle in solid.input_handles():
+            source_output_handles = None
             if dependency_structure.has_deps(handle):
-                for source_output_handle in dependency_structure.get_deps_list(handle):
+                # input is connected to outputs from the same dependency structure
+                source_output_handles = dependency_structure.get_deps_list(handle)
+            elif solid.container_maps_input(handle.input_name):
+                # input is connected to outputs from outer dependency structure, e.g. first solids
+                # in a composite
+                outer_handle = SolidInputHandle(
+                    solid=outer_solid,
+                    input_def=solid.container_mapped_input(handle.input_name).definition,
+                )
+                source_output_handles = outer_dependency_structure.get_deps_list(outer_handle)
+            if source_output_handles:
+                # input is connected to source output handles within the graph
+                for source_output_handle in source_output_handles:
                     output_manager_key = source_output_handle.output_def.io_manager_key
                     output_manager_def = mode_def.resource_defs[output_manager_key]
                     if not isinstance(output_manager_def, IInputManagerDefinition):
@@ -857,6 +875,7 @@ def _checked_input_resource_reqs_for_mode(
                             f"the upstream output."
                         )
             else:
+                # input is unconnected
                 input_def = handle.input_def
                 if (
                     not input_def.dagster_type.loader

python_modules/dagster/dagster/core/execution/plan/plan.py
@@ -371,8 +371,15 @@ def get_step_input_source(
     solid_config = plan_builder.resolved_run_config.solids.get(str(handle))
 
     input_def = solid.definition.input_def_named(input_name)
-    if input_def.root_manager_key and not dependency_structure.has_deps(input_handle):
-        return FromRootInputManager(solid_handle=handle, input_name=input_name)
+
+    if (
+        input_def.root_manager_key
+        # input is unconnected inside the current dependency structure
+        and not dependency_structure.has_deps(input_handle)
+    ):
+        #  make sure input is unconnected in the outer dependency structure too
+        if not solid.container_maps_input(input_handle.input_name):
+            return FromRootInputManager(solid_handle=handle, input_name=input_name)
 
     if dependency_structure.has_direct_dep(input_handle):
         solid_output_handle = dependency_structure.get_direct_dep(input_handle)

python_modules/dagster/dagster_tests/core_tests/storage_tests/test_io_manager_composites.py
@@ -1,12 +1,14 @@
 import pytest
 from dagster import (
     DagsterInvalidDefinitionError,
+    DagsterType,
     InputDefinition,
     ModeDefinition,
     OutputDefinition,
     composite_solid,
     execute_pipeline,
     pipeline,
+    root_input_manager,
     solid,
 )
 from dagster.core.storage.io_manager import IOManager, io_manager
@@ -175,3 +177,59 @@ def test_io_manager_config_inside_composite():
         stored_dict.get((result.run_id, "my_composite_solid.my_solid", "result", "my_suffix"))
         == "hello"
     )
+
+
+def test_inner_inputs_connected_to_outer_dependency():
+    my_dagster_type = DagsterType(name="foo", type_check_fn=lambda _, _a: True)
+
+    @solid(input_defs=[InputDefinition("data", my_dagster_type)])
+    def inner_solid(data):
+        return data
+
+    @composite_solid(input_defs=[InputDefinition("data", my_dagster_type)])
+    def my_composite(data):
+        return inner_solid(data)
+
+    @solid
+    def top_level_solid():
+        return "from top_level_solid"
+
+    @pipeline
+    def my_pipeline():
+        # inner_solid should be connected to top_level_solid
+        my_composite(top_level_solid())
+
+    result = execute_pipeline(my_pipeline)
+    assert result.success
+    assert result.output_for_solid("my_composite.inner_solid") == "from top_level_solid"
+
+
+def test_inner_inputs_connected_to_outer_dependency_with_root_input_manager():
+    called = {}
+
+    @root_input_manager(input_config_schema={"test": str})
+    def my_root(_):
+        # should not reach
+        called["my_root"] = True
+
+    @solid(input_defs=[InputDefinition("data", dagster_type=str, root_manager_key="my_root")])
+    def inner_solid(_, data):
+        return data
+
+    @composite_solid
+    def my_composite(data: str):
+        return inner_solid(data)
+
+    @solid
+    def top_level_solid():
+        return "from top_level_solid"
+
+    @pipeline(mode_defs=[ModeDefinition(resource_defs={"my_root": my_root})])
+    def my_pipeline():
+        # inner_solid should be connected to top_level_solid
+        my_composite(top_level_solid())
+
+    result = execute_pipeline(my_pipeline)
+    assert result.success
+    assert result.output_for_solid("my_composite.inner_solid") == "from top_level_solid"
+    assert "my_root" not in called