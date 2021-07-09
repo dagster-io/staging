41734ad90eddd6ddafa9b894ad42e02ba60b4223
alangenfeld <alex@elementl.com>
2021-07-09 16:51:11 -0500

    [dag-star] graphql websocket endpoint
    Test Plan: wip
    
    Differential Revision: https://dagster.phacility.com/D8810

--- python_modules/dagit/dagit_tests/starlette/test_app.py
+++ python_modules/dagit/dagit_tests/starlette/test_app.py
@@ -1,5 +1,22 @@
-from dagit.starlette import ROOT_ADDRESS_STATIC_RESOURCES
+from dagit.starlette import (
+    GQL_CONNECTION_ACK,
+    GQL_CONNECTION_INIT,
+    GQL_DATA,
+    GQL_ERROR,
+    GQL_START,
+    GRAPHQL_WS,
+    ROOT_ADDRESS_STATIC_RESOURCES,
+)
+from dagster import execute_pipeline, pipeline
 from starlette.testclient import TestClient
+
+EVENT_LOG_SUBSCRIPTION = """
+    subscription PipelineRunLogsSubscription($runId: ID!) {
+        pipelineRunLogs(runId: $runId) {
+            __typename
+        }
+    }
+"""
 
 
 def test_dagit_info(empty_app):
@@ -51,3 +68,52 @@ def test_graphql_post(empty_app):
     )
     assert response.status_code == 200, response.text
     assert response.json() == {"data": {"__typename": "Query"}}
+
+
+def test_graphql_ws_error(empty_app):
+    # wtf pylint
+    # pylint: disable=not-context-manager
+    with TestClient(empty_app).websocket_connect("/graphql", GRAPHQL_WS) as ws:
+        ws.send_json({"type": GQL_CONNECTION_INIT})
+        ws.send_json(
+            {
+                "type": GQL_START,
+                "id": "1",
+                "payload": {"query": "subscription { oops }"},
+            }
+        )
+
+        response = ws.receive_json()
+        assert response["type"] == GQL_CONNECTION_ACK
+
+        response = ws.receive_json()
+
+        assert response["id"] == "1"
+        assert response["type"] == GQL_ERROR
+
+
+def test_graphql_ws_success(instance, empty_app):
+    @pipeline
+    def _test():
+        pass
+
+    result = execute_pipeline(_test, instance=instance)
+    run_id = result.run_id
+    # wtf pylint
+    # pylint: disable=not-context-manager
+    with TestClient(empty_app).websocket_connect("/graphql", GRAPHQL_WS) as ws:
+        ws.send_json({"type": GQL_CONNECTION_INIT})
+        ws.send_json(
+            {
+                "type": GQL_START,
+                "id": "1",
+                "payload": {"query": EVENT_LOG_SUBSCRIPTION, "variables": {"runId": run_id}},
+            }
+        )
+
+        response = ws.receive_json()
+        assert response["type"] == GQL_CONNECTION_ACK
+
+        response = ws.receive_json()
+        assert response["id"] == "1"
+        assert response["type"] == GQL_DATA
