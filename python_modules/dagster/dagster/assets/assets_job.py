e3cc113cf81630ad7d3bcd4050c973e1a8b128c1
Sandy Ryza <sandy@elementl.com>
2021-07-20 15:50:39 -0700

    infer dependencies from arg names

--- python_modules/dagster/dagster/assets/assets_job.py
+++ python_modules/dagster/dagster/assets/assets_job.py
@@ -1,4 +1,4 @@
-from typing import AbstractSet, Any, Dict, List, Optional, Tuple, Union, cast
+from typing import AbstractSet, Any, Dict, List, Mapping, Optional, Sequence, Tuple, Union, cast
 
 from dagster import (
     DependencyDefinition,
@@ -6,31 +6,54 @@ from dagster import (
     PipelineDefinition,
     ResourceDefinition,
     RootInputManagerDefinition,
-    SolidDefinition,
     build_input_context,
     build_output_context,
+    check,
     root_input_manager,
 )
 from dagster.core.definitions.dependency import IDependencyDefinition, SolidInvocation
+from dagster.core.definitions.events import AssetKey
 from dagster.core.definitions.graph import GraphDefinition
 from dagster.core.definitions.i_solid_definition import NodeDefinition
+from dagster.core.errors import DagsterInvalidDefinitionError
 from dagster.utils.merger import merge_dicts
 
+from .decorators import LOGICAL_ASSET_KEY
 from .source_asset import SourceAsset
 
 
 def build_assets_job(
     name: str,
-    assets: List[NodeDefinition],
-    source_assets: Optional[List[SourceAsset]] = None,
-    resource_defs: Dict[str, ResourceDefinition] = None,
+    assets: Sequence[NodeDefinition],
+    source_assets: Optional[Sequence[SourceAsset]] = None,
+    resource_defs: Optional[Dict[str, ResourceDefinition]] = None,
     description: Optional[str] = None,
 ) -> PipelineDefinition:
-    """Builds a job that refreshes the given assets."""
-    source_paths = {source.path for source in source_assets or []}
+    """Builds a job that materializes the given assets.
 
-    solid_deps = build_solid_deps(assets, source_paths)
-    root_manager = build_root_manager(source_assets or [])
+    The dependencies between the nodes in the job are determined by the asset dependencies defined
+    in the metadata on the provided asset nodes.
+
+    Args:
+        name (str): The name of the job.
+        assets (Sequence[NodeDefinition]): A list of ops or graphs that are assets.
+        source_assets (Optional[Sequence[SourceAsset]]): A list of assets that are not materialized
+            by this job, but that assets in this job depend on.
+        resource_defs (Optional[Dict[str, ResourceDefinition]]): Resource defs to be included in
+            this job.
+        description (Optional[str]): A description of the job.
+
+    Returns:
+        PipelineDefinition: A job that materializes the given assets.
+    """
+    check.str_param(name, "name")
+    check.list_param(assets, "assets", of_type=NodeDefinition)
+    check.opt_list_param(source_assets, "source_assets", of_type=SourceAsset)
+    check.opt_str_param(description, "description")
+    source_assets_by_key = {source.key: source for source in source_assets or {}}
+
+    solid_deps = build_solid_deps(assets, source_assets_by_key.keys())
+    root_manager = build_root_manager(source_assets_by_key)
 
     return GraphDefinition(
         name=name,
@@ -46,22 +69,22 @@ def build_assets_job(
 def build_solid_deps(
     assets: List[NodeDefinition], source_paths: AbstractSet[Tuple[str]]
 ) -> Dict[Union[str, SolidInvocation], Dict[str, IDependencyDefinition]]:
-    solids_by_logical_asset: Dict[str, SolidDefinition] = {}
+    solids_by_logical_asset: Dict[AssetKey, NodeDefinition] = {}
     for solid in assets:
         n_outputs = len(solid.output_defs)
         if len(solid.output_defs) != 1:
-            raise ValueError(
+            raise DagsterInvalidDefinitionError(
                 f"Solids must have exactly one output, but '{solid.name}'' has {n_outputs}"
             )
 
         output_def = solid.output_defs[0]
-        if output_def.metadata is None or "logical_asset" not in output_def.metadata:
-            raise ValueError(f"Output metadata of solid '{solid.name}' is missing 'logical_asset'")
+        logical_asset = get_asset_key(
+            output_def.metadata, f"Output metadata of solid '{solid.name}'"
+        )
 
-        logical_asset = output_def.metadata["logical_asset"]
         if logical_asset in solids_by_logical_asset:
             prev_solid = solids_by_logical_asset[logical_asset].name
-            raise ValueError(
+            raise DagsterInvalidDefinitionError(
                 f"Two solids produce the same logical asset: '{solid.name}' and '{prev_solid.name}"
             )
 
@@ -71,19 +94,17 @@ def build_solid_deps(
     for solid in assets:
         solid_deps[solid.name] = {}
         for input_def in solid.input_defs:
-            if input_def.metadata is None or "logical_asset" not in input_def.metadata:
-                raise ValueError(
-                    f"Metadata for input '{input_def.name}'' of solid '{solid.name}' is missing "
-                    "'logical_asset'"
-                )
+            logical_asset = get_asset_key(
+                input_def.metadata,
+                f"Metadata for input '{input_def.name}' of solid '{solid.name}'",
+            )
 
-            logical_asset = input_def.metadata["logical_asset"]
             if logical_asset in solids_by_logical_asset:
                 solid_deps[solid.name][input_def.name] = DependencyDefinition(
                     solids_by_logical_asset[logical_asset].name, "result"
                 )
             elif logical_asset not in source_paths:
-                raise ValueError(
+                raise DagsterInvalidDefinitionError(
                     f"Logical input asset '{logical_asset}' for solid '{solid.name}' is not "
                     "produced by any of the provided asset solids and is not one of the provided "
                     "sources"
@@ -92,18 +113,16 @@ def build_solid_deps(
     return solid_deps
 
 
-def build_root_manager(source_assets: List[SourceAsset]) -> RootInputManagerDefinition:
-    source_assets_by_path = {source_asset.path: source_asset for source_asset in source_assets}
-
+def build_root_manager(
+    source_assets_by_key: Mapping[AssetKey, SourceAsset]
+) -> RootInputManagerDefinition:
     @root_input_manager(required_resource_keys={"io_manager"})
     def _root_manager(input_context: InputContext) -> Any:
-        if input_context.metadata is None or "logical_asset" not in input_context.metadata:
-            raise ValueError("Metadata for input is missing 'logical_asset'")
-
-        source_asset = source_assets_by_path[input_context.metadata["logical_asset"]]
+        source_asset_path = get_asset_key(input_context.metadata, "Metadata for input")
+        source_asset = source_assets_by_key[source_asset_path]
 
         output_context = build_output_context(
-            name=source_asset.path[-1], step_key="none", metadata=source_asset.metadata
+            name=source_asset.key.path[-1], step_key="none", metadata=source_asset.metadata
         )
         input_context_with_upstream = build_input_context(
             name=input_context.name, upstream_output=output_context
@@ -112,3 +131,13 @@ def build_root_manager(source_assets: List[SourceAsset]) -> RootInputManagerDefi
         return cast(Any, input_context.resources).io_manager.load_input(input_context_with_upstream)
 
     return _root_manager
+
+
+def get_asset_key(metadata: Optional[Mapping[str, Any]], error_prefix: str) -> AssetKey:
+    if metadata is None:
+        raise DagsterInvalidDefinitionError(f"{error_prefix}' is None")
+
+    if LOGICAL_ASSET_KEY not in metadata:
+        raise DagsterInvalidDefinitionError(f"{error_prefix} is missing 'logical_asset_key'")
+
+    return metadata[LOGICAL_ASSET_KEY]
