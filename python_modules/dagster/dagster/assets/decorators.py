17ec4fe7b13aac4cebdeeb916f748e915a7cb3d2
Sandy Ryza <sandy@elementl.com>
2021-07-20 15:50:39 -0700

    infer dependencies from arg names

--- python_modules/dagster/dagster/assets/decorators.py
+++ python_modules/dagster/dagster/assets/decorators.py
@@ -1,33 +1,39 @@
-from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union
+from typing import Any, Callable, Mapping, Optional, Sequence, Set
 
 from dagster import InputDefinition, OutputDefinition, SolidDefinition
 from dagster.core.decorator_utils import get_function_params, get_valid_name_permutations
 from dagster.core.definitions.decorators.solid import _Solid
+from dagster.core.definitions.events import AssetKey
+from dagster.utils.merger import merge_dicts
+
+LOGICAL_ASSET_KEY = "logical_asset_key"
 
 
 def asset(
     name: Optional[str] = None,
-    namespace: Optional[List[str]] = None,
-    inputs: Optional[List[Union[Tuple[str], List[str]]]] = None,
-    metadata: Optional[Dict[str, Any]] = None,
+    namespace: Optional[str] = None,
+    input_namespaces: Optional[Mapping[str, Sequence]] = None,
+    metadata: Optional[Mapping[str, Any]] = None,
     description: Optional[str] = None,
     required_resource_keys: Optional[Set[str]] = None,
+    io_manager_key: Optional[str] = None,
 ) -> Callable[[Callable[..., Any]], SolidDefinition]:
     """Create a solid that updates an asset."""
     if callable(name):
         return _Asset()(name)
 
-    if metadata and "namespace" in metadata:
+    if metadata and ("namespace" in metadata or LOGICAL_ASSET_KEY in metadata):
         raise ValueError("TODO")
 
     def inner(fn: Callable[..., Any]) -> SolidDefinition:
         return _Asset(
             name=name,
             namespace=namespace,
-            inputs=inputs,
+            input_namespaces=input_namespaces,
             metadata=metadata,
             description=description,
             required_resource_keys=required_resource_keys,
+            io_manager_key=io_manager_key,
         )(fn)
 
     return inner
@@ -37,18 +43,20 @@ class _Asset:
     def __init__(
         self,
         name: Optional[str] = None,
-        namespace: Optional[List[str]] = None,
-        inputs: Optional[List[Union[Tuple[str], List[str]]]] = None,
-        metadata: Optional[Dict[str, Any]] = None,
+        namespace: Optional[str] = None,
+        input_namespaces: Mapping[str, Sequence] = None,
+        metadata: Optional[Mapping[str, Any]] = None,
         description: Optional[str] = None,
         required_resource_keys: Optional[Set[str]] = None,
+        io_manager_key: Optional[str] = None,
     ):
         self.name = name
         self.namespace = namespace
-        self.inputs = inputs
+        self.input_namespaces = input_namespaces or {}
         self.metadata = metadata
         self.description = description
         self.required_resource_keys = required_resource_keys
+        self.io_manager_key = io_manager_key
 
     def __call__(self, fn: Callable):
         asset_name = self.name or fn.__name__
@@ -57,19 +65,33 @@ class _Asset:
             "context"
         )
         input_params = params[1:] if is_context_provided else params
-        if len(input_params) != len(self.inputs or []):
-            raise ValueError("TODO")
 
         input_defs = [
             InputDefinition(
                 name=input_param.name,
-                metadata={"logical_asset": tuple(input_path)},
+                metadata={
+                    LOGICAL_ASSET_KEY: AssetKey(
+                        tuple(
+                            filter(
+                                None,
+                                [
+                                    self.input_namespaces.get(input_param.name, self.namespace),
+                                    input_param.name,
+                                ],
+                            )
+                        )
+                    ),
+                },
                 root_manager_key="root_manager",
             )
-            for input_path, input_param in zip(self.inputs or [], input_params)
+            for input_param in input_params
         ]
         output_def = OutputDefinition(
-            metadata={"logical_asset": tuple(self.namespace or ()) + (asset_name,)}
+            metadata=merge_dicts(
+                {LOGICAL_ASSET_KEY: AssetKey(tuple(filter(None, [self.namespace, asset_name])))},
+                self.metadata or {},
+            ),
+            io_manager_key=self.io_manager_key,
         )
         return _Solid(
             name=asset_name,
--- python_modules/dagster/dagster_tests/assets_tests/test_decorators.py
+++ python_modules/dagster/dagster_tests/assets_tests/test_decorators.py
@@ -1,4 +1,4 @@
-from dagster import SolidDefinition
+from dagster import AssetKey, SolidDefinition
 from dagster.assets import asset
 
 
@@ -13,13 +13,27 @@ def test_asset_no_decorator_args():
 
 
 def test_asset_with_inputs():
-    @asset(inputs=[["source1"]])
+    @asset
     def my_asset(arg1):
         return arg1
 
     assert isinstance(my_asset, SolidDefinition)
     assert len(my_asset.output_defs) == 1
     assert len(my_asset.input_defs) == 1
+    assert my_asset.input_defs[0].metadata["logical_asset_key"] == AssetKey("arg1")
+
+
+def test_asset_with_inputs_and_namespace():
+    @asset(namespace="my_namespace")
+    def my_asset(arg1):
+        return arg1
+
+    assert isinstance(my_asset, SolidDefinition)
+    assert len(my_asset.output_defs) == 1
+    assert len(my_asset.input_defs) == 1
+    assert my_asset.input_defs[0].metadata["logical_asset_key"] == AssetKey(
+        ["my_namespace", "arg1"]
+    )
 
 
 def test_asset_with_context_arg():
@@ -29,3 +43,21 @@ def test_asset_with_context_arg():
 
     assert isinstance(my_asset, SolidDefinition)
     assert len(my_asset.input_defs) == 0
+
+
+def test_all_fields():
+    @asset(
+        required_resource_keys={"abc", "123"},
+        io_manager_key="my_io_key",
+        description="some description",
+        metadata={"metakey": "metaval"},
+    )
+    def my_asset():
+        pass
+
+    assert my_asset.required_resource_keys == {"abc", "123"}
+    assert my_asset.description == "some description"
+    assert len(my_asset.output_defs) == 1
+    output_def = my_asset.output_defs[0]
+    assert output_def.io_manager_key == "my_io_key"
+    assert output_def.metadata["metakey"] == "metaval"
