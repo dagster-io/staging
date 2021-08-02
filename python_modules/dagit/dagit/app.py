de0ea121cc9ad143c03f966eb8278a851cffcdd1
gibsondan <daniel@elementl.com>
2021-08-02 13:35:49 -0500

    [dagit] Add Permissions query
    Summary: Create a `PermissionsProvider` that queries for permissions via GraphQL, as arranged in https://dagster.phacility.com/D9122.
    
    Test Plan: Load Dagit, verify that permissions are queried and used properly.
    
    Reviewers:
    
    dgibson
    
    Subscribers:

--- python_modules/dagit/dagit/app.py
+++ python_modules/dagit/dagit/app.py
@@ -139,47 +139,11 @@ def download_dump_view(context):
     return view
 
 
-def register_permissions(app, context: IWorkspaceProcessContext):
-    # TODO: This is a nasty hack because we aren't currently using Jinja templating to render the
-    # static views; we should instead be injecting values into the Jinja environment and using Jinja
-    # to template them in rather than using string replace
-    def make_wrapper(view_fn):
-        def _wrapper(*args, **kwargs):
-            res = view_fn(*args, **kwargs)
-            if not isinstance(res, str):
-                return res
-
-            templated_res = res.replace(
-                '"[permissions_here]"',
-                json.dumps(
-                    get_user_permissions(context),
-                ),
-            )
-            return templated_res
-
-        return _wrapper
-
-    for view_function_key in app.view_functions:
-        if view_function_key != "routes.graphql":
-            app.view_functions[view_function_key] = make_wrapper(
-                app.view_functions[view_function_key]
-            )
-    # TODO: This is a brutal hack which is following on a brutal hack (treating
-    # all paths other than / as 404s and falling through to an error handler) -- we should instead
-    # have a catch-all handler for "/<path:path>"
-    error_handler = app.error_handler_spec[None][404][werkzeug.exceptions.NotFound]
-    app.register_error_handler(404, make_wrapper(error_handler))
-
-    return app
-
-
 def instantiate_app_with_views(
     context: IWorkspaceProcessContext,
     schema,
     app_path_prefix,
     target_dir=os.path.dirname(__file__),
-    # If you are injecting middleware that registers permissions on its own, set register_permissions_middleware to False
-    register_permissions_middleware=True,
     graphql_middleware=None,
 ):
     app = Flask(
@@ -262,9 +226,6 @@ def instantiate_app_with_views(
     app.app_protocol = lambda environ_path_info: "graphql-ws"
     app.register_blueprint(bp)
     app.register_error_handler(404, index_view)
-
-    if register_permissions_middleware:
-        app = register_permissions(app, context)
 
     CORS(app)
 
